f5e8a11fec658d99a1d3aab79ef251f660b6b0d4
Robert <robert.dober@af83.com>
2017-09-12 15:39:43 +0200

    Refs: #4440@1.5h;
       ImportPolicy changing *.imports →' imports.*

--- lib/stif/permission_translator.rb
+++ lib/stif/permission_translator.rb
@@ -19,6 +19,7 @@ module Stif
         access_points
          connection_links calendars
          footnotes
+         imports
          journey_patterns
          referentials routes routing_constraint_zones
          time_tables
@@ -28,7 +29,7 @@ module Stif
 
     def destructive_permissions_for(models)
       @__destructive_permissions_for__ ||=
-        models.product( %w{create destroy import update} ).map{ |model_action| model_action.join('.') }
+        models.product( %w{create destroy update} ).map{ |model_action| model_action.join('.') }
     end
 
     def extra_permission_translation extra_permission

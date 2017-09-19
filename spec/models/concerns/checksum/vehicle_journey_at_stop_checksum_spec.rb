55f0e2cc6344cfecbce88c61754f672b6e6996a5
Robert <robert.dober@af83.com>
2017-09-19 16:06:54 +0200

    Refs: #3845@0.25h;
    Added time insensivity spec for VehicleJourneyAtStop checksum spec.

--- spec/models/concerns/checksum/vehicle_journey_at_stop_checksum_spec.rb
+++ spec/models/concerns/checksum/vehicle_journey_at_stop_checksum_spec.rb
@@ -20,4 +20,22 @@ RSpec.describe Chouette::VehicleJourneyAtStop, type: :checksum do
 
   it_behaves_like 'checksummed model'
   
+
+  context 'its checksum does not depend on different dates' do
+    let( :today_arrival ){ Time.now.beginning_of_day + 1.hour }
+    let( :today_departure ){ today_arrival + 1.minute }
+    let( :yesterday_arrival ){ today_arrival.yesterday }
+    let( :yesterday_departure ){ today_departure.yesterday }
+    
+    let( :today ){ create factory, arrival_time: today_arrival, departure_time: today_departure }
+    let( :yesterday ){ create factory, arrival_time: yesterday_arrival, departure_time: yesterday_departure }
+
+    it do
+      today_checksum = today.checksum
+      expect( yesterday.checksum ).to eq(today_checksum)
+    end
+
+
+    
+  end
 end

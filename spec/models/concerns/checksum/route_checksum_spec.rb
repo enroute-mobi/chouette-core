f2edba421b6bcdf83e98dd29e628eaf7b4e246ac
Robert <robert.dober@af83.com>
2017-09-19 09:47:02 +0200

    Refs: #3845@0.5h;
      Refactoring, giving things in `spec/support/data_modifier**' some meaningfull - I hope - names

--- spec/models/concerns/checksum/route_checksum_spec.rb
+++ spec/models/concerns/checksum/route_checksum_spec.rb
@@ -1,10 +1,12 @@
+EnumBox = Support::DataModifier::EnumBox
+
 RSpec.describe Chouette::Route, type: :checksum do
   let( :factory ){ :route }
 
   let( :base_atts ){{
     name: 'sixty six',
     published_name: 'ninty nine',
-    wayback: make_enum(:inbound, :outbound)
+    wayback: EnumBox.new(:inbound, :outbound)
   }}
 
   let( :same_checksum_atts ){ base_atts.merge( object_version: base_atts.fetch(:object_version, 0).succ )  }
--- spec/support/checksum_support.rb
+++ spec/support/checksum_support.rb
@@ -1,9 +1,9 @@
-require 'support/helpers/data_modifier'
-include Support::Helpers::DataModifier
+require 'support/data_modifier'
+include Support::DataModifier
 
 shared_examples 'checksummed model' do
-  let( :stripped_base_atts ){ strip_values( base_atts )  }
-  let( :stripped_same_atts ){ strip_values( same_checksum_atts ) }
+  let( :stripped_base_atts ){ Box.unbox( base_atts )  }
+  let( :stripped_same_atts ){ Box.unbox( same_checksum_atts ) }
 
   let( :reference ){ create factory, **stripped_base_atts }
 
@@ -14,7 +14,7 @@ shared_examples 'checksummed model' do
     expect( create(factory, **stripped_same_atts).checksum ).to eq(reference.checksum)
     # Different checksum related data --> different checksum
     modify_atts(base_atts).each do | delta_atts |
-      expect( create(factory, **strip_values(delta_atts)).checksum ).not_to eq(reference.checksum)
+      expect( create(factory, **Box.unbox( delta_atts )).checksum ).not_to eq(reference.checksum)
     end
   end
 end

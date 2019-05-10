95cf585fe6aee4865fa811c7d4141c70ebaf733c
Johan Van Ryseghem <johan@about-blank.fr>
2019-05-10 10:55:35 +0200

    Refs #11699; Update companies form
    Also rework migration to use pre-existing columns

--- db/migrate/20190509142514_add_codifligne_v2_fields_to_companies.rb
+++ db/migrate/20190509142514_add_codifligne_v2_fields_to_companies.rb
@@ -1,7 +1,16 @@
 class AddCodifligneV2FieldsToCompanies < ActiveRecord::Migration[5.2]
   def change
     on_public_schema_only do
-      %w(default_contact private_contact customer_service_contact).each do |f|
+      rename_column :companies, :organizational_unit, :default_contact_organizational_unit
+      rename_column :companies, :operating_department_name, :default_contact_operating_department_name
+      rename_column :companies, :email, :default_contact_email
+      rename_column :companies, :phone, :default_contact_phone
+      rename_column :companies, :fax, :default_contact_fax
+      rename_column :companies, :url, :default_contact_url
+      add_column :companies, :default_contact_name, :string
+      add_column :companies, :default_contact_more, :text
+
+      %w(private_contact customer_service_contact).each do |f|
         %w(name email phone url).each do |attr|
           add_column :companies, "#{f}_#{attr}", :string
         end

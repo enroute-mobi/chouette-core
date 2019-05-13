11002b247e1dba1e914c4b1f9b0a515dcc2911c0
Edouard Maffert <edouard.maffert@af83.com>
2019-05-13 18:02:56 +0200

    Fix specs

--- spec/policies/connection_link_policy_spec.rb
+++ spec/policies/connection_link_policy_spec.rb
@@ -1,20 +1,41 @@
 RSpec.describe ConnectionLinkPolicy, type: :policy do
 
   let( :record ){ build_stubbed :connection_link }
+  before { stub_policy_scope(record) }
 
-  permissions :create? do
-      it_behaves_like 'permitted policy and same organisation', "connection_links.create", archived_and_finalised: true
+  #
+  #  Non Destructive
+  #  ---------------
+
+  context 'Non Destructive actions →' do
+    permissions :index? do
+      it_behaves_like 'always allowed', 'anything', archived_and_finalised: true
+    end
+    permissions :show? do
+      it_behaves_like 'always allowed', 'anything', archived_and_finalised: true
+    end
   end
-  permissions :destroy? do
-      it_behaves_like 'permitted policy and same organisation', "connection_links.destroy", archived_and_finalised: true
-  end
-  permissions :edit? do
-      it_behaves_like 'permitted policy and same organisation', "connection_links.update", archived_and_finalised: true
-  end
-  permissions :new? do
-      it_behaves_like 'permitted policy and same organisation', "connection_links.create", archived_and_finalised: true
-  end
-  permissions :update? do
-      it_behaves_like 'permitted policy and same organisation', "connection_links.update", archived_and_finalised: true
+
+
+  #
+  #  Destructive
+  #  -----------
+
+  context 'Destructive actions →' do
+    permissions :create? do
+      it_behaves_like 'permitted policy', 'connection_links.create'
+    end
+    permissions :destroy? do
+      it_behaves_like 'permitted policy', 'connection_links.destroy'
+    end
+    permissions :edit? do
+      it_behaves_like 'permitted policy', 'connection_links.update'
+    end
+    permissions :new? do
+      it_behaves_like 'permitted policy', 'connection_links.create'
+    end
+    permissions :update? do
+      it_behaves_like 'permitted policy', 'connection_links.update'
+    end
   end
 end

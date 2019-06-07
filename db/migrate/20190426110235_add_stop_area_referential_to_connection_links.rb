687be07ae3e4f7ddb2c2a0fc3af5da23e28d92a1
Johan Van Ryseghem <johan@about-blank.fr>
2019-06-07 11:46:11 +0200

    Fix Connections migration

--- db/migrate/20190426110235_add_stop_area_referential_to_connection_links.rb
+++ db/migrate/20190426110235_add_stop_area_referential_to_connection_links.rb
@@ -1,6 +1,6 @@
 class AddStopAreaReferentialToConnectionLinks < ActiveRecord::Migration[4.2]
   def change
-    add_column :connection_links, :stop_area_referential_id, :integer
+    add_column :connection_links, :stop_area_referential_id, :integer, limit: 8
     add_index :connection_links, :stop_area_referential_id
   end
 end

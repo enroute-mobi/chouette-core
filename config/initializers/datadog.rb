b5759d30f6d62238295afaeb2b639945fe913248
Johan Van Ryseghem <johan@about-blank.fr>
2019-07-15 09:14:03 +0200

    Refs #11686; Update datadog service name

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,5 +1,5 @@
 Datadog.configure do |c|
-  c.use :rails, service_name: 'chouette-core'
+  c.use :rails, service_name: SmartEnv.fetch(:DATADOG_SERVICE_NAME, default: 'chouette-core')
   if Rails.env.production?
     c.tracer hostname: 'datadog-agent', port: 8126
   end

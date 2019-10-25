cf304532c6458dfdb69941ea06b285dd43b1c42c
Alban Peignier <alban.peignier@enroute.mobi>
2019-10-25 12:45:32 +0200

    CHOUETTE-47 Use bitbucket scripts

--- script/bitbucket-build-docker.sh
+++ script/bitbucket-build-docker.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 export IMAGE_NAME="eu.gcr.io/$GCLOUD_PROJECT/$BITBUCKET_REPO_SLUG:$BITBUCKET_COMMIT"
-echo $GCLOUD_API_KEYFILE | base64 -d > ~/.gcloud-api-key.json
+echo "$GCLOUD_API_KEYFILE" | base64 -d > ~/.gcloud-api-key.json
 gcloud auth activate-service-account --key-file ~/.gcloud-api-key.json
-gcloud config set project $GCLOUD_PROJECT
+gcloud config set project "$GCLOUD_PROJECT"
 gcloud auth configure-docker --quiet
 
 # Build image
-docker build . -t $IMAGE_NAME
+docker build . -t "$IMAGE_NAME"
 
 # Publish image
-docker push $IMAGE_NAME
+docker push "$IMAGE_NAME"

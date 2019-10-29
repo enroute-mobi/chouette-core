27b1db4fbcca4cb1a7cfc8b61e59a0409c100e89
Alban Peignier <alban.peignier@enroute.mobi>
2019-10-30 00:29:48 +0100

    CHOUETTE-31 Retrieve cluster credentials in bitbucket-update-image script

--- script/bitbucket-update-image.sh
+++ script/bitbucket-update-image.sh
@@ -4,7 +4,9 @@ echo "$GCLOUD_API_KEYFILE" | base64 -d > ~/.gcloud-api-key.json
 gcloud auth activate-service-account --key-file ~/.gcloud-api-key.json
 gcloud config set project "$GCLOUD_PROJECT"
 
+gcloud container clusters get-credentials "$GCLOUD_CLUSTER" --zone "$GCLOUD_ZONE"
+
 IMAGE_NAME="eu.gcr.io/$GCLOUD_PROJECT/$BITBUCKET_REPO_SLUG:$BITBUCKET_COMMIT"
-kubectl set image deployment --namespace="$GCLOUD_NAMESPACE" front front="$IMAGE_NAME"
-kubectl set image deployment --namespace="$GCLOUD_NAMESPACE" async async="$IMAGE_NAME"
-kubectl set image deployment --namespace="$GCLOUD_NAMESPACE" sync sync="$IMAGE_NAME"
+kubectl set image deployment --namespace="$GCLOUD_NAMESPACE" front front="$IMAGE_NAME" --record
+kubectl set image deployment --namespace="$GCLOUD_NAMESPACE" async async="$IMAGE_NAME" --record
+kubectl set image deployment --namespace="$GCLOUD_NAMESPACE" sync sync="$IMAGE_NAME" --record

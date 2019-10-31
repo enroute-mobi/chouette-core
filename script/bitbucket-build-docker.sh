de0a70858278972a6481c3d6ef789876f0116935
Alban Peignier <alban.peignier@enroute.mobi>
2019-10-31 09:21:34 +0100

    CHOUETTE-31 Create version.json file in bitbucket pipeline build

--- script/bitbucket-build-docker.sh
+++ script/bitbucket-build-docker.sh
@@ -7,7 +7,7 @@ gcloud config set project "$GCLOUD_PROJECT"
 gcloud auth configure-docker --quiet
 
 # Build image
-docker build . -t "$IMAGE_NAME"
+docker build . -t "$IMAGE_NAME" --build-arg VERSION="build-$BITBUCKET_BUILD_NUMBER"
 
 # Publish image
 docker push "$IMAGE_NAME"

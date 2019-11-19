0ba64e67280e8a0cd5e87fcd0a98522994c815cb
Alban Peignier <alban.peignier@enroute.mobi>
2019-11-19 19:02:10 +0100

    CHOUETTE-142 Remove seedbank

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,10 @@
 # Empty
+
+def on_public_schema_only
+  yield if Apartment::Tenant.current == "public"
+end
+
+on_public_schema_only do
+  "Update user permissions according profiles"
+  Permission::Profile.update_users_permissions
+end

d95972888dd6226a5529a729ec7e6fa6d0999570
Alban Peignier <alban.peignier@enroute.mobi>
2019-11-23 21:32:12 +0100

    CHOUETTE-106 Use 'using' option in ChangeCompaniesCustomFieldsValuesType migration to cast existing values

--- db/migrate/20180405133659_change_companies_custom_fields_values_type.rb
+++ db/migrate/20180405133659_change_companies_custom_fields_values_type.rb
@@ -1,8 +1,8 @@
 class ChangeCompaniesCustomFieldsValuesType < ActiveRecord::Migration[4.2]
   def change
     reversible do |dir|
-      dir.up { change_column :companies, :custom_field_values, 'jsonb USING CAST(custom_field_values AS jsonb)', :default => {} }
-      dir.down { change_column :companies, :custom_field_values, 'json USING CAST(custom_field_values AS json)', :default => {} }
+      dir.up { change_column :companies, :custom_field_values, 'jsonb', using: 'custom_field_values::jsonb', :default => {} }
+      dir.down { change_column :companies, :custom_field_values, 'json', using: 'custom_field_values::json', :default => {} }
     end
   end
 end

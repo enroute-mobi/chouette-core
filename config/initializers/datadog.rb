1adb31b4873ca7e7530d6bbddcc50ce447d6394a
Alban Peignier <alban.peignier@enroute.mobi>
2019-11-24 22:26:58 +0100

    CHOUETTE-167 Add DD_TRACE_DEBUG to enable Datadog Trace Debug

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,8 +1,9 @@
 Datadog.configure do |c|
   c.use :rails
   c.use :delayed_job
-  # c.use :faraday
-  # c.use :rake
+  c.use :faraday
+  c.use :rake
 
   c.runtime_metrics_enabled = true
+  c.tracer debug: true if ENV['DD_TRACE_DEBUG'] == 'true'
 end

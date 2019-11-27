406c862e592fff293e7ea1be18e7346b109fb2b2
Alban Peignier <alban.peignier@enroute.mobi>
2019-11-27 19:06:25 +0100

    CHOUETTE-171 Fixes period check in GTFS Export spec

--- spec/models/export/gtfs_spec.rb
+++ spec/models/export/gtfs_spec.rb
@@ -352,7 +352,7 @@ RSpec.describe Export::Gtfs, type: [:model, :with_exportable_referential] do
         random_vj_period = vj_periods.sample
 
         # Find matching random stop in exported trips.txt file
-        random_gtfs_trip = source.trips.detect {|t| t.service_id == random_vj_period.first.object_id.to_s && t.route_id == random_vj_period.last.route.line.registration_number.to_s}
+        random_gtfs_trip = source.trips.detect {|t| t.service_id =~ /#{random_vj_period.second.objectid}/ && t.route_id == random_vj_period.last.route.line.registration_number.to_s}
         expect(random_gtfs_trip).not_to be_nil
 
         ################################

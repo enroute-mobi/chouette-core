82029d9672568fa0e1f4d0f15555e3abcbbf7cf9
Alban Peignier <alban.peignier@enroute.mobi>
2019-11-30 11:11:03 +0100

    CHOUETTE-167 Only configure Datadog when DD_AGENT_HOST env variable is defined

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,9 +1,11 @@
-Datadog.configure do |c|
-  c.use :rails
-  c.use :delayed_job
-  c.use :faraday
-  c.use :rake
+if ENV['DD_AGENT_HOST']
+  Datadog.configure do |c|
+    c.use :rails
+    c.use :delayed_job
+    c.use :faraday
+    c.use :rake
 
-  c.runtime_metrics_enabled = true
-  c.tracer debug: true if ENV['DD_TRACE_DEBUG'] == 'true'
+    c.runtime_metrics_enabled = true
+    c.tracer debug: true if ENV['DD_TRACE_DEBUG'] == 'true'
+  end
 end

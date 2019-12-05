429d1057517434af8523b6aec6c281114ab15c84
Alban Peignier <alban.peignier@enroute.mobi>
2019-12-05 15:43:35 +0100

    CHOUETTE-167 Configure Datadog agent according to DD_AGENT_APP and DD_AGENT_ENV env variables

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,11 +1,18 @@
 if ENV['DD_AGENT_HOST']
   Datadog.configure do |c|
-    c.use :rails
-    c.use :delayed_job
-    c.use :faraday
-    c.use :rake
+    app_name = ENV.fetch('DD_AGENT_APP', "chouette-core")
+    debug = (ENV['DD_TRACE_DEBUG'] == 'true')
+    env = ENV.fetch('DD_AGENT_ENV', "production")
+
+    puts "Enable Datadog Agent for #{app_name}:#{env}"
+
+    c.tracer debug: debug, env:env
+
+    c.use :rails, service_name: "#{app_name}-front", cache_service: "#{app_name}-cache", controller_service: "#{app_name}-front", database_service: "#{app_name}-postgresql"
+    c.use :delayed_job, service_name: "#{app_name}-worker"
+    c.use :faraday, service_name: "#{app_name}-http"
+    c.use :rake, service_name: "#{app_name}-rake"
 
     c.runtime_metrics_enabled = true
-    c.tracer debug: true if ENV['DD_TRACE_DEBUG'] == 'true'
   end
 end

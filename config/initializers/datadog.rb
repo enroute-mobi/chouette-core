296dcddf5bd9a51008bc432670149321e71b411e
Alban Peignier <alban.peignier@enroute.mobi>
2019-12-06 10:28:19 +0100

    CHOUETTE-167 Use front/worker for delayed_job/http/faraday integration

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -8,9 +8,17 @@ if ENV['DD_AGENT_HOST']
 
     c.tracer debug: debug, tags: {app: app_name, env: env}
 
+    service_context = (ENV['DELAYED_JOB_WORKER'] == 'true' ? "worker" : "front")
+    if service_context == "worker"
+      # To avoid Delayed::Backend::ActiveRecord::Job instantiation on 'active-record' service
+      c.use :active_record, service_name: "#{app_name}-worker", orm_service_name: "#{app_name}-worker"
+    end
+
     c.use :rails, service_name: "#{app_name}-front", cache_service: "#{app_name}-cache", controller_service: "#{app_name}-front", database_service: "#{app_name}-postgresql"
     c.use :delayed_job, service_name: "#{app_name}-worker"
-    c.use :faraday, service_name: "#{app_name}-http"
+
+    c.use :http, service_name: "#{app_name}-#{service_context}"
+    c.use :faraday, service_name: "#{app_name}-#{service_context}"
     c.use :rake, service_name: "#{app_name}-rake"
 
     c.runtime_metrics_enabled = true

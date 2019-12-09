b59410ce668538dfa96ea12817fb0e54e2c307c2
Alban Peignier <alban.peignier@enroute.mobi>
2019-12-09 14:58:49 +0100

    CHOUETTE-167 Log app name and service context when initializing Datadog trace

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -3,12 +3,12 @@ if ENV['DD_AGENT_HOST']
     app_name = ENV.fetch('DD_AGENT_APP', "chouette-core")
     debug = (ENV['DD_TRACE_DEBUG'] == 'true')
     env = ENV.fetch('DD_AGENT_ENV', "production")
+    service_context = ENV.fetch('DD_TRACE_CONTEXT',"front")
 
-    puts "Enable Datadog Agent for #{app_name}:#{env}"
+    puts "Enable Datadog Agent for #{app_name}-#{service_context}:#{env}"
 
     c.tracer debug: debug, tags: {app: app_name, env: env}
 
-    service_context = ENV.fetch('DD_TRACE_CONTEXT',"front")
     if service_context != "front"
       # To avoid Delayed::Backend::ActiveRecord::Job and User/Organisation instantiations
       # on 'active-record' service
@@ -16,7 +16,7 @@ if ENV['DD_AGENT_HOST']
       c.use :active_record, service_name: active_record_service_name, orm_service_name: active_record_service_name
     end
 
-    c.use :rails, service_name: "#{app_name}-front", cache_service: "#{app_name}-cache", controller_service: "#{app_name}-front", database_service: "#{app_name}-postgresql"
+    c.use :rails, service_name: "#{app_name}-#{service_context}", cache_service: "#{app_name}-cache", controller_service: "#{app_name}-front", database_service: "#{app_name}-postgresql"
     c.use :delayed_job, service_name: "#{app_name}-worker"
 
     c.use :http, service_name: "#{app_name}-#{service_context}"

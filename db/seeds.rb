37545b85d9ee983111e8b9b91baeb4c7d718ddaf
Alban Peignier <alban.peignier@enroute.mobi>
2020-01-06 11:38:26 +0100

    Fix missing puts in Permission::Profile seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@ def on_public_schema_only
 end
 
 on_public_schema_only do
-  "Update user permissions according profiles"
+  puts "Update user permissions according profiles"
   Permission::Profile.update_users_permissions
 end

580a543ca215fab8b170134a80736572c10f1a62
Edouard Maffert <edouard.maffert@enroute.mobi>
2020-01-21 12:40:29 +0100

    CHOUETTE-41, update CarrierWave initializer to use environment variables

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,21 +1,37 @@
 CarrierWave.configure do |config|
   config.cache_dir = Rails.root.join 'tmp/uploads'
 
-  # unless Rails.env.test? or Rails.env.cucumber?
-  # config.storage = :file
+  if Rails.env.test?
+    config.storage = :file
+    return
+  end
 
-  config.storage                             = :gcloud
-  config.gcloud_bucket                       = 'chouette-core-local'
-  config.gcloud_bucket_is_public             = false
-  config.gcloud_authenticated_url_expiration = 600
+  SmartEnv.set :STORAGE, default: 'file'
+  raise "CarrierWave initializer error: Unknown CarrierWave storage strategy" unless ['file', 'gcloud'].include? SmartEnv['STORAGE']
+
+  config.storage = SmartEnv['STORAGE']
+
+  return if SmartEnv['STORAGE'] == 'file'
+
+  SmartEnv.set :GCLOUD_PROJECT
+  raise "CarrierWave initializer error: No gcloud project specified (use GCLOUD_PROJECT environment variable)" if SmartEnv['GCLOUD_PROJECT'].blank?
+
+  SmartEnv.set :GCLOUD_BUCKET, default: 'chouette-core-local'
+  SmartEnv.set :GCLOUD_BUCKET_IS_PUBLIC, default: false
+  SmartEnv.set :GCLOUD_AUTHENTICATED_URL_EXPIRATION, default: 600
+  SmartEnv.set :GCLOUD_KEYFILE, default: 'config/storage-key.json'
+
+  config.gcloud_bucket                       = SmartEnv['GCLOUD_BUCKET']
+  config.gcloud_bucket_is_public             = SmartEnv['GCLOUD_BUCKET_IS_PUBLIC']
+  config.gcloud_authenticated_url_expiration = SmartEnv['GCLOUD_AUTHENTICATED_URL_EXPIRATION']
 
   config.gcloud_attributes = {
-    expires: 600
+    expires: SmartEnv['GCLOUD_AUTHENTICATED_URL_EXPIRATION']
   }
 
   config.gcloud_credentials = {
-    gcloud_project: 'test-alban-250508',
-    gcloud_keyfile: 'storage-key.json'
+    gcloud_project: SmartEnv['GCLOUD_PROJECT'],
+    gcloud_keyfile: SmartEnv['GCLOUD_KEYFILE']
   }
 end
 

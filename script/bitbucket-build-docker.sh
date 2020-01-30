f243685cbb674ed51560d4b4495922554fbd50b0
Alban Peignier <alban.peignier@enroute.mobi>
2020-01-30 17:10:58 +0100

    Update bitbucket scripts to share gcloud configuration

--- script/bitbucket-build-docker.sh
+++ script/bitbucket-build-docker.sh
@@ -1,10 +1,6 @@
 #!/bin/sh -e
 
 export IMAGE_NAME="eu.gcr.io/$GCLOUD_PROJECT/$BITBUCKET_REPO_SLUG:$BITBUCKET_COMMIT"
-echo "$GCLOUD_API_KEYFILE" | base64 -d > ~/.gcloud-api-key.json
-gcloud auth activate-service-account --key-file ~/.gcloud-api-key.json
-gcloud config set project "$GCLOUD_PROJECT"
-gcloud auth configure-docker --quiet
 
 # Build image
 docker build . -t "$IMAGE_NAME" --build-arg VERSION="build-$BITBUCKET_BUILD_NUMBER"

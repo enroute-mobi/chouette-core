b5e94b3957e7356de91a44696bb693166ef54e81
Alban Peignier <alban.peignier@enroute.mobi>
2020-02-05 17:59:13 +0100

    CHOUETTE-287 Define belongs_to relations in TimeTablesVehicleJourney. Add find_each_without_primary_key method and copy support

--- app/models/chouette/time_tables_vehicle_journey.rb
+++ app/models/chouette/time_tables_vehicle_journey.rb
@@ -1,2 +1,23 @@
 class Chouette::TimeTablesVehicleJourney < ActiveRecord::Base
+  acts_as_copy_target
+
+  belongs_to :time_table
+  belongs_to :vehicle_journey
+
+  def self.find_each_without_primary_key(&block)
+    batch_size = 1000
+    offset = 0
+
+    loop do
+      records = order(:time_table_id, :vehicle_journey_id).offset(offset).limit(batch_size).records
+
+      records.each do |record|
+        yield record
+      end
+
+      break if records.size < batch_size
+      offset += batch_size
+    end
+  end
+
 end

665e0486ef8115401356e974a08f251deed0b261
Luc Donnet <luc.donnet@enroute.mobi>
2020-02-07 20:41:30 +0100

    CHOUETTE-292 Delete unused |u| in spec

--- spec/factories/publication_setups.rb
+++ spec/factories/publication_setups.rb
@@ -7,22 +7,22 @@ FactoryGirl.define do
     export_options { { duration: 200 } }
   end
 
-  factory :publication_setup_gtfs, :parent => :publication_setup do |u|
+  factory :publication_setup_gtfs, :parent => :publication_setup do
     export_type "Export::Gtfs"
     export_options { { duration: 200 } }
   end
 
-  factory :publication_setup_idfm_netex_full, :parent => :publication_setup do |u|
+  factory :publication_setup_idfm_netex_full, :parent => :publication_setup do
     export_type "Export::Netex"
     export_options { {export_type: :full, duration: 60} }
   end
 
-  factory :publication_setup_idfm_netex_line, :parent => :publication_setup do |u|
+  factory :publication_setup_idfm_netex_line, :parent => :publication_setup do
     export_type "Export::Netex"
     export_options { {export_type: :line, duration: 60} }
   end
 
-  factory :publication_setup_netex_full, :parent => :publication_setup do |u|
+  factory :publication_setup_netex_full, :parent => :publication_setup do
     export_type "Export::NetexFull"
     export_options { { duration: 200 } }
   end

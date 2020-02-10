550ce001bc912fef8dd53aface480f136a34f4c7
Aurélien GUIRAMAND <aurelien.guiramand@enroute.mobi>
2020-02-10 16:22:50 +0100

    Add relationship record abstract class to manage model without pk export

---
+++ app/models/chouette/relationship_record.rb
@@ -0,0 +1,26 @@
+module Chouette
+  class RelationshipRecord < Chouette::ActiveRecord
+    acts_as_copy_target
+
+    self.abstract_class = true
+    class << self
+      def find_each_without_primary_key(&block)
+        batch_size = 1000
+        offset = 0
+
+        loop do
+          records = order(self.column_names).offset(offset).limit(batch_size).records
+
+          records.each do |record|
+            yield record
+          end
+
+          break if records.size < batch_size
+          offset += batch_size
+        end
+      end
+
+    end
+
+  end
+end
--- app/models/chouette/time_tables_vehicle_journey.rb
+++ app/models/chouette/time_tables_vehicle_journey.rb
@@ -1,23 +1,8 @@
-class Chouette::TimeTablesVehicleJourney < ActiveRecord::Base
-  acts_as_copy_target
+module Chouette
+  class TimeTablesVehicleJourney < Chouette::RelationshipRecord
 
-  belongs_to :time_table
-  belongs_to :vehicle_journey
+    belongs_to :time_table
+    belongs_to :vehicle_journey
 
-  def self.find_each_without_primary_key(&block)
-    batch_size = 1000
-    offset = 0
-
-    loop do
-      records = order(:time_table_id, :vehicle_journey_id).offset(offset).limit(batch_size).records
-
-      records.each do |record|
-        yield record
-      end
-
-      break if records.size < batch_size
-      offset += batch_size
-    end
   end
-
 end

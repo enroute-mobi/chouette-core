e461b50aa825ef1b203ecb9d0c86360add62a072
Alban Peignier <alban.peignier@enroute.mobi>
2020-02-22 15:15:02 +0100

    CHOUETTE-152 Create Pull Request after bummr

---
+++ script/bitbucket-bummr-pull-request.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -e
+
+# remove the Gemfile freeze for bundle outdated
+bundle install --no-deployment
+
+export BUMMR_TEST="bundle exec rake ci"
+export BUMMR_HEADLESS="true"
+
+branch_name="gems-update-$BITBUCKET_BUILD_NUMBER"
+echo "Create branch '$branch_name"
+git checkout -b $branch_name
+bummr update
+git push
+
+pull_request_name="Gems update Week `date "+%W %Y"`"
+
+echo "Create Pull Request '$pull_request_name'"
+
+curl -q "https://api.bitbucket.org/2.0/repositories/$BITBUCKET_REPO_FULL_NAME/pullrequests" \
+  -u "$BITBUCKET_USERNAME:$BITBUCKET_APP_PASSWORD" \
+  --header 'Content-Type: application/json' \
+  --data '{
+    "title": "$pull_request_name",
+    "source": {
+      "branch": {
+        "name": "$branch_name"
+      }
+    }
+  }'

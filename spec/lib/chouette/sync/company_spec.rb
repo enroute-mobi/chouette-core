44eec1b7e6d37e5a6798d2691b25b0bc68dd3d60
Alban Peignier <alban.peignier@enroute.mobi>
2020-02-23 10:34:18 +0100

    CHOUETTE-311 Complete Chouette::Sync::Company::Netex

--- spec/lib/chouette/sync/company_spec.rb
+++ spec/lib/chouette/sync/company_spec.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 RSpec.describe Chouette::Sync::Company do
 
   describe Chouette::Sync::Company::Netex do
@@ -15,49 +16,54 @@ RSpec.describe Chouette::Sync::Company do
         <operators>
           <Operator version="any"
           dataSourceRef="FR1:OrganisationalUnit::"
-          id="FR1:Operator:503:LOC">
-            <BrandingRef ref="" />
-            <Name>AEROPORT PARIS BEAUVAIS</Name>
+          changed="2019-04-23T13:04:39Z"
+          id="FR1:Operator:025:LOC">
+            <BrandingRef ref="/uploads/logos/" />
+            <Name>CEOBUS</Name>
             <ContactDetails>
               <ContactPerson></ContactPerson>
               <Email></Email>
-              <Phone></Phone>
+              <Phone>01 34 42 72 74</Phone>
               <Url></Url>
               <FurtherDetails></FurtherDetails>
             </ContactDetails>
             <Address>
-              <HouseNumber></HouseNumber>
+              <HouseNumber>35</HouseNumber>
               <AddressLine1></AddressLine1>
-              <Street></Street>
-              <Town></Town>
-              <PostCode></PostCode>
+              <Street>rue des fossettes</Street>
+              <Town>Génicourt</Town>
+              <PostCode>95650</PostCode>
               <PostCodeExtension></PostCodeExtension>
             </Address>
           </Operator>
           <Operator version="any"
-          dataSourceRef="FR1:OrganisationalUnit::"
-          id="FR1:Operator:088:LOC">
-            <BrandingRef ref="" />
-            <Name>VEXIN BUS</Name>
+          dataSourceRef="FR1:OrganisationalUnit:IDFM:"
+          changed="2019-09-09T09:20:30Z"
+          id="FR1:Operator:251:LOC">
+            <BrandingRef ref="/uploads/logos/" />
+            <Name>TIM BUS</Name>
             <ContactDetails>
-              <ContactPerson></ContactPerson>
-              <Email></Email>
-              <Phone></Phone>
-              <Url></Url>
-              <FurtherDetails></FurtherDetails>
+              <ContactPerson>Maddy</ContactPerson>
+              <Email>contact@tim-bus.fr</Email>
+              <Phone>01 34 46 88 00</Phone>
+              <Url>http://tim-bus.fr</Url>
+              <FurtherDetails>More</FurtherDetails>
             </ContactDetails>
             <Address>
-              <HouseNumber></HouseNumber>
-              <AddressLine1></AddressLine1>
-              <Street></Street>
-              <Town></Town>
-              <PostCode></PostCode>
-              <PostCodeExtension></PostCodeExtension>
+              <HouseNumber>7</HouseNumber>
+              <AddressLine1>ZA de la Demi-Lune</AddressLine1>
+              <Street>rue des Frères Montgolfier</Street>
+              <Town>Magny en Vexin</Town>
+              <PostCode>95420</PostCode>
+              <PostCodeExtension>Cedex 21</PostCodeExtension>
             </Address>
           </Operator>
         </operators>
       }
     end
+
+    CREATED_ID = 'FR1:Operator:025:LOC'
+    UDPATED_ID = 'FR1:Operator:251:LOC'
 
     let(:source) do
       Netex::Source.new.tap do |source|
@@ -70,40 +76,57 @@ RSpec.describe Chouette::Sync::Company do
       Chouette::Sync::Company::Netex.new source: source, target: target
     end
 
-    let!(:existing_company) do
-      target.companies.create! name: "Old Name", registration_number: "FR1:Operator:503:LOC"
+    let!(:updated_company) do
+      target.companies.create! name: 'Old Name', registration_number: UDPATED_ID
     end
 
     let(:created_company) do
-      company("FR1:Operator:088:LOC")
+      company(CREATED_ID)
     end
 
     def company(registration_number)
       target.companies.find_by(registration_number: registration_number)
     end
 
-    it "should create the Company FR1:Operator:088:LOC" do
+    it "should create the Company #{CREATED_ID}" do
       sync.synchronize
 
       expected_attributes = {
-        name: "VEXIN BUS",
-        time_zone: "Europe/Paris"
+        name: 'CEOBUS',
+        time_zone: 'Europe/Paris',
+        default_contact_phone: '01 34 42 72 74',
+        house_number: '35',
+        street: 'rue des fossettes',
+        town: 'Génicourt',
+        postcode: '95650'
       }
       expect(created_company).to have_attributes(expected_attributes)
     end
 
-    it "should update the FR1:Company:C01659:" do
+    it "should update the #{UDPATED_ID}" do
       sync.synchronize
 
       expected_attributes = {
-        name: "AEROPORT PARIS BEAUVAIS",
+        name: 'TIM BUS',
+        time_zone: 'Europe/Paris',
+        default_contact_name: 'Maddy',
+        default_contact_email: 'contact@tim-bus.fr',
+        default_contact_phone: '01 34 46 88 00',
+        default_contact_url: 'http://tim-bus.fr',
+        default_contact_more: 'More',
+        house_number: '7',
+        address_line_1: 'ZA de la Demi-Lune',
+        street: 'rue des Frères Montgolfier',
+        town: 'Magny en Vexin',
+        postcode: '95420',
+        postcode_extension: 'Cedex 21'
       }
-      expect(existing_company.reload).to have_attributes(expected_attributes)
+      expect(updated_company.reload).to have_attributes(expected_attributes)
     end
 
-    it "should destroy Companies no referenced in the source" do
+    it 'should destroy Companies no referenced in the source' do
       useless_company =
-        target.companies.create! name: "Useless", registration_number: "unknown"
+        target.companies.create! name: 'Useless', registration_number: 'unknown'
       sync.synchronize
       expect(target.companies.where(id:useless_company)).to_not exist
     end

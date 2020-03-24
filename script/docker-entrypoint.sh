6ac251506e91ca48a63ce3dd196fd22ee9fde599
Alban Peignier <alban.peignier@enroute.mobi>
2020-03-24 17:24:11 +0100

    CHOUETTE-41 Fixes docker-entrypoint when creating storage-key.json

--- script/docker-entrypoint.sh
+++ script/docker-entrypoint.sh
@@ -5,7 +5,7 @@ command=${1:-front}
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 if [ -n "$GCLOUD_KEYFILE_JSON" ]; then
-    echo "$GCLOUD_KEYFILE_JSON" > "config/storage-key.json"
+    echo -En "$GCLOUD_KEYFILE_JSON" > "config/storage-key.json"
     unset GCLOUD_KEYFILE_JSON
 fi
 

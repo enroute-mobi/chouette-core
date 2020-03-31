3d2d31a49d93a0ac1f48bb2d858acb5cde70407f
Alban Peignier <alban.peignier@enroute.mobi>
2020-03-31 14:34:26 +0200

    CHOUETTE-369 Run migrations via a Kubernetes Job

--- script/bitbucket-update-image.sh
+++ script/bitbucket-update-image.sh
@@ -12,6 +12,36 @@ fi
 gcloud container clusters get-credentials "$GCLOUD_CLUSTER" $CLUSTER_OPTIONS
 
 IMAGE_NAME="eu.gcr.io/$GCLOUD_PROJECT/$BITBUCKET_REPO_SLUG:$BITBUCKET_COMMIT"
+
+# Run migrations
+
+MIGRATE_JOB_FILE=tmp/k8s-migrate.yaml
+cat > $MIGRATE_JOB_FILE <<EOF
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: migrate
+spec:
+  template:
+    spec:
+      restartPolicy: Never
+      containers:
+      - name: migrate
+        image: $IMAGE_NAME
+        args: ['migrate']
+        envFrom:
+          - configMapRef:
+              name: chouette-config
+          - secretRef:
+              name: chouette-secrets
+EOF
+
+kubectl --namespace="$GCLOUD_NAMESPACE" create -f $MIGRATE_JOB_FILE
+kubectl --namespace="$GCLOUD_NAMESPACE" wait --for=condition=complete --timeout=600s job/migrate
+kubectl --namespace="$GCLOUD_NAMESPACE" delete job migrate
+
+# Update containers image
+
 kubectl set image deployment --namespace="$GCLOUD_NAMESPACE" front front="$IMAGE_NAME" --record
 kubectl set image deployment --namespace="$GCLOUD_NAMESPACE" async async="$IMAGE_NAME" --record
 kubectl set image deployment --namespace="$GCLOUD_NAMESPACE" sync sync="$IMAGE_NAME" --record

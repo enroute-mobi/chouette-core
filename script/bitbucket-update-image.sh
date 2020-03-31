3d9222ec0600f2a9be1a0ad7a50fee52e884cf72
Alban Peignier <alban.peignier@enroute.mobi>
2020-03-31 10:28:09 +0200

    CHOUETTE-368 Support both region and zone cluster options in bitbucket-update-image

--- script/bitbucket-update-image.sh
+++ script/bitbucket-update-image.sh
@@ -1,6 +1,15 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
-gcloud container clusters get-credentials "$GCLOUD_CLUSTER" --zone "$GCLOUD_ZONE"
+CLUSTER_OPTIONS=""
+
+if [ -n "$GCLOUD_REGION" ]; then
+    CLUSTER_OPTIONS="$CLUSTER_OPTIONS --region '$GCLOUD_REGION'"
+fi
+if [ -n "$GCLOUD_ZONE" ]; then
+    CLUSTER_OPTIONS="$CLUSTER_OPTIONS --zone '$GCLOUD_ZONE'"
+fi
+
+gcloud container clusters get-credentials "$GCLOUD_CLUSTER" $CLUSTER_OPTIONS
 
 IMAGE_NAME="eu.gcr.io/$GCLOUD_PROJECT/$BITBUCKET_REPO_SLUG:$BITBUCKET_COMMIT"
 kubectl set image deployment --namespace="$GCLOUD_NAMESPACE" front front="$IMAGE_NAME" --record

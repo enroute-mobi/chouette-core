f6eb07a8f5e839946fe39f73687474ec6ffbfe65
Alban Peignier <alban.peignier@enroute.mobi>
2020-04-22 12:17:19 +0200

    CHOUETTE-345 Split WorkbenchController#authenticate in an Authentication object

--- app/controllers/api/v1/workbench_controller.rb
+++ app/controllers/api/v1/workbench_controller.rb
@@ -4,30 +4,80 @@ class Api::V1::WorkbenchController < ActionController::Base
   layout false
   before_action :authenticate
 
+  protected
+
+  def current_workbench
+    @current_workbench
+  end
+
   private
 
-  def authenticate
-    auth_method = ActionController::HttpAuthentication::Basic.auth_scheme(request)&.downcase&.to_sym
-    if (auth_method == :basic)
+  def authentication_scope
+    Workbench.find(params[:workbench_id]).api_keys
+  end
+
+  def authentication_scheme
+    ActionController::HttpAuthentication::Basic.auth_scheme(request)&.downcase&.to_sym
+  end
+
+  def authenticate_or_request(&block)
+    if authentication_scheme == :basic
       authenticate_or_request_with_http_basic do |username, password|
-        api_key = ApiKey.find_by token: password
-        workbench = api_key&.workbench
-        @current_workbench = (workbench && workbench.organisation.code == username && (params[:workbench_id] ? workbench.id.to_s == params[:workbench_id] : true)) ? workbench : nil
-        @current_workgroup = @current_workbench&.workgroup
-        @current_workbench.present?
-      end
-    elsif (auth_method == :token)
-      authenticate_or_request_with_http_token do |token|
-        api_key = ApiKey.find_by token: token
-        workbench = api_key&.workbench
-        @current_workbench = (workbench && (params[:workbench_id] ? workbench.id.to_s == params[:workbench_id] : true)) ? workbench : nil
-        @current_workgroup = @current_workbench&.workgroup
-        @current_workgroup.present?
+        Authentication.new(authentication_scope, token: password, organisation_code: username).validate(&block)
       end
     else
-      # Render a 401 http error
-      ActionController::HttpAuthentication::Token.authentication_request(self, "Application")
+      authenticate_or_request_with_http_token do |token|
+        Authentication.new(authentication_scope, token: token).validate(&block)
+      end
     end
+  rescue ActiveRecord::RecordNotFound
+    request_http_token_authentication
+  end
+
+  def authenticate
+    authenticate_or_request do |authentication|
+      @current_workbench = authentication.workbench
+    end
+  end
+
+  class Authentication
+
+    def initialize(scope, token:, organisation_code: nil)
+      @scope, @token, @organisation_code = scope, token, organisation_code
+    end
+
+    attr_accessor :scope, :token, :organisation_code
+
+    def api_key
+      @api_key ||= scope.find_by token: token
+    end
+
+    def validate(&block)
+      if valid?
+        block.call self
+        true
+      else
+        false
+      end
+    end
+
+    def valid?
+      api_key.present? && valid_organisation_code?
+    end
+
+    def valid_organisation_code?
+      return true unless organisation_code
+      organisation_code == organisation&.code
+    end
+
+    def organisation
+      workbench&.organisation
+    end
+
+    def workbench
+      api_key&.workbench
+    end
+
   end
 
 end
--- spec/controllers/api/v1/workbench_controller_spec.rb
+++ spec/controllers/api/v1/workbench_controller_spec.rb
@@ -1,4 +1,175 @@
 RSpec.describe Api::V1::WorkbenchController, type: :controller do
+
+  describe Api::V1::WorkbenchController::Authentication do
+
+    let(:scope) { double find_by: api_key }
+    let(:token) { double "Token" }
+    let(:api_key) { double "API Key", workbench: workbench }
+    let(:workbench) { double "Workbench associated to API Key", organisation: organisation  }
+    let(:organisation) { double "Organisation associated to the Workbench", code: 'test' }
+
+    let(:authentication) do
+      Api::V1::WorkbenchController::Authentication.new scope, token: token
+    end
+
+    describe "#valid?" do
+
+      subject { authentication.valid? }
+
+      context "when API key isn't found" do
+        before { allow(authentication).to receive(:api_key).and_return nil }
+        it { is_expected.to be(false) }
+      end
+
+      context "when organisation code isn't valid" do
+        before { allow(authentication).to receive(:valid_organisation_code?).and_return false }
+        it { is_expected.to be(false) }
+      end
+
+      context "when API key is present and organisation code is valid" do
+        before do
+          allow(authentication).to receive(:api_key).and_return double
+          allow(authentication).to receive(:valid_organisation_code?).and_return true
+        end
+        it { is_expected.to be(true) }
+      end
+
+    end
+
+    describe "#api_key" do
+
+      context "when scope is a Workbench" do
+
+        let(:context) { Chouette.create { workbench } }
+        let(:scope) { context.workbench.api_keys }
+
+        subject { authentication.api_key }
+
+        context "when API Key exists in the Workbench with the given token" do
+
+          let(:api_key) { context.workbench.api_keys.create! }
+          let(:token) { api_key.token }
+
+          it "return this API key" do
+            expect(subject).to eq(api_key)
+            expect(subject.token).to eq(authentication.token)
+          end
+
+        end
+
+        context "when no API key exists with the given token" do
+
+          let(:token) { 'dummy' }
+
+          it { is_expected.to be_nil }
+
+        end
+
+      end
+
+    end
+
+    describe "#validate" do
+
+      subject { authentication.validate {} }
+
+      context "when authentification is valid" do
+
+        it { is_expected.to be(true) }
+
+        it "yield the given block" do
+          expect { |b| authentication.validate(&b) }.to yield_control
+        end
+
+      end
+
+      context "when authentification isn't valid" do
+
+        before { allow(authentication).to receive(:valid?).and_return(false) }
+
+        it { is_expected.to be(false) }
+
+        it "don't yield the given block" do
+          expect { |b| authentication.validate(&b) }.to_not yield_control
+        end
+
+      end
+
+    end
+
+    describe "#workbench" do
+
+      subject { authentication.workbench }
+
+      context "when API key is found" do
+        it { is_expected.to be(api_key.workbench) }
+      end
+
+      context "when no API key is found" do
+        before { allow(authentication).to receive(:api_key).and_return nil }
+        it { is_expected.to be_nil }
+      end
+
+    end
+
+    describe "#organisation" do
+
+      subject { authentication.organisation }
+
+      context "when API key is found" do
+        it { is_expected.to be(api_key.workbench.organisation) }
+      end
+
+      context "when no API key is found" do
+        before { allow(authentication).to receive(:api_key).and_return nil }
+        it { is_expected.to be_nil }
+      end
+
+    end
+
+  end
+
+  describe "#authentication_scheme" do
+
+    subject { controller.send :authentication_scheme }
+
+    it "returns the request authentification scheme (in downcase as symbol)" do
+      allow(controller.request).to receive(:authorization).and_return("Dummy")
+      is_expected.to be(:dummy)
+    end
+
+    it "returns nil when no authentification is found" do
+      is_expected.to be_nil
+    end
+
+  end
+
+  describe "#authentication_scope" do
+
+    subject { controller.send :authentication_scope }
+
+    context "when the request uses an existing Workbench identifier" do
+      before { allow(controller).to receive(:params).and_return(workbench_id: workbench.id) }
+
+      let(:context) { Chouette.create { workbench } }
+      let(:workbench) { context.workbench }
+
+      it "is api keys collection of this Workbench" do
+        is_expected.to eq(workbench.api_keys)
+      end
+
+    end
+
+    context "when the request uses an unknown Workbench identifier" do
+      before { allow(controller).to receive(:params).and_return(workbench_id: "dummy") }
+
+      it "raises an ActiveRecord::NotFound error" do
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+  end
+
   context '#authenticate' do
     before do
       # It appears that controller.send(:authenticate) throw an errror while testing because the request headers can't be properly instanciated this way
@@ -90,7 +261,7 @@ RSpec.describe Api::V1::WorkbenchController, type: :controller do
       it "shouldn't set the current workbench" do
         expect(assigns(:current_workbench)).to be_nil
       end
-      
+
     end
 
   end

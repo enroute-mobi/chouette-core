0fc2b8118fb1ff32f5007696be775a4c8fb34bcf
Luc Donnet <luc.donnet@enroute.mobi>
2020-04-23 10:33:43 +0200

    CHOUETTE-424 Add filter parameters for sentry

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ end
 module ChouetteIhm
   class Application < Rails::Application
 
+    # Set filter parameters for sentry
+    config.filter_parameters << :password
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

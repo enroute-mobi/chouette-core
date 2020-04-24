aeb0c7e75e50a313ff6748ebf26d1c975f3d1c81
Alban Peignier <alban.peignier@enroute.mobi>
2020-04-24 09:53:23 +0200

    CHOUETTE-349 Refactor UserDeliverInterceptor to use an instance

---
+++ app/lib/user_deliver_interceptor.rb
@@ -0,0 +1,75 @@
+class UserDeliverInterceptor
+
+  def initialize(enabled: false, whitelist: nil, blacklist: nil)
+    @enabled, @whitelist, @blacklist = enabled, whitelist, blacklist
+
+    @whitelist ||= []
+    @blacklist ||= []
+  end
+
+  attr_accessor :enabled, :blacklist, :whitelist
+
+  def delivering_email(message)
+    cancel(message) unless accept?(message)
+  end
+
+  def enabled?
+    @enabled
+  end
+
+  def accept?(message)
+    return false unless enabled?
+
+    message.to.all? do |email_address|
+      accept_email_address?(email_address)
+    end
+  end
+
+  def accept_email_address?(email_address)
+    !blacklisted?(email_address) && whitelisted?(email_address)
+  end
+
+  def cancel(message)
+    message.perform_deliveries = false
+    Rails.logger.info "Canceled email to #{message.to}"
+  end
+
+  def whitelisted?(email_address)
+    return true if whitelist.empty?
+    self.class.match? whitelist, email_address
+  end
+
+  def blacklisted?(email_address)
+    self.class.match? blacklist, email_address
+  end
+
+  DOMAIN_DEFINITION = "@"
+
+  def self.match?(definition, email_address)
+    case definition
+    when Array
+      definition.any? { |d| match? d, email_address }
+    when Regexp
+      definition.match? email_address
+    when String
+      if definition.start_with?(DOMAIN_DEFINITION)
+        email_address.end_with? definition
+      else
+        definition == email_address
+      end
+    else
+      false
+    end
+  end
+
+  def self.from_config(config = Rails.application.config)
+    unless config.try(:chouette_email_user)
+      return UserDeliverInterceptor.new enabled: false
+    end
+
+    UserDeliverInterceptor.new enabled: true,
+                               blacklist: config.try(:chouette_email_blacklist),
+                               whitelist: config.try(:chouette_email_whitelist)
+  end
+
+end
--- spec/lib/user_deliver_interceptor_spec.rb
+++ spec/lib/user_deliver_interceptor_spec.rb
@@ -1,45 +1,223 @@
 RSpec.describe UserDeliverInterceptor do
 
-  describe 'delivering_email #accepts' do
-    let(:message){ Mail::Message.new(to: ["test@randommailer.com"]) }
+  subject(:interceptor) { UserDeliverInterceptor.new(enabled: true) }
 
-    describe 'allowed recipient' do
-      before do
-        allow(Rails.application.config).to receive(:chouette_email_blacklist){ [] }
+  let(:message) { Mail::Message.new to: ["first@example.com", "second@example.com"] }
+  let(:email_address) { 'test@example.com' }
+
+  describe '#delivering_email' do
+
+    context "when the interceptor accepts the email" do
+      before { allow(interceptor).to receive(:accept?).with(message).and_return true }
+
+      it "doesn't cancel the message" do
+        expect { interceptor.delivering_email(message) }.to_not change(message, :perform_deliveries)
       end
 
-      it 'should pass' do
-        expect(UserDeliverInterceptor.blacklisted?(message)).to be false
+    end
+
+    context "when the interceptor don't accept the email" do
+      before { allow(interceptor).to receive(:accept?).with(message).and_return false }
+
+      it "doesn't cancel the message" do
+        expect { interceptor.delivering_email(message) }.to change(message, :perform_deliveries).to(false)
+      end
+
+    end
+
+  end
+
+  describe "#enabled?" do
+
+    subject { interceptor.enabled? }
+
+    context "when UserDeliverInterceptor is created with enabled: true" do
+      it { is_expected.to be(true) }
+    end
+
+    context "when UserDeliverInterceptor is created with enabled: false" do
+      let(:interceptor) { UserDeliverInterceptor.new(enabled: false) }
+      it { is_expected.to be(false) }
+    end
+
+  end
+
+  describe "#accept?" do
+
+    subject { interceptor.accept?(message) }
+
+    context "when interceptor isn't enabled?" do
+      before { interceptor.enabled = false }
+      it { is_expected.to be(false) }
+    end
+
+    context "when interceptor is enabled" do
+
+      context "and all message recipients are accepted" do
+        before { allow(interceptor).to receive(:accept_email_address?).and_return true }
+        it { is_expected.to be(true) }
+      end
+
+      context "and one of message recipients isn't accepted" do
+        before do
+          allow(interceptor).to receive(:accept_email_address?).with(message.to.first).and_return true
+          allow(interceptor).to receive(:accept_email_address?).with(message.to.second).and_return false
+        end
+        it { is_expected.to be(false) }
+      end
+
+      context "and all message recipients aren't accepted" do
+        before { allow(interceptor).to receive(:accept_email_address?).and_return false }
+        it { is_expected.to be(false) }
+      end
+
+    end
+
+  end
+
+  describe "#accept_email_address?" do
+
+    subject { interceptor.accept_email_address? email_address }
+
+    context "when email address is blacklisted" do
+      before { allow(interceptor).to receive(:blacklisted?).with(email_address).and_return true }
+      it { is_expected.to be(false) }
+    end
+
+    context "when email address isn't blacklisted" do
+      before { allow(interceptor).to receive(:blacklisted?).with(email_address).and_return false }
+
+      context "and is whitelisted" do
+        before { allow(interceptor).to receive(:whitelisted?).with(email_address).and_return true }
+        it { is_expected.to be(true) }
+      end
+
+      context "and isn't whitelisted" do
+        before { allow(interceptor).to receive(:whitelisted?).with(email_address).and_return false }
+        it { is_expected.to be(false) }
       end
     end
 
-    describe 'blacklisted recipient' do
-      before do
-        allow(Rails.application.config).to receive(:chouette_email_blacklist){ ["@randommailer.com", "@pishingmailer.net", "noreply"] }
+  end
+
+  describe '#cancel' do
+
+    it "change message perform_deliveries to false" do
+      expect { interceptor.cancel(message) }.to change(message, :perform_deliveries).to(false)
+    end
+
+  end
+
+  describe "#whitelisted?" do
+
+    subject { interceptor.whitelisted? email_address }
+
+    context "when whitelist is empty" do
+      before { interceptor.whitelist = [] }
+      it { is_expected.to be(true) }
+    end
+
+    context "when whitelist isn't empty" do
+      before { interceptor.whitelist = 'not empty' }
+
+      context "when whitelist matches the email address" do
+        before { allow(UserDeliverInterceptor).to receive(:match?).with(interceptor.whitelist, email_address).and_return true }
+        it { is_expected.to be(true) }
       end
 
-      it 'should not pass' do
-        expect(UserDeliverInterceptor.blacklisted?(message)).to be true
+      context "when whitelist doesn't match the email address" do
+        before { allow(UserDeliverInterceptor).to receive(:match?).with(interceptor.whitelist, email_address).and_return false }
+        it { is_expected.to be(false) }
+      end
+
+    end
+
+  end
+
+  describe "#blacklisted?" do
+
+    subject { interceptor.blacklisted? email_address }
+
+    context "when blacklist matches the email address" do
+      before { allow(UserDeliverInterceptor).to receive(:match?).with(interceptor.blacklist, email_address).and_return true }
+      it { is_expected.to be(true) }
+    end
+
+    context "when blacklist doesn't match the email address" do
+      before { allow(UserDeliverInterceptor).to receive(:match?).with(interceptor.blacklist, email_address).and_return false }
+      it { is_expected.to be(false) }
+    end
+
+  end
+
+  describe ".match?" do
+
+    [ # definition, email_address, expected
+      [ "@domain", "test@domain", true ],
+      [ "@domain", "test@otherdomain", false ],
+      [ 'test@example.com', 'test@example.com', true ],
+      [ 'test@example.com', ' test@example.com', false ],
+      [ 'test@example.com', 'test@example.com ', false ],
+      [ '', 'test@example.com', false ],
+      [ /example/, "test@example.com", true ],
+      [ /dummy/, "test@example.com", false ],
+      [ [ '@domain' ], "test@domain", true ],
+      [ [ '@otherdomain', '@domain' ], 'test@domain', true ],
+      [ [], 'test@example.com', false ],
+      [ :strange, 'test@example.com', false ],
+    ].each do |definition, email_address, expected|
+      it "#{expected ? "matches" : "doesn't match"} #{email_address} with this definition #{definition.inspect}" do
+        expect(UserDeliverInterceptor.match? definition, email_address).to be(expected)
+      end
+
+    end
+
+  end
+
+  describe ".from_config" do
+
+    it "uses Rails.application.config by default" do
+      expect(Rails.application.config).to receive(:chouette_email_user).and_return(true)
+      expect(UserDeliverInterceptor.from_config).to be_enabled
+    end
+
+    let(:config) { double "Rails config" }
+
+    context "when config.chouette_email_user is false" do
+      before { allow(config).to receive(:chouette_email_user).and_return(false) }
+
+      it "returns a disabled interceptor" do
+        expect(UserDeliverInterceptor.from_config(config)).to_not be_enabled
       end
     end
 
-    describe 'allow mails config' do
-      before do
-        allow(Rails.application.config).to receive(:chouette_email_user){ true }
+    context "when config.chouette_email_user is true" do
+      before { allow(config).to receive(:chouette_email_user).and_return(true) }
+
+      it "returns a enabled interceptor" do
+        expect(UserDeliverInterceptor.from_config(config)).to be_enabled
       end
 
-      it 'should pass' do
-        expect(UserDeliverInterceptor.prevent_mails?).to be false
-      end
-    end
-
-    describe 'prevent mails config' do
-      before do
-        allow(Rails.application.config).to receive(:chouette_email_user){ false }
+      it "blacklist and whitelist are empty by default" do
+        expect(UserDeliverInterceptor.from_config(config)).to have_attributes(blacklist: [], whitelist: [])
       end
 
-      it 'should not pass' do
-        expect(UserDeliverInterceptor.prevent_mails?).to be true
+      context "when config.chouette_email_blacklist is defined" do
+        let(:blacklist) { 'dummy' }
+        before { allow(config).to receive(:chouette_email_blacklist).and_return(blacklist) }
+
+        it "returns an interceptor with this blacklist" do
+          expect(UserDeliverInterceptor.from_config(config)).to have_attributes(blacklist: blacklist)
+        end
+      end
+
+      context "when config.chouette_email_whitelist is defined" do
+        let(:whitelist) { 'dummy' }
+        before { allow(config).to receive(:chouette_email_whitelist).and_return(whitelist) }
+
+        it "returns an interceptor with this whitelist" do
+          expect(UserDeliverInterceptor.from_config(config)).to have_attributes(whitelist: whitelist)
+        end
       end
     end
 

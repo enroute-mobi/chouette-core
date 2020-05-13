91d0b1938f9fa1ae2ef12cd712778ee428f38d46
Alban Peignier <alban.peignier@enroute.mobi>
2020-05-13 12:27:18 +0200

    CHOUETTE-362 Apply Company#default_language migration only in public schema

--- db/migrate/20200507124309_add_default_language_to_company.rb
+++ db/migrate/20200507124309_add_default_language_to_company.rb
@@ -1,5 +1,7 @@
 class AddDefaultLanguageToCompany < ActiveRecord::Migration[5.2]
   def change
-  	add_column :companies, :default_language, :string
+    on_public_schema_only do
+  	  add_column :companies, :default_language, :string
+    end
   end
 end

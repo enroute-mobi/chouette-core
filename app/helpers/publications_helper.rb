67aacf7a74002961adbe4e2b2ba2cdf94aa751ca
Alban Peignier <alban.peignier@enroute.mobi>
2020-05-18 12:36:54 +0200

    CHOUETTE-452 Avoid a duplicated/dedicated helper for Publication processing block

--- app/helpers/publications_helper.rb
+++ app/helpers/publications_helper.rb
@@ -10,13 +10,4 @@ module PublicationsHelper
     metadatas
   end
 
-  def publication_processing_helper(object)
-    simple_block_for object, title: I18n.t("simple_block_for.title.processing"), class: "col-lg-6 col-md-6 col-sm-12 col-xs-12" do |b|
-      content = b.attribute :created_at, as: :datetime
-      content += b.attribute :started_at, as: :datetime
-      content += b.attribute :ended_at, as: :datetime
-      content + b.attribute(:duration, value: object.ended_at.presence && object.started_at.presence && object.ended_at - object.started_at)
-    end
-  end
-
 end

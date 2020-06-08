e6962e4aecb68d6767693edbb343d677c3e8cbc0
Alban Peignier <alban.peignier@enroute.mobi>
2020-06-08 14:33:41 +0200

    CHOUETTE-502 Use on_public_schema_only in RemoveCorruptedPublicationApiSources migration

--- db/migrate/20200525144929_remove_corrupted_publication_api_sources.rb
+++ db/migrate/20200525144929_remove_corrupted_publication_api_sources.rb
@@ -1,8 +1,10 @@
 class RemoveCorruptedPublicationApiSources < ActiveRecord::Migration[5.2]
   def up
-    # Remove publication api sources without any related publication
-    PublicationApiSource.where.not(id: PublicationApiSource.joins(:publication)).destroy_all
-    # Remove publication api sources without any related publication_api
-    PublicationApiSource.where.not(id: PublicationApiSource.joins(:publication_api)).destroy_all
+    on_public_schema_only do
+      # Remove publication api sources without any related publication
+      PublicationApiSource.where.not(id: PublicationApiSource.joins(:publication)).destroy_all
+      # Remove publication api sources without any related publication_api
+      PublicationApiSource.where.not(id: PublicationApiSource.joins(:publication_api)).destroy_all
+    end
   end
 end

28fe1bc9e06ce30d9ec2b490cea76837048d1b22
Alban Peignier <alban.peignier@enroute.mobi>
2020-07-21 11:21:24 +0200

    CHOUETTE-616 Fixes ReferentialCloning specs

--- spec/models/referential_cloning_spec.rb
+++ spec/models/referential_cloning_spec.rb
@@ -1,4 +1,3 @@
-
 RSpec.describe ReferentialCloning, :type => :model do
 
   it 'should have a valid factory' do
@@ -7,19 +6,6 @@ RSpec.describe ReferentialCloning, :type => :model do
 
   it { should belong_to :source_referential }
   it { should belong_to :target_referential }
-
-  it 'should have the correct arguments in commands' do
-    r = FactoryGirl.create(:referential_cloning)
-    allow(r).to receive(:source_schema) { 'test_1' }
-    allow(r).to receive(:database) { 'chouette_test' }
-
-    expect(r.dump_command).to eq "PGPASSWORD='chouette' pg_dump --host localhost --port 5432 --username chouette --schema=test_1 chouette_test"
-    expect(r.restore_command).to eq "PGPASSWORD='chouette' psql -q --host localhost --port 5432 --username chouette chouette_test"
-
-    allow(r).to receive(:port) { 1234 }
-    expect(r.dump_command).to eq "PGPASSWORD='chouette' pg_dump --host localhost --port 1234 --username chouette --schema=test_1 chouette_test"
-    expect(r.restore_command).to eq "PGPASSWORD='chouette' psql -q --host localhost --port 1234 --username chouette chouette_test"
-  end
 
   let(:source_referential) { Referential.new slug: "source", organisation: build(:organisation), prefix: "source"}
   let(:target_referential) { Referential.new slug: "target", organisation: source_referential.organisation, prefix: "target"}
@@ -56,21 +42,22 @@ RSpec.describe ReferentialCloning, :type => :model do
   end
 
   describe '#clone!' do
-    let(:cloner) { double }
-
-    it 'creates a schema cloner with source and target schemas and clone schema' do
-      %w{dump_command sed_command restore_command}.each do |command|
-        allow(referential_cloning).to receive(command).and_return(command)
-      end
+    before do
       allow(referential_cloning).to receive(:clean)
+    end
 
-      expect(referential_cloning).to receive(:system).with("dump_command | sed_command | restore_command").and_return(true)
-
+    it 'clones source schema into target schema' do
+      expect(source_referential.schema).to receive(:clone_to).with(target_referential.schema)
       referential_cloning.clone!
     end
   end
 
   describe '#clone_with_status!' do
+
+    before do
+      allow(referential_cloning).to receive(:clone!)
+    end
+
     it 'invokes clone! method' do
       expect(referential_cloning).to receive(:clone!)
       referential_cloning.clone_with_status!
@@ -78,7 +65,6 @@ RSpec.describe ReferentialCloning, :type => :model do
 
     context 'when clone_schema is performed without error' do
       it "should have successful status" do
-        expect(referential_cloning).to receive(:clone!)
         referential_cloning.clone_with_status!
         expect(referential_cloning.status).to eq("successful")
       end
@@ -86,7 +72,6 @@ RSpec.describe ReferentialCloning, :type => :model do
 
     context 'when clone_schema raises an error' do
       it "should have failed status", truncation: true do
-        expect(referential_cloning).to receive(:clone!).and_raise("#fail")
         referential_cloning.clone_with_status!
         expect(referential_cloning.status).to eq("failed")
         expect(referential_cloning.target_referential.state).to eq(:failed)

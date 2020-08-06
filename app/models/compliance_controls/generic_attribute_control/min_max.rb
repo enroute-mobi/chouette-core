7f137054f7ae417a9f2c595f7606b3361fec803c
Alban Peignier <alban.peignier@enroute.mobi>
2020-08-06 16:46:13 +0200

    CHOUETTE-513 Comment GenericAttributeControl::MinMax
    ... to prevent loading in production

--- app/models/compliance_controls/generic_attribute_control/min_max.rb
+++ app/models/compliance_controls/generic_attribute_control/min_max.rb
@@ -1,13 +1,14 @@
-module GenericAttributeControl
-  class MinMax < ComplianceControl
-    store_accessor :control_attributes, :minimum, :maximum, :target
-    
-    validates :target, presence: true
-    include MinMaxValuesValidation
-
-    class << self
-      def attribute_type; :integer end
-      def default_code; "3-Generic-2" end
-    end
-  end
-end
+# See CHOUETTE-513
+# module GenericAttributeControl
+#   class MinMax < ComplianceControl
+#     store_accessor :control_attributes, :minimum, :maximum, :target
+#
+#     validates :target, presence: true
+#     include MinMaxValuesValidation
+#
+#     class << self
+#       def attribute_type; :integer end
+#       def default_code; "3-Generic-2" end
+#     end
+#   end
+# end

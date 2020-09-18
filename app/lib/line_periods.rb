a100fbefc2be9f823a3892a6dd5472736ddba3d8
Alban Peignier <alban.peignier@enroute.mobi>
2020-09-18 09:05:28 +0200

    CHOUETTE-691 Fixes ReferentialMetadata cloning to avoid side effect on merged metadatas

--- app/lib/line_periods.rb
+++ app/lib/line_periods.rb
@@ -14,8 +14,35 @@ class LinePeriods
     end
   end
 
+  def initialize_copy(orig)
+    super
+    @periods_by_line = orig.periods_by_line.deep_dup
+  end
+
+  def merge(other)
+    dup.merge! other
+  end
+
+  # Keep periods unchanged
+  def merge!(other)
+    other.each do |line_id, periods|
+      periods.each do |period|
+        add line_id, period
+      end
+    end
+    self
+  end
+
   def periods(line_id)
     @periods_by_line[line_id]
+  end
+
+  def to_s
+    @periods_by_line.inspect
+  end
+
+  def ==(other)
+    periods_by_line == other.periods_by_line
   end
 
   def self.from_metadatas(metadatas)
@@ -31,5 +58,9 @@ class LinePeriods
 
     line_periods
   end
+
+  protected
+
+  attr_reader :periods_by_line
 
 end

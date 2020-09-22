4dd915611fdbb3b11335333cf8d65582837e6c91
Luc Donnet <luc.donnet@enroute.mobi>
2020-09-22 16:30:16 +0200

    CHOUETTE-699 Delete line_code validation for publication_setup export options

--- app/models/publication_setup.rb
+++ app/models/publication_setup.rb
@@ -37,7 +37,7 @@ class PublicationSetup < ApplicationModel
   def export_options_are_valid
     dummy = new_export
     dummy.validate
-    dummy.errors.to_h.except(:name, :referential_id, :workgroup).each do |k, v|
+    dummy.errors.to_h.except(:name, :referential_id, :workgroup, :line_code).each do |k, v|
       errors.add(k, v)
     end
   end

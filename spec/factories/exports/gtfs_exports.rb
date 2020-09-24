8dd64bf3a240e07bc2559ddab964f013633476e3
Aurélien GUIRAMAND <aurelien.guiramand@enroute.mobi>
2020-09-24 10:24:45 +0200

    CHOUETTE-688 Add ignore_single_stop_station option to GTFS export

--- spec/factories/exports/gtfs_exports.rb
+++ spec/factories/exports/gtfs_exports.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :gtfs_export, class: Export::Gtfs, parent: :export do
-    options({duration: 90, prefer_referent_stop_area: false})
+    options({duration: 90, prefer_referent_stop_area: false, ignore_single_stop_station: false})
     type 'Export::Gtfs'
     association :parent, factory: :workgroup_export
     association :referential, factory: :workbench_referential

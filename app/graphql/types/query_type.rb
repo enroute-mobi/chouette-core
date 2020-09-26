e65a2d6a2717a80f2b44333bd20654fbd1ddfa77
Luc Donnet <luc.donnet@enroute.mobi>
2020-09-26 11:53:17 +0200

    CHOUETTE-706 Increase stop areas and lines retrieve by query in GraphQL API

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,8 +1,8 @@
 module Types
   class QueryType < Types::BaseObject
-    field :lines, resolver: Queries::Lines
+    field :lines, resolver: Queries::Lines, max_page_size: 500
     field :line, resolver: Queries::Line
-    field :stop_areas, resolver: Queries::StopAreas
+    field :stop_areas, resolver: Queries::StopAreas, max_page_size: 500
     field :stop_area, resolver: Queries::StopArea
   end
 end

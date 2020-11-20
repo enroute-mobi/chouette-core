b617fd9a4d883e590a723dabe239455c2af45ac0
Alban Peignier <alban.peignier@enroute.mobi>
2020-11-20 14:20:16 +0100

    CHOUETTE-632 Refactor StopAreaProvider migration

--- db/migrate/20201023144216_add_stop_area_provider_reference_to_stop_areas.rb
+++
@@ -1,9 +0,0 @@
-class AddStopAreaProviderReferenceToStopAreas < ActiveRecord::Migration[5.2]
-  # / ! \ This migration has to be run only on CHOUETTE, not IBOO !
-  def change
-    on_public_schema_only do
-      add_reference :stop_areas, :stop_area_provider, index: true
-    end
-  end
-
-end

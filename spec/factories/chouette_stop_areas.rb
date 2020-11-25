31833776533311f8cd66483daaf6b6218c4a9d3f
Aurélien GUIRAMAND <aurelien.guiramand@enroute.mobi>
2020-11-25 15:56:40 +0100

    CHOUETTE-752 Fix specs #11

--- spec/factories/chouette_stop_areas.rb
+++ spec/factories/chouette_stop_areas.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :stop_area, :class => Chouette::StopArea do
-    sequence(:objectid) { |n| "FR::ZDE:#{n}:STIF" }
     sequence(:name) { |n| "stop_area_#{n}" }
     sequence(:registration_number) { |n| "test-#{n}" }
     area_type { Chouette::AreaType.commercial.sample }
--- spec/factories/stop_area_providers.rb
+++ spec/factories/stop_area_providers.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :stop_area_provider do
-    sequence(:objectid) { |n| "FR1:OrganisationalUnit:#{n}:LOC" }
     sequence(:name) { |n| "StopAreaProvider #{n}"}
 
     association :workbench
+    association :stop_area_referential
   end
 end

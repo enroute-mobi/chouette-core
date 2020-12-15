5b1068e45c08c32860f1ec12f07a199aacedff35
Edouard Maffert <edouard.maffert@enroute.mobi>
2020-12-15 10:47:06 +0100

    CHOUETTE-860 New Find options for liine request

--- app/graphql/queries/line.rb
+++ app/graphql/queries/line.rb
@@ -4,11 +4,19 @@ module Queries
 
     argument :objectid, String, required: false
     argument :registration_number, String, required: false
+    argument :transport_mode, Types::TransportMode, required: false
+    argument :company, String, required: false
 
     type Types::LineType, null: true
 
-    def resolve(objectid: nil, registration_number: nil)
-      context[:target_referential].lines.find_by({objectid: objectid, registration_number: registration_number}.compact)
+    def resolve(objectid: nil, registration_number: nil, transport_mode: nil, company: nil)
+      l = context[:target_referential].lines.joins(:company).where({
+        objectid: objectid,
+        registration_number: registration_number,
+        transport_mode: transport_mode&.downcase
+      }.compact)
+      l.where("companies.name like #{company}") if company
+      l.first
     end
   end
 end
---
+++ app/graphql/types/transport_mode.rb
@@ -0,0 +1,9 @@
+module Types
+  class TransportMode < Types::BaseEnum
+    value "tram"
+    value "metro"
+    value "funicular"
+    value "bus"
+    value "rail"
+  end
+end

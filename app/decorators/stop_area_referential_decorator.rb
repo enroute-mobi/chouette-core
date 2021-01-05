53b50f07904363a96dc3bf115450bd9ed1864c08
Aurélien GUIRAMAND <aurelien.guiramand@enroute.mobi>
2021-01-05 15:00:48 +0100

    CHOUETTE-872 CHOUETTE-873 Fix some details

--- app/decorators/stop_area_referential_decorator.rb
+++ app/decorators/stop_area_referential_decorator.rb
@@ -4,10 +4,6 @@ class StopAreaReferentialDecorator < AF83::Decorator
   set_scope { [context[:workbench]] }
 
   with_instance_decorator do |instance_decorator|
-    instance_decorator.action_link secondary: :show do |l|
-      l.content { Chouette::StopArea.t.capitalize }
-      l.href { h.workbench_stop_area_referential_stop_areas_path context[:workbench] }
-    end
 
     instance_decorator.action_link primary: :show do |l|
       l.content t('stop_area_referentials.actions.edit_params')

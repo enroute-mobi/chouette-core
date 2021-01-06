782fae23af6684aaa691c87d38bb8a7cc38840a2
Luc Donnet <luc.donnet@enroute.mobi>
2021-01-06 20:33:48 +0100

    CHOUETTE-846 Stop area public code migration should be made in public schema only

--- db/migrate/20210105140047_add_public_code_to_stop_area.rb
+++ db/migrate/20210105140047_add_public_code_to_stop_area.rb
@@ -1,5 +1,7 @@
 class AddPublicCodeToStopArea < ActiveRecord::Migration[5.2]
   def change
-    add_column :stop_areas, :public_code, :string
+    on_public_schema_only do
+      add_column :stop_areas, :public_code, :string
+    end
   end
 end

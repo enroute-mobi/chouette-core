36c68ed92584b190776d9b2dc8d5b7df9a0d475f
Edouard Maffert <edouard.maffert@enroute.mobi>
2021-01-27 11:42:42 +0100

    CHOUETTE-927 Add more filters to lines Graphql Query

--- app/graphql/queries/lines.rb
+++ app/graphql/queries/lines.rb
@@ -3,14 +3,45 @@ module Queries
     description 'Find all lines'
 
     argument :transport_mode, Types::TransportMode, required: false
+    argument :transport_modes, [Types::TransportMode], required: false
     argument :company, String, required: false
+    argument :companies, [String], required: false
+
+    argument :not_transport_mode, Types::TransportMode, required: false
+    argument :not_transport_modes, [Types::TransportMode], required: false
+    argument :not_company, String, required: false
+    argument :not_companies, [String], required: false
 
     type Types::LineType.connection_type, null: false
 
-    def resolve(transport_mode: nil, company: nil)
+    def resolve(transport_mode: nil,
+      transport_modes: [],
+      company: nil,
+      companies: nil,
+      not_transport_mode: nil,
+      not_transport_modes: [],
+      not_company: nil,
+      not_companies: nil)
       scope = context[:target_referential].lines
-      scope = scope.joins(:company).where("companies.name ilike '%#{company}%'") if company
-      scope = scope.where({transport_mode: transport_mode.downcase}) if transport_mode
+
+      if company
+        scope = scope.joins(:company).where("companies.name ~~* '%#{company}%'")
+      elsif companies
+        scope = scope.joins(:company).where("companies.name ~* '.*(#{companies.compact.join('|')}).*'")
+      end
+      if not_company
+        scope = scope.joins(:company).where("companies.name !~~* '%#{not_company}%'")
+      elsif companies
+        scope = scope.joins(:company).where("companies.name !~* '.*(#{not_companies.compact.join('|')}).*'")
+      end
+
+      if transport_mode || transport_modes.length > 0
+        scope = scope.where({transport_mode: (transport_modes << transport_mode).compact.map(&:downcase)})
+      end
+      if not_transport_mode || not_transport_modes.length > 0
+        scope = scope.where.not({transport_mode: (not_transport_modes << not_transport_mode).compact.map(&:downcase)})
+      end
+
       scope
     end
   end

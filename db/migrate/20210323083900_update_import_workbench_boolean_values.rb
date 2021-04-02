fe348bd1be7a2e000222c0506ce0785f73e6e5c6
Luc Donnet <luc.donnet@enroute.mobi>
2021-04-02 15:23:26 +0200

    CHOUETTE-1022 Apply on public schema only in previous migration and delete all Export::NetexFull

--- db/migrate/20210323083900_update_import_workbench_boolean_values.rb
+++ db/migrate/20210323083900_update_import_workbench_boolean_values.rb
@@ -1,15 +1,19 @@
 class UpdateImportWorkbenchBooleanValues < ActiveRecord::Migration[5.2]
   def up
-    %w(automatic_merge archive_on_fail flag_urgent).each do |name|
-      Import::Workbench.where("options ->> ? = '0'", name).update_all("options = jsonb_set(options, '{#{name}}', 'false')")
-      Import::Workbench.where("options ->> ? = '1'", name).update_all("options = jsonb_set(options, '{#{name}}', 'true')")
+    on_public_schema_only do
+      %w(automatic_merge archive_on_fail flag_urgent).each do |name|
+        Import::Workbench.where("options ->> ? = '0'", name).update_all("options = jsonb_set(options, '{#{name}}', 'false')")
+        Import::Workbench.where("options ->> ? = '1'", name).update_all("options = jsonb_set(options, '{#{name}}', 'true')")
+      end
     end
   end
 
   def down
-    %w(automatic_merge archive_on_fail flag_urgent).each do |name|
-      Import::Workbench.where("options ->> ? = 'false'", name).update_all("options = jsonb_set(options, '{#{name}}', '0')")
-      Import::Workbench.where("options ->> ? = 'true'", name).update_all("options = jsonb_set(options, '{#{name}}', '1')")
+    on_public_schema_only do
+      %w(automatic_merge archive_on_fail flag_urgent).each do |name|
+        Import::Workbench.where("options ->> ? = 'false'", name).update_all("options = jsonb_set(options, '{#{name}}', '0')")
+        Import::Workbench.where("options ->> ? = 'true'", name).update_all("options = jsonb_set(options, '{#{name}}', '1')")
+      end
     end
   end
 end

e4bd3cc054ca817dd8a808a986745b9387c05706
Alban Peignier <alban.peignier@enroute.mobi>
2021-05-01 20:11:08 +0200

    CHOUETTE-710 Create extensions if needed into shared_extensions schema
    * Use schema by default in ci tests

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -9,3 +9,63 @@ class ActiveRecord::Migration
     yield if Apartment::Tenant.current != "public"
   end
 end
+
+class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+
+  # Create extension only when needed and into our shared_extensions schema
+  def enable_extension(name)
+    Extension.new(self, name).enable do
+      reload_type_map
+    end
+  end
+
+  class Extension
+
+    attr_reader :name, :adapter
+    def initialize(adapter, name)
+      @adapter, @name = adapter, name
+    end
+
+    delegate :query_value, :exec_query, :quote, to: :adapter
+
+    PLPGSQL = "plpgsql"
+
+    SCHEMA_SHARED_EXTENSIONS = "shared_extensions"
+    SCHEMA_PG_CATALOG = "pg_catalog"
+
+    def plpgsql?
+      name == PLPGSQL
+    end
+
+    def destination_schema
+      plpgsql? ? SCHEMA_PG_CATALOG : SCHEMA_SHARED_EXTENSIONS
+    end
+
+    def enabled?
+      query = <<~SQL
+      SELECT true FROM pg_catalog.pg_extension e
+      LEFT JOIN pg_catalog.pg_namespace n ON n.oid = e.extnamespace
+      where e.extname = #{quote(name)} and n.nspname = #{quote(destination_schema)}
+      SQL
+
+      query_value query
+    end
+
+    def enable(&block)
+      unless enabled?
+        enable!
+        block.call
+      end
+    end
+
+    def enable!
+      Rails.logger.info "Create extension #{name} in #{destination_schema}"
+
+      query = "CREATE EXTENSION IF NOT EXISTS \"#{name}\" WITH SCHEMA #{destination_schema};"
+      exec_query query
+    end
+
+  end
+
+
+end

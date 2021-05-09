add057f45679d352cad8e0d0a84e193594850a20
Alban Peignier <alban.peignier@enroute.mobi>
2021-05-09 23:01:09 +0200

    CHOUETTE-1105 Refactor ChangeKeyFormatForPublicationApiSource migration

--- db/migrate/20210509170255_change_key_format_for_publication_api_source.rb
+++ db/migrate/20210509170255_change_key_format_for_publication_api_source.rb
@@ -1,16 +1,20 @@
 class ChangeKeyFormatForPublicationApiSource < ActiveRecord::Migration[5.2]
   def change
-    Destination::PublicationApi.joins(:publication_setup).where('publication_setups.publish_per_line = true').each do |destination|
-      destination.publication_api.publication_api_sources.each do |publication_api_source|
-        new_key = "lines/#{publication_api_source.key}.#{publication_api_source.export.user_file.extension}"
-        publication_api_source.update_attribute(:key, new_key)
-      end
-    end
+    on_public_schema_only do
+      PublicationApiSource.joins(publication: :publication_setup).where('publication_setups.publish_per_line = true').each do |source|
+        # Skip PublicationApiSource created with new code
+        next if source.key.starts_with?("lines")
 
-    Destination::PublicationApi.joins(:publication_setup).where('publication_setups.publish_per_line = false').each do |destination|
-      destination.publication_api.publication_api_sources.each do |publication_api_source|
-        new_key = "#{publication_api_source.key}.#{publication_api_source.export.user_file.extension}"
-        publication_api_source.update_attribute(:key, new_key)
+        new_key = "lines/#{source.key}.#{source.export&.user_file&.extension || 'zip'}"
+        source.update_attribute :key, new_key
+      end
+
+      PublicationApiSource.joins(publication: :publication_setup).where('publication_setups.publish_per_line = false').each do |source|
+        # Skip PublicationApiSource created with new code
+        next if source.key.ends_with?(".zip")
+
+        new_key = "#{source.key}.#{source.export&.user_file&.extension || 'zip'}"
+        source.update_attribute :key, new_key
       end
     end
   end

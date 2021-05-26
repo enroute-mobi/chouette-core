1f61be98801c7cbc8574c2ce011ec6fdf0f9cbbe
Luc Donnet <luc.donnet@enroute.mobi>
2021-05-26 15:15:26 +0200

    CHOUETTE-1002 Delete unused Export::Workgroup class

--- spec/factories/exports/netex_generic_exports.rb
+++ spec/factories/exports/netex_generic_exports.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :netex_generic_export, class: Export::NetexGeneric, parent: :export do
     association :referential, factory: :workbench_referential
-    association :parent, factory: :workgroup_export
     options {{profile: 'european', duration: 60}}
     type {'Export::NetexGeneric'}
   end
--- spec/factories/exports/workgroup_exports.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :workgroup_export, class: Export::Workgroup, parent: :export do
-    options {{ duration: 90 }}
-    type {'Export::Workgroup'}
-    association :referential, factory: :workbench_referential
-  end
-end

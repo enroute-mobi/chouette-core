b4b4249356cad2079cf71f07746e9508319b7fb2
cnjanga <cedric.njanga@enroute.mobi>
2021-06-01 15:40:25 +0200

    disable duration input when period input value is not 'only_next_days'

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,25 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-// import jQuery from 'jquery'
-// import {} from 'jquery-ujs'
 import "@fortawesome/fontawesome-free/css/all"
 import '@ryangjchandler/spruce'
 import 'alpinejs'
-import TomSelect from 'tom-select'
-
-window.initTomSelect = (selector, config) => {
-
-  const { placeholder, multiple, minimumInputLength, ...rest } = config
-
-  const selectConfig = {
-    maxItems: multiple ? null : 1,
-    shouldLoad: query => query.length >= minimumInputLength || 0,
-    render: {
-      option: (data, escape) => '<div>' + escape(data.text) + '</div>'
-    },
-    ...rest
-  }
-
-  return new TomSelect(selector, selectConfig)
-}

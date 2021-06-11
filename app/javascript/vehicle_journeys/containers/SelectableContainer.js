07b4d0df3201705df0e2544ce662c809a7b483fe
cnjanga <cedric.njanga@enroute.mobi>
2021-06-11 19:24:51 +0200

    fix issue with dummy vehicle journey at stops

--- app/javascript/vehicle_journeys/containers/SelectableContainer.js
+++ app/javascript/vehicle_journeys/containers/SelectableContainer.js
@@ -1,15 +1,18 @@
 import actions from '../actions'
 import { connect } from 'react-redux'
-import { flatten, sortBy } from 'lodash'
+import { chain } from 'lodash'
 import SelectableContainer from '../components/SelectableContainer'
 
 const sortedVjas = vehicleJourneys => {
 	const vjasMapper = x => (vjas, y) => ({ ...vjas, x, y })
 	const vjMapper = (vj, x) => vj.vehicle_journey_at_stops.map(vjasMapper(x))
 
-	const vjas = flatten(vehicleJourneys.map(vjMapper))
-
-	return sortBy(vjas, ['y', 'x'])
+	return chain(vehicleJourneys)
+		.map(vjMapper)
+		.flatten()
+		.reject('dummy')
+		.sortBy(['y', 'x'])
+		.value()
 }
 
 const mapStateToProps = ({ selection, filters, vehicleJourneys }) => ({

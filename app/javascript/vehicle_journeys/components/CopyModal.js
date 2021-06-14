15eef6034882ab5b41c4264622034a7c70885cda
cnjanga <cedric.njanga@enroute.mobi>
2021-06-14 15:27:55 +0200

    update CopyModal component by debouncing updatePasteContent

--- app/javascript/vehicle_journeys/components/CopyModal.js
+++ app/javascript/vehicle_journeys/components/CopyModal.js
@@ -1,148 +1,144 @@
-import React, { Component } from 'react'
+import React, { useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import autoBind from 'react-autobind'
+
+import { useDebounce } from '../../helpers/hooks'
 import { CopyContent, PasteContent } from '../helpers/ClipboardHelper'
 
-export default class CopyModal extends Component {
-  constructor(props) {
-    super(props)
+const CopyModal = props => {
+  const copyContentRef = useRef(null)
+  const pasteContentRef = useRef(null)
 
-    autoBind(this)
-  }
+  const {
+    closeModal,
+    content,
+    error,
+    mode,
+    pasteContent,
+    pasteOnly,
+    toCopyMode,
+    toPasteMode,
+    visible
+  } = props
 
-  updatePasteContent() {
-    this.props.updatePasteContent(this.refs.pasteContent.value)
-  }
+  const updatePasteContent = useDebounce(
+    () => props.updatePasteContent(pasteContentRef.current.value),
+    300
+  )
 
-  selectAll() {
+  const selectAll = () => {
     if (document.body.createTextRange) { // ms
-      var range = document.body.createTextRange();
-      range.moveToElementText(this.refs.copyContent);
+      const range = document.body.createTextRange();
+      range.moveToElementText(copyContentRef.current);
       range.select();
     } else if (window.getSelection) { // moz, opera, webkit
-      var selection = window.getSelection();
-      var range = document.createRange();
-      range.selectNodeContents(this.refs.copyContent);
+      const selection = window.getSelection();
+      const range = document.createRange();
+      range.selectNodeContents(copyContentRef.current);
       selection.removeAllRanges();
       selection.addRange(range);
     }
   }
 
-  onKeyDown(event) {
-    if(!this.props.visible){ return }
+  const onKeyDown = event => {
+    if (!visible) return
 
-    if(this.props.mode == 'copy' && event.key == "a" && (event.metaKey || event.ctrlKey)){
+    if(mode == 'copy' && event.key == "a" && (event.metaKey || event.ctrlKey)){
       event.stopImmediatePropagation()
       event.preventDefault()
-      this.selectAll()
+      selectAll()
       return false
     }
   }
 
-  pasteFromClipboardAvailable() {
-    return !! (navigator.clipboard && navigator.clipboard.readText)
-  }
+  const pasteFromClipboardAvailable = () =>
+    !!(navigator.clipboard && navigator.clipboard.readText)
+  
 
-  pasteFromClipboard() {
-    let self = this
-    navigator.clipboard.readText().then(function(clipText){
-      self.props.updatePasteContent(clipText)
-    }).catch(function(err){ console.log(err) })
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if(this.props.visible){
-      if(this.props.mode == 'copy'){
-        this.selectAll()
-      }
-      else {
-        this.refs.pasteContent.focus()
-      }
+  const pasteFromClipboard = async () => {
+    try {
+      const clipText = await navigator.clipboard.readText()
+      props.updatePasteContent(clipText)
+    } catch(e) {
+      console.error(e)
     }
-    document.addEventListener("keydown", this.onKeyDown)
   }
 
-  render() {
-    const {
-      closeModal,
-      content,
-      error,
-      mode,
-      pasteContent,
-      pasteOnly,
-      toCopyMode,
-      toPasteMode,
-      visible
-    } = this.props
+  useEffect(() => {
+    if (visible){
+      mode == 'copy' ? selectAll() : pasteContentRef.current.focus()
+    }
 
-    if (!visible) return false
+    document.addEventListener("keydown", onKeyDown)
+  })
 
-    return (
-      <div>
-        <div className={'modal fade ' + (visible ? 'in' : '')} style={{ display: (visible ? 'block' : 'none') }} id='CopyModal'>
-          <div className='modal-container'>
-            <div className='modal-dialog'>
-              <div className='modal-content'>
-                <div className='modal-header'>
+  if (!visible) return false
+
+  return (
+    <div>
+      <div className={'modal fade ' + (visible ? 'in' : '')} style={{ display: (visible ? 'block' : 'none') }} id='CopyModal'>
+        <div className='modal-container'>
+          <div className='modal-dialog'>
+            <div className='modal-content'>
+              <div className='modal-header'>
+                <i className='fa fa-paste'></i>
+                <span>{ I18n.t('courses_copy_paste.modal.head') }</span>
+                <span type="button" className="close modal-close" onClick={closeModal}>&times;</span>
+              </div>
+              <div className='modal-body'>
+                {error && <div className='alert alert-danger'>
+                  { I18n.t('courses_copy_paste.errors.' + error) }
+                </div>}
+                {mode == 'copy' && <div>
+                  <pre ref={copyContentRef}>{content.copy}</pre>
+                </div>}
+                {mode == 'paste' && <div>
+                  <textarea
+                    ref={pasteContentRef}
+                    onChange={updatePasteContent}
+                  >
+                    {content.paste}
+                  </textarea>
+                  {pasteFromClipboardAvailable() && <button
+                    className="btn btn-default pull-right"
+                    onClick={pasteFromClipboard}>
+                      { I18n.t('courses_copy_paste.modal.paste_from_clipboard') }
+                  </button>}
+                  <br/>
+                </div>}
+              </div>
+              <div className='modal-footer'>
+              <button
+                className="btn btn-link"
+                onClick={closeModal}>
+                  {I18n.t('cancel')}
+              </button>
+              {mode == 'copy' && <button
+                className='btn btn-primary'
+                onClick={toPasteMode}>
                   <i className='fa fa-paste'></i>
-                  <span>{ I18n.t('courses_copy_paste.modal.head') }</span>
-                  <span type="button" className="close modal-close" onClick={closeModal}>&times;</span>
-                </div>
-                <div className='modal-body'>
-                  {error && <div className='alert alert-danger'>
-                    { I18n.t('courses_copy_paste.errors.' + error) }
-                  </div>}
-                  {mode == 'copy' && <div>
-                    <pre ref='copyContent'>{content.copy}</pre>
-                  </div>}
-                  {mode == 'paste' && <div>
-                    <textarea
-                      ref='pasteContent'
-                      onChange={this.updatePasteContent}
-                      value={content.paste}
-                    />
-                    {this.pasteFromClipboardAvailable() && <button
-                      className="btn btn-default pull-right"
-                      onClick={this.pasteFromClipboard}>
-                        { I18n.t('courses_copy_paste.modal.paste_from_clipboard') }
-                    </button>}
-                    <br/>
-                  </div>}
-                </div>
-                <div className='modal-footer'>
-                <button
-                  className="btn btn-link"
-                  onClick={closeModal}>
-                    {I18n.t('cancel')}
-                </button>
-                {mode == 'copy' && <button
-                  className='btn btn-primary'
-                  onClick={toPasteMode}>
-                    <i className='fa fa-paste'></i>
-                    <span>{ I18n.t('courses_copy_paste.modal.to_paste_mode') }</span>
-                </button>}
-                {mode == 'paste' && !pasteOnly && <button
-                  className='btn btn-default'
-                    onClick={toCopyMode}>
-                    <i className='fa fa-caret-left'></i>
-                    <span>{ I18n.t('courses_copy_paste.modal.to_copy_mode') }</span>
-                </button>}
-                {mode == 'paste' && <button
-                  className='btn btn-primary'
-                  disabled={!!error}
-                  onClick={pasteContent}>
-                    <i className='fa fa-paste'></i>
-                    <span>{ I18n.t('courses_copy_paste.modal.paste_content') }</span>
-                </button>}
-                </div>
+                  <span>{ I18n.t('courses_copy_paste.modal.to_paste_mode') }</span>
+              </button>}
+              {mode == 'paste' && !pasteOnly && <button
+                className='btn btn-default'
+                  onClick={toCopyMode}>
+                  <i className='fa fa-caret-left'></i>
+                  <span>{ I18n.t('courses_copy_paste.modal.to_copy_mode') }</span>
+              </button>}
+              {mode == 'paste' && <button
+                className='btn btn-primary'
+                disabled={!!error}
+                onClick={pasteContent}>
+                  <i className='fa fa-paste'></i>
+                  <span>{ I18n.t('courses_copy_paste.modal.paste_content') }</span>
+              </button>}
               </div>
             </div>
           </div>
         </div>
-        <div className={'modal-backdrop fade ' + (visible ? 'in' : '')} style={{ display: (visible ? 'block' : 'none') }}/>
       </div>
-    )
-  }
+      <div className={'modal-backdrop fade ' + (visible ? 'in' : '')} style={{ display: (visible ? 'block' : 'none') }}/>
+    </div>
+  )
 }
 
 CopyModal.propTypes = {
@@ -154,3 +150,5 @@ CopyModal.propTypes = {
     paste: PropTypes.instanceOf(PasteContent),
   }
 }
+
+export default CopyModal

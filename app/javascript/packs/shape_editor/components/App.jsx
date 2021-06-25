fe8df0671b5fa3521776c51b44657b97e623104d
cnjanga <cedric.njanga@enroute.mobi>
2021-06-25 10:31:10 +0200

    remove context

--- app/javascript/packs/shape_editor/components/App.jsx
+++ app/javascript/packs/shape_editor/components/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import { SWRConfig } from 'swr'
-import { ShapeContext, defaultValue } from '../shape.context'
 import ShapeEditorMap from './ShapeEditorMap'
 
 const options = {
-  fetcher: url => fetch(url).then(res => res.json())
+  fetcher: url => fetch(url).then(res => res.json()),
+  revalidateOnFocus: false
 }
 
 export default function App() {
   return (
     <SWRConfig value={options}>
-      <ShapeContext.Provider value={defaultValue}>
-        <ShapeEditorMap />
-      </ShapeContext.Provider>
+      <ShapeEditorMap />
     </SWRConfig>
   )
 }

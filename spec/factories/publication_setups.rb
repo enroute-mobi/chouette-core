50382e8aaae9496089b6f050d81fe50aad4e987d
Luc Donnet <luc.donnet@enroute.mobi>
2021-06-30 17:11:29 +0200

    CHOUETTE-1114 Delete unused export_type attribute for PublicationSetup creation

--- spec/factories/publication_setups.rb
+++ spec/factories/publication_setups.rb
@@ -3,9 +3,6 @@ FactoryBot.define do
     sequence(:name) { |n| "Publication #{n}" }
     workgroup { create(:workgroup) }
     enabled {false}
-    transient do
-      export_type { "Export::Gtfs" }
-    end
 
     export_options { {type: "Export::Gtfs", duration: 200, prefer_referent_stop_area: false, ignore_single_stop_station: false } }
 
@@ -16,25 +13,13 @@ FactoryBot.define do
 
   factory :publication_setup_gtfs, :parent => :publication_setup do
     export_options { {type: "Export::Gtfs", duration: 200, prefer_referent_stop_area: false, ignore_single_stop_station: false } }
-
-    transient do
-      export_type { "Export::Gtfs" }
-    end
   end
 
   factory :publication_setup_netex_generic, :parent => :publication_setup do
     export_options { { type: "Export::NetexGeneric", duration: 200, profile: :none } }
-
-    transient do
-      export_type { "Export::NetexGeneric" }
-    end
   end
 
   factory :publication_setup_idfm_netex_full, :parent => :publication_setup do
     export_options { { type:  "Export::Netex", export_type: :full, duration: 60} }
-
-    transient do
-      export_type { "Export::Netex" }
-    end
   end
 end

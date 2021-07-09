7e18bddfaadf63552a9749ff6e562fd88a4fe7df
Alban Peignier <alban.peignier@enroute.mobi>
2021-07-09 10:56:05 +0200

    CHOUETTE-1130 Fixes DropPurchaseWindowsTable migration
    Use a drop_table for each table

--- db/migrate/20210706093921_drop_purchase_windows_table.rb
+++ db/migrate/20210706093921_drop_purchase_windows_table.rb
@@ -1,5 +1,6 @@
 class DropPurchaseWindowsTable < ActiveRecord::Migration[5.2]
   def up
-    drop_table :purchase_windows, :purchase_windows_vehicle_journeys
+    drop_table :purchase_windows
+    drop_table :purchase_windows_vehicle_journeys
   end
 end

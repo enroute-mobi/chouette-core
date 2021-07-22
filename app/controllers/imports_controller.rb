9d0e13152f41ad2f102c67ed65cfbb9925f1597c
Luc Donnet <luc.donnet@enroute.mobi>
2021-07-22 09:57:35 +0200

    CHOUETTE-1159 Fix imports filter for workgroup

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -97,7 +97,7 @@ class ImportsController < ChouetteController
   delegate :collection, to: :search
 
   def import_params
-    permitted_keys = %i[name file type referential_id notification_target]
+    permitted_keys = %i(name file type referential_id notification_target)
     permitted_keys += Import::Workbench.options.keys
     import_params = params.require(:import).permit(permitted_keys)
     import_params[:user_id] ||= current_user.id
--- app/queries/import_query.rb
+++ app/queries/import_query.rb
@@ -6,37 +6,38 @@ class ImportQuery
 
   def self.status_group
     {
-      'pending' => ['new', 'pending', 'running'],
-      'failed' => ['failed', 'aborted', 'canceled'],
+      'pending' => %w[new pending running],
+      'failed' => %w[failed aborted canceled],
       'warning' => ['warning'],
       'successful' => ['successful']
     }
   end
 
   def find_import_statuses(values)
-    values.map{ |value| ImportQuery.status_group[value]}.flatten.compact
+    values.map { |value| ImportQuery.status_group[value] }.flatten.compact
   end
 
   def call(params = {})
-    scope = text(params[:name])
-    scope = statuses(params[:status])
+    self.scope = text(params[:name])
+    self.scope = statuses(params[:status])
   end
 
-
   def text(value)
-   return scope if value.blank?
-   self.scope = scope.where("name ILIKE ?", "%#{value}%")
+    return scope if value.blank?
+
+    scope.where('imports.name ILIKE ?', "%#{value}%")
   end
 
   def statuses(values)
-   return scope if values.blank?
+    # Use filter because rails form sends an empty string inside array [""]
+    return scope if values.filter{ |value| value.present? }.blank?
 
-   import_statuses = find_import_statuses(values)
-   self.scope = scope.having_status(import_statuses)
+    import_statuses = find_import_statuses(values)
+    scope.having_status(import_statuses)
   end
 
   def include_start_date(begin_date, end_date)
-    return scope if (begin_date.blank? && end_date.blank?)
+    return scope if begin_date.blank? && end_date.blank?
 
     if begin_date.present && end_date.present
       self.scope = scope.started_at_between(begin_date, end_date)
@@ -45,9 +46,7 @@ class ImportQuery
     elsif end_date.present?
       self.scope = scope.started_at_before(end_date)
     else
-      return scope
+      scope
     end
-
   end
-
 end

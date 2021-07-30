303c362f0b25d1b482f6e76a1a7eda332bf41f6b
cnjanga <cedric.njanga@enroute.mobi>
2021-07-30 10:56:02 +0200

    CHOUETTE-1233 update day type input to use bitwise operators

--- app/inputs/day_type_input.rb
+++ app/inputs/day_type_input.rb
@@ -7,13 +7,13 @@ class DayTypeInput < SimpleForm::Inputs::CollectionCheckBoxesInput
 
     content = ''
 
-    content << @builder.hidden_field(attribute_name, value: default_value, ':value': 'value') # relevant data is coming from this field
+    content << @builder.hidden_field(attribute_name, value: default_value, 'x-model': 'value') # relevant data is coming from this field
 
     collection.each_with_index.map do |(value, label), index|
       content << template.content_tag(:div, class: 'lcbx-group-item') do
         template.content_tag(:div, class: 'checkbox') do
           template.content_tag(:label) do
-            template.concat @builder.check_box(attribute_name, checkbox_options(value, index))
+            template.concat @builder.check_box(attribute_name, checkbox_options(value), value, value)
             template.concat template.content_tag(:span, label, class: 'lcbx-group-item-label')
           end
         end
@@ -25,24 +25,21 @@ class DayTypeInput < SimpleForm::Inputs::CollectionCheckBoxesInput
 
   def x_data
     "{
-      value: '#{default_value}', 
+      value: '#{default_value}',
       handleChange(e) {
-        const { checked, dataset: { index } } = e.target
-        const coll = this.value.split('')
-        coll[index] = checked ? '1' : '0'
-
-        this.value = coll.join('')
+        this.value = (
+          parseInt(this.value, 2) ^ parseInt(e.target.value, 2)
+        ).toString(2)
       }
     }"
   end
 
-  def checkbox_options(value, index)
+  def checkbox_options(value)
     # We remove the name & the id of the checkbox because we do not want them to send data
     {
       name: nil,
       id: nil,
-      checked: days_of_week.days.include?(value.to_sym),
-      'data-index': index,
+      checked: (default_value.to_i(2) & value.to_i(2)) != 0,
       'x-on:change': 'handleChange' # update the hidden field value on change
     }
   end
@@ -57,9 +54,24 @@ class DayTypeInput < SimpleForm::Inputs::CollectionCheckBoxesInput
 
   private
 
+=begin
+  Private representation of each week day
+  | Day       | Binary  | Integer   |
+  |---------------------------------|
+  | Monday    | '1000000' | 64      |
+  | Tuesday   | '0100000' | 32      |
+  | Wednesday | '0010000' | 16      |
+  | Thursday  | '0001000' | 8       |
+  | Friday    | '0000100' | 4       |
+  | Saturday  | '0000010' | 2       |
+  | Sunday    | '0000001' | 1       |
+=end
+
   def collection
-    Timetable::DaysOfWeek::SYMBOLIC_DAYS.map do |d|
-      [d.to_s,  Chouette::TimeTable.tmf(d)[0...2]]
+    Timetable::DaysOfWeek::SYMBOLIC_DAYS.each_with_index.map do |d, i|
+      value = '0000000'
+      value[i] = '1'
+      [value,  Chouette::TimeTable.tmf(d)[0...2]]
     end
   end
 end

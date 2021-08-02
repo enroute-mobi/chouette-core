f265e7f6c82bae654f6512e273e4c11f3cfa4ecf
Guillaume Perot <guillaume.perot@enroute.mobi>
2021-08-02 15:12:31 +0200

    CHOUETTE-1252 - wrong controller removed

---
+++ app/controllers/imports_controller.rb
@@ -0,0 +1,68 @@
+class ImportsController < ChouetteController
+  include PolicyChecker
+  include RansackDateFilter
+  include IevInterfaces
+  skip_before_action :authenticate_user!, only: [:internal_download]
+  defaults resource_class: Import::Base, collection_name: 'imports', instance_name: 'import'
+  respond_to :json, :html
+
+  def internal_download
+    resource = Import::Base.find params[:id]
+    if params[:token] == resource.token_download
+      store_file_and_clean_cache(resource)
+      send_file resource.file.path
+    else
+      user_not_authorized
+    end
+  end
+
+  def download
+    store_file_and_clean_cache(resource)
+    send_file resource.file.path, filename: resource.user_file.name, type: resource.user_file.content_type
+  end
+
+  def show
+    @import = resource.decorate(context: {parent: parent})
+    respond_to do |format|
+      format.html
+      format.json do
+        fragment = render_to_string(partial: "imports/#{@import.short_type}", formats: :html)
+        render json: {fragment: fragment}
+      end
+    end
+  end
+
+  private
+
+  def index_model
+    Import::Workbench
+  end
+
+  def resource
+    @import ||= parent.imports.find(params[:id])
+  end
+
+  def build_resource
+    @import ||= Import::Workbench.new(*resource_params) do |import|
+      import.workbench = parent
+      import.creator   = current_user.name
+    end
+  end
+
+  def import_params
+    permitted_keys = %i(name file type referential_id notification_target)
+    permitted_keys += Import::Workbench.options.keys
+    import_params = params.require(:import).permit(permitted_keys)
+    import_params[:user_id] ||= current_user.id
+    import_params
+  end
+
+  def decorate_collection(imports)
+    ImportDecorator.decorate(
+      imports,
+      context: {
+        parent: parent
+      }
+    )
+  end
+end

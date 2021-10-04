441fc0b15276ede751dade19920441525b339190
Alban Peignier <alban.peignier@enroute.mobi>
2021-10-04 14:02:59 +0200

    CHOUETTE-1372 Create Search::Operation & Query::Operation

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -113,43 +113,9 @@ class ImportsController < ChouetteController
     )
   end
 
-  class Search < Search::Base
-    # All search attributes
-    attribute :name
-    attribute :workbench_ids
-    attribute :statuses
-    attribute :start_date, type: Date
-    attribute :end_date, type: Date
-
-    def candidate_statuses
-      Operation::UserStatus.all
-    end
-
-    def period
-      Period.new(from: start_date, to: end_date).presence
-    end
-
-    validates :period, valid: true
-
-    attr_accessor :workgroup
-
-    def candidate_workbenches
-      workgroup&.workbenches || Workbench.none
-    end
-
-    def workbenches
-      candidate_workbenches.where(id: workbench_ids)
-    end
-
-    def query
-      Query::Import.new(scope).workbenches(workbenches).text(name).user_statuses(statuses).in_period(period)
-    end
-
-    class Order < ::Search::Order
-      attribute :status
-      attribute :name
-      attribute :started_at
-      attribute :creator
+  class Search < Search::Operation
+    def query_class
+      Query::Import
     end
   end
 end
--- app/lib/query/import.rb
+++ app/lib/query/import.rb
@@ -1,45 +1,4 @@
 module Query
-  class Import < Base
-    def text(value)
-      unless value.blank?
-        self.scope = scope.where 'imports.name ILIKE ?', "%#{value}%"
-      end
-
-      self
-    end
-
-    def user_statuses(user_statuses)
-      unless user_statuses.blank?
-        statuses Operation::UserStatus.find(user_statuses).flat_map(&:operation_statuses)
-      end
-
-      self
-    end
-
-    def statuses(*statuses)
-      statuses = statuses.flatten
-      unless statuses.blank?
-        self.scope = scope.having_status statuses
-      end
-
-      self
-    end
-
-    def workbenches(*workbenches)
-      workbenches = workbenches.flatten
-      unless workbenches.blank?
-        self.scope = scope.where workbench: workbenches
-      end
-
-      self
-    end
-
-    def in_period(period)
-      if period.present?
-        self.scope = scope.where started_at: period.infinite_time_range
-      end
-
-      self
-    end
+  class Import < Query::Operation
   end
 end

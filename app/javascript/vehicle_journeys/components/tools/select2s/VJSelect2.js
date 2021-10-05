b74f412a68dafc918289de08c0cd9b0f20c8cf52
cnjanga <cedric.njanga@enroute.mobi>
2021-10-05 12:06:01 +0200

    CHOUETTE-1373 improve vj select2

--- app/javascript/vehicle_journeys/components/tools/select2s/VJSelect2.js
+++ app/javascript/vehicle_journeys/components/tools/select2s/VJSelect2.js
@@ -6,6 +6,8 @@ const VJSelect2 = ({ _isFilter, onSelect2VehicleJourney, options, selectedItem }
     options={options}
     getOptionLabel={({ text }) => text}
     getOptionValue={({ id }) => id}
+    formatOptionLabel={(option, _meta) => <div dangerouslySetInnerHTML={{ __html: option.text }} />}
+    searchField={['short_id', 'published_journey_name']}
     placeholder={I18n.t('vehicle_journeys.vehicle_journeys_matrix.filters.id')}
     onChange={(selectedItem, meta) => {
       meta.action == 'select-option' && onSelect2VehicleJourney(selectedItem)

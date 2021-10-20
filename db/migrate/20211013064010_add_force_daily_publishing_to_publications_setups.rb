5c825ad29130701b26e0e48e2aa408758ee37a16
Alban Peignier <alban.peignier@enroute.mobi>
2021-10-20 10:25:45 +0200

    CHOUETTE-902 Add missing on_public_schema_only in PublicationSetups force_daily_publishing migration

--- db/migrate/20211013064010_add_force_daily_publishing_to_publications_setups.rb
+++ db/migrate/20211013064010_add_force_daily_publishing_to_publications_setups.rb
@@ -1,5 +1,7 @@
 class AddForceDailyPublishingToPublicationsSetups < ActiveRecord::Migration[5.2]
   def change
-    add_column :publication_setups, :force_daily_publishing, :boolean, default: false
+    on_public_schema_only do
+      add_column :publication_setups, :force_daily_publishing, :boolean, default: false
+    end
   end
 end

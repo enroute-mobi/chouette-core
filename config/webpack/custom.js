e603932523866e20165e06b6cf3198566a2d4df2
Luc Donnet <luc.donnet@enroute.mobi>
2021-10-20 11:17:41 +0200

    CHOUETTE-1422 Fix webpack config to include node-sass-glob-importer

---
+++ config/webpack/custom.js
@@ -0,0 +1,21 @@
+const globImporter = require('node-sass-glob-importer');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                importer: globImporter()
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
+}

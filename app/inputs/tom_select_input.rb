33e899ef611dd923d70c976b7b837d38d8e7525c
cnjanga <cedric.njanga@enroute.mobi>
2021-10-26 09:17:21 +0200

    CHOUETTE-1416 fix issue with export form

--- app/inputs/tom_select_input.rb
+++ app/inputs/tom_select_input.rb
@@ -3,21 +3,23 @@ class TomSelectInput < SimpleForm::Inputs::CollectionSelectInput
     label_method, value_method = detect_collection_methods
 
     config = options.fetch(:config, {})
-    id = "#{object.model_name.singular}_#{attribute_name}"
 
-    template.content_tag(:div) do
-      template.concat @builder.collection_select(
+    select = @builder.collection_select(
         attribute_name,
         collection,
         Proc.new { |i| i[:id] },
         Proc.new { |i| i[:text] },
         input_options,
         input_html_options.merge(
-          id: id,
           class: 'tom_selectable',
           'data-config': config.to_json
         )
       )
+
+    id = select.scan(/id="([^"]*)"/).first.first.to_s # TODO Find a better way to find the auto generated input id
+
+    template.content_tag(:div) do
+      template.concat select
 
       template.concat template.javascript_tag "initTomSelect('#{id}')"
     end

85dea69ad7ecc58024f252976f06d10f9fb40b57
cnjanga <cedric.njanga@enroute.mobi>
2021-10-27 12:05:02 +0200

    CHOUETTE-1411 fix issue with create/edit modals

--- app/packs/src/journey_patterns/components/CreateModal.js
+++ app/packs/src/journey_patterns/components/CreateModal.js
@@ -16,7 +16,7 @@ export default class CreateModal extends Component {
     if(actions.validateFields(this.refs) == true) {
       this.props.onAddJourneyPattern(_.assign({}, this.refs, {
         custom_fields: this.custom_fields,
-        shape: this.props.journeyPattern.shape ? { id: this.props.journeyPattern.shape.id, name: this.props.journeyPattern.shape.name, uuid: this.props.journeyPattern.shape.uuid } : undefined
+        shape: this.props?.journeyPattern?.shape ? { id: this.props.journeyPattern.shape.id, name: this.props.journeyPattern.shape.name, uuid: this.props.journeyPattern.shape.uuid } : undefined
        }
      ))
       this.props.onModalClose()

938c9cdffd0b244e414d1705f10bdfff6b451cca
Luc Donnet <luc.donnet@enroute.mobi>
2021-10-28 09:12:48 +0200

    CHOUETTE-1513 Load sentry configuration if environment variable SENTRY_DSN is set

--- app/packs/entrypoints/sentry.js
+++ app/packs/entrypoints/sentry.js
@@ -1,20 +1,22 @@
 import * as Sentry from "@sentry/browser";
 import { Integrations } from "@sentry/tracing";
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+if(process.env.SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
 
-  // Alternatively, use `process.env.npm_package_version` for a dynamic release version
-  // if your build tool supports it.
-  release: process.env.npm_package_version,
-  environment: process.env.SENTRY_ENVIRONMENT,
-  integrations: [new Integrations.BrowserTracing()],
+    // Alternatively, use `process.env.npm_package_version` for a dynamic release version
+    // if your build tool supports it.
+    release: process.env.npm_package_version,
+    environment: process.env.SENTRY_ENVIRONMENT,
+    integrations: [new Integrations.BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
 
-});
+  });
 
-Sentry.setTag("service", process.env.SENTRY_APP + "-" + process.env.SENTRY_CONTEXT);
+  Sentry.setTag("service", process.env.SENTRY_APP + "-" + process.env.SENTRY_CONTEXT);
+}

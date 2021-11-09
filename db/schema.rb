720b5451321dbb4d41dcceac47a51c127b011116
Luc Donnet <luc.donnet@enroute.mobi>
2021-11-09 11:20:17 +0100

    CHOUETTE-1350 Delete migration from another PR in schema.rb for route and stop points

--- db/schema.rb
+++ db/schema.rb
@@ -1166,7 +1166,6 @@ ActiveRecord::Schema.define(version: 2021_11_17_172911) do
     t.jsonb "metadata", default: {}
     t.index ["objectid"], name: "stop_points_objectid_key", unique: true
     t.index ["route_id", "position"], name: "index_stop_points_on_route_id_and_position", unique: true
-    t.index ["route_id"], name: "index_stop_points_on_route_id"
   end
 
   create_table "taggings", force: :cascade do |t|

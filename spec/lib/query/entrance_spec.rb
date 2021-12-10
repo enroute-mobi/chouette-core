fcad43cc289aa1a5d825512ba03e43e254444690
Luc Donnet <luc.donnet@enroute.mobi>
2021-12-10 10:59:31 +0100

    CHOUETTE-1574 Refactor spec to use Chouette factory and be more readable

--- spec/lib/query/entrance_spec.rb
+++ spec/lib/query/entrance_spec.rb
@@ -1,46 +1,72 @@
 RSpec.describe Query::Entrance do
-  let(:query) { Query::Entrance.new(Entrance.all) }
 
-  let(:stop_area) { create(:stop_area) }
-  let(:stop_area_provider) { stop_area.stop_area_provider }
-
-  def run_test(name, true_value, false_value)
-    base_attribute = { name: 'test', stop_area_provider_id: stop_area_provider.id }
-    first_attributes = {}.tap { |h| h[name] = true_value }
-    last_attributes = {}.tap { |h| h[name] = false_value }
-
-    first = stop_area.entrances.create(base_attribute.merge(first_attributes))
-    last = stop_area.entrances.create(base_attribute.merge(last_attributes))
-
-    scope = query.send(name, true_value).scope
-
-    expect(scope).to include(first)
-    expect(scope).not_to include(last)
+  let!(:context) do
+    Chouette.create do
+      stop_area_provider :searched_stop_area_provider do
+        stop_area :searched_stop_area do
+          entrance :searched_entrance, name: 'foo', entrance_type: 'opening', city_name: 'foo', entry_flag: true, exit_flag: true
+        end
+      end
+      stop_area_provider do
+        stop_area do
+          entrance name: 'bar', entrance_type: 'open_door', city_name: 'bar', entry_flag: false, exit_flag: false
+        end
+      end
+    end
   end
 
-  {
-    name: ['foo', 'bar'],
-    entrance_type: ['opening', 'open_door'],
-    city_name: ['foo', 'bar'],
-    entry_flag: [true, false],
-    exit_flag: [true, false],
-  }.each do |name, (true_value, false_value)|
-    describe "##{name}" do
-      it 'should return the right entrance' do
-        run_test(name, true_value, false_value)
-      end
+  let(:query) { Query::Entrance.new(Entrance.all) }
+
+  subject(:entrance) { context.entrance(:searched_entrance) }
+  let(:stop_area_provider) { context.stop_area_provider(:searched_stop_area_provider) }
+  let(:stop_area) { context.stop_area(:searched_stop_area) }
+
+  describe '#name' do
+    it 'should return the entrance with name foo' do
+      scope = query.name('foo').scope
+      expect(scope).to eq([entrance])
+    end
+  end
+
+  describe '#entrance_type' do
+    it 'should return the entrance with type opening' do
+      scope = query.entrance_type('opening').scope
+      expect(scope).to eq([entrance])
+    end
+  end
+
+  describe '#city_name' do
+    it 'should return the entrance with city_name foo' do
+      scope = query.city_name('foo').scope
+      expect(scope).to eq([entrance])
+    end
+  end
+
+  describe '#entry_flag' do
+    it 'should return the entrance with entry_flag true' do
+      scope = query.entry_flag(true).scope
+      expect(scope).to eq([entrance])
+    end
+  end
+
+  describe '#exit_flag' do
+    it 'should return the entrance with exit_flag true' do
+      scope = query.exit_flag(true).scope
+      expect(scope).to eq([entrance])
     end
   end
 
   describe '#stop_area_id' do
     it 'should return the right entrance' do
-      run_test(:stop_area_id, stop_area.id, (stop_area.id + 1))
+      scope = query.stop_area_id(stop_area.id).scope
+      expect(scope).to eq([entrance])
     end
   end
 
   describe '#stop_area_provider_id' do
     it 'should return the right entrance' do
-      run_test(:stop_area_provider_id, stop_area_provider.id, (stop_area_provider.id + 1))
+      scope = query.stop_area_provider_id(stop_area_provider.id).scope
+      expect(scope).to eq([entrance])
     end
   end
 end

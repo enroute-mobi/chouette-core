9f9fa844fae97dede54857c3a18ab51bc8b35215
cnjanga <cedric.njanga@enroute.mobi>
2021-12-22 15:25:24 +0100

    CHOUETTE-1484 add duplicate action

--- app/packs/entrypoints/macro_lists/form.js
+++ app/packs/entrypoints/macro_lists/form.js
@@ -1,5 +1,7 @@
 import Alpine from 'alpinejs'
 
+import { omit } from 'lodash'
+
 Alpine.store('macroList', {
 	selectedType: '',
 	macros: [],
@@ -8,12 +10,18 @@ Alpine.store('macroList', {
 		this.macros = [...this.macros, { ...attributes, isDeleted: false }]
 	},
 	setMacros(macros) {
-		console.log('this', this)
-		console.log('set macros', macros)
 		macros.forEach(this.addMacro.bind(this))
 	},
+	getMacro(index) {
+		return this.macros[index]
+	},
 	duplicate(index) {
+		const newMacro = {
+			...omit(this.getMacro(index), ['id', 'created_at', 'updated_at', 'position']),
+			position: this.macros.length + 1
 
+		}
+		this.macros = [...this.macros, newMacro]
 	},
 	move_up(index) {
 		if (index == 0) return
@@ -27,28 +35,27 @@ Alpine.store('macroList', {
 	},
 	send_to_top(index) {
 		this.macros = [
-			this.macros[index],
+			this.getMacro(index),
 			...this.macros.filter((_m, i) => i != index),
 		]
 	},
 	send_to_bottom(index) {
 		this.macros = [
 			...this.macros.filter((_m, i) => i != index),
-			this.macros[index]
+			this.getMacro(index)
 		]
 	},
 	delete(index) {
 		console.log('this', this)
-		this.macros[index].isDeleted = true
+		this.getMacro(index).isDeleted = true
 	},
 	restore(index) {
-		this.macros[index].isDeleted = false
+		this.getMacro(index).isDeleted = false
 	},
 	swapMacro(indexA, indexB) {
 		[this.macros[indexA], this.macros[indexB]] = [this.macros[indexB], this.macros[indexA]]
 	},
 	inputName(position, name) { return `macro_list[macros_attributes][${position}][${name}]` },
 })
-
 
 Alpine.start()

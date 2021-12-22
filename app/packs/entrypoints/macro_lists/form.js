b91f82c310069b6b4c3770acbee7c5c29e5d91cd
cnjanga <cedric.njanga@enroute.mobi>
2021-12-22 12:03:42 +0100

    CHOUETTE-1484 finish form

--- app/packs/entrypoints/macro_lists/form.js
+++ app/packs/entrypoints/macro_lists/form.js
@@ -3,43 +3,52 @@ import Alpine from 'alpinejs'
 Alpine.store('macroList', {
 	selectedType: '',
 	macros: [],
+	actions: ['duplicate', 'send_to_top', 'move_up', 'move_down', 'send_to_bottom', 'delete', 'restore'],
 	addMacro(attributes) {
 		this.macros = [...this.macros, { ...attributes, isDeleted: false }]
 	},
 	setMacros(macros) {
+		console.log('this', this)
+		console.log('set macros', macros)
 		macros.forEach(this.addMacro.bind(this))
 	},
-	removeMacro(index) {
+	duplicate(index) {
+
+	},
+	move_up(index) {
+		if (index == 0) return
+
+		this.swapMacro(index, index - 1)
+	},
+	move_down(index) {
+		if (index + 1 == this.macros.length) return
+
+		this.swapMacro(index, index + 1)
+	},
+	send_to_top(index) {
+		this.macros = [
+			this.macros[index],
+			...this.macros.filter((_m, i) => i != index),
+		]
+	},
+	send_to_bottom(index) {
+		this.macros = [
+			...this.macros.filter((_m, i) => i != index),
+			this.macros[index]
+		]
+	},
+	delete(index) {
+		console.log('this', this)
 		this.macros[index].isDeleted = true
 	},
-	restoreMacro(index) {
+	restore(index) {
 		this.macros[index].isDeleted = false
 	},
 	swapMacro(indexA, indexB) {
 		[this.macros[indexA], this.macros[indexB]] = [this.macros[indexB], this.macros[indexA]]
 	},
 	inputName(position, name) { return `macro_list[macros_attributes][${position}][${name}]` },
-	moveUp(index) {
-		if (index == 0) return
-
-		this.swapMacro(index, index - 1)
-	},
-	moveDown(index) {
-		if (index + 1 == this.macros.length) return
-
-		this.swapMacro(index, index + 1)
-	},
-	sendToTop(index) {
-		this.macros = [
-			this.macros[index],
-			...this.macros.filter((_m, i) => i != index),
-		]
-	},
-	sendToBottom(index) {
-		this.macros = [
-			...this.macros.filter((_m, i) => i != index),
-			this.macros[index]
-		]
-	}
 })
 
+
+Alpine.start()

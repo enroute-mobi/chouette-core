34c6b3345a91ad72eaf1cff275730a4472999fc5
Guillaume Perot <guillaume.perot@enroute.mobi>
2022-01-10 10:20:40 +0100

    CHOUETTE-1484 - fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -1223,7 +1223,6 @@ ActiveRecord::Schema.define(version: 2021_12_17_073653) do
     t.datetime "updated_at"
     t.jsonb "metadata", default: {}
     t.index ["objectid"], name: "stop_points_objectid_key", unique: true
-    t.index ["route_id", "position"], name: "index_stop_points_on_route_id_and_position", unique: true
     t.index ["route_id"], name: "index_stop_points_on_route_id"
   end
 

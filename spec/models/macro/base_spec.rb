dec9c3194c901c1c4ee61f59395e092edc90788f
Hai Hieu VU <haihieu.vu@enroute.mobi>
2022-01-27 18:32:21 +0100

    CHOUETTE-1699 fix optional for macro_context and macro_list

--- spec/models/macro/base_spec.rb
+++ spec/models/macro/base_spec.rb
@@ -1,7 +1,8 @@
 RSpec.describe Macro::Base do
 
   subject { Macro::Dummy.new }
-  it { is_expected.to belong_to(:macro_list).required(true) }
+  it { is_expected.to belong_to(:macro_list).required(false) }
+  it { is_expected.to belong_to(:macro_context).required(false) }
 
   describe ".table_name" do
     subject { described_class.table_name }

95349e15f989b37a659226062d9a6e67feda6e15
Guillaume Perot <guillaume.perot@enroute.mobi>
2022-02-01 18:42:40 +0100

    CHOUETTE-1531 - finish autocomplete on form, force name, update show view, update locales

--- app/controllers/line_routing_constraint_zones_controller.rb
+++ app/controllers/line_routing_constraint_zones_controller.rb
@@ -4,7 +4,7 @@ class LineRoutingConstraintZonesController < ChouetteController
 
   defaults :resource_class => LineRoutingConstraintZone
 
-  # before_action :decorate_line_routing_constraint_zone, only: %i[show new edit]
+  before_action :decorate_line_routing_constraint_zone, only: %i[show new edit]
   before_action :line_routing_constraint_zone_params, only: [:create, :update]
 
   belongs_to :workbench
@@ -26,42 +26,10 @@ class LineRoutingConstraintZonesController < ChouetteController
     end
   end
 
-  # def create
-  #   create! do |_success, failure|
-  #     failure.html do
-  #       @macro_list = LineRoutingConstraintZoneDecorator.decorate(@line_routing_constraint_zones, context: { workbench: @workbench })
-
-  #       render 'new'
-  #     end
-  #   end
-  # end
-
-  # def update
-  #    update! do |_success, failure|
-  #     failure.html do
-  #       @macro_list = LineRoutingConstraintZoneDecorator.decorate(@line_routing_constraint_zones, context: { workbench: @workbench })
-
-  #       render 'edit'
-  #     end
-  #   end
-  # end
-
-  # def show
-  #   show! do |format|
-  #     @line_routing_constraint_zone = @line_routing_constraint_zone.decorate(context: { workbench: @workbench })
-  #   end
-  # end
-
   protected
 
-  alias_method :line, :resource
+  alias_method :line_routing_constraint_zone, :resource
   alias_method :line_referential, :parent
-
-  # def build_resource
-  #   get_resource_ivar || super.tap do |line_routing_constraint_zone|
-  #     line_routing_constraint_zone.line_provider ||= @workbench.default_line_provider
-  #   end
-  # end
 
   def collection
     @line_routing_constraint_zones = parent.line_routing_constraint_zones.paginate(page: params[:page], per_page: 30)
@@ -69,35 +37,23 @@ class LineRoutingConstraintZonesController < ChouetteController
 
   private
 
-  # def decorate_line_routing_constraint_zone
-  #   object = ine_routing_constraint_zone rescue build_resource
-  #   @ine_routing_constraint_zone = LineRoutingConstraintZoneDecorator.decorate(
-  #     object,
-  #     context: {
-  #       workbench: line_referential
-  #     }
-  #   )
-  # end
-
-  # def sort_column
-  #   params[:sort].presence || 'from_name'
-  # end
-
-  # def sort_direction
-  #   %w[asc desc].include?(params[:direction]) ?  params[:direction] : 'asc'
-  # end
-
-  # def stop_area_routing_constraint_params
-  #   params.require(:stop_area_routing_constraint).permit(:from_id, :to_id, :both_way)
-  # end
+  def decorate_line_routing_constraint_zone
+    object = line_routing_constraint_zone rescue build_resource
+    @line_routing_constraint_zone = LineRoutingConstraintZoneDecorator.decorate(
+      object,
+      context: {
+        workbench: @workbench
+      }
+    )
+  end
 
   def line_routing_constraint_zone_params
     params.require(:line_routing_constraint_zone).permit(
       :name,
-      :line_ids,
-      :stop_area_ids,
       :created_at,
-      :updated_at
+      :updated_at,
+      line_ids: [],
+      stop_area_ids: [],
     )
   end
 end
--- app/decorators/line_routing_constraint_zone_decorator.rb
+++ app/decorators/line_routing_constraint_zone_decorator.rb
@@ -8,10 +8,4 @@ class LineRoutingConstraintZoneDecorator < AF83::Decorator
   with_instance_decorator do |instance_decorator|
     instance_decorator.crud
   end
-
-  define_instance_method :line_ids_options do
-    return [] unless object.line_ids
-
-    Rabl::Renderer.new('autocomplete/lines', Chouette::Line.where(id: object.line_ids), format: :hash, view_path: 'app/views').render
-  end
 end

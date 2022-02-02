4b3683ff49bcc8c2a92183b3a2c403e60ea4e337
Luc Donnet <luc.donnet@enroute.mobi>
2022-02-02 17:42:09 +0100

    CHOUETTE-1739 Use class method to find if audit is enabled

--- app/mailers/audit_mailer.rb
+++ app/mailers/audit_mailer.rb
@@ -1,10 +1,10 @@
 class AuditMailer < ApplicationMailer
-  def enabled?
+  def self.enabled?
     !!Rails.configuration.enable_automated_audits
   end
 
   def audit content
-    return unless enabled?
+    return unless self.class.enabled?
     @content = content
     mail to: Rails.configuration.automated_audits_recipients, subject: t('mailers.audit_mailer.audit.subject', date: Time.now.l, host: URI.parse(Rails.application.config.action_mailer.asset_host).host)
   end

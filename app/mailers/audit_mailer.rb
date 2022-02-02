24ff00e9a43f8f19971092dc45075927cca93d22
Luc Donnet <luc.donnet@enroute.mobi>
2022-02-02 17:32:06 +0100

    CHOUETTE-1739 Fix audit job to send really email with deliver method

--- app/mailers/audit_mailer.rb
+++ app/mailers/audit_mailer.rb
@@ -1,14 +1,10 @@
 class AuditMailer < ApplicationMailer
-  def self.enabled?
+  def enabled?
     !!Rails.configuration.enable_automated_audits
   end
 
-  def self.audit_if_enabled opts={}
-    return unless enabled?
-    audit(opts).deliver
-  end
-
   def audit content
+    return unless enabled?
     @content = content
     mail to: Rails.configuration.automated_audits_recipients, subject: t('mailers.audit_mailer.audit.subject', date: Time.now.l, host: URI.parse(Rails.application.config.action_mailer.asset_host).host)
   end

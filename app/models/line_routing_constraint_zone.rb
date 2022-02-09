954ef0af2dce958ecb006fa3664e389220d8fdaa
Alban Peignier <alban.peignier@enroute.mobi>
2022-02-09 09:56:35 +0100

    CHOUETTE-1531 Ensure LineRoutingConstraintZone has a line and a stop area

--- app/models/line_routing_constraint_zone.rb
+++ app/models/line_routing_constraint_zone.rb
@@ -1,9 +1,8 @@
 class LineRoutingConstraintZone < ApplicationModel
   include LineReferentialSupport
 
-  validates_presence_of :name
+  validates :name, :lines, :stop_areas, presence: true
 
   has_array_of :stop_areas, class_name: 'Chouette::StopArea'
   has_array_of :lines, class_name: 'Chouette::Line'
-
 end

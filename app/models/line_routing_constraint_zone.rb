b67d2e7d2c8a7b6ec99b4a285607bbd10f1936fc
Guillaume Perot <guillaume.perot@enroute.mobi>
2022-02-10 11:17:08 +0100

    CHOUETTE-1531 - fix validation

--- app/models/line_routing_constraint_zone.rb
+++ app/models/line_routing_constraint_zone.rb
@@ -1,7 +1,7 @@
 class LineRoutingConstraintZone < ApplicationModel
   include LineReferentialSupport
 
-  validates :name, :lines, :stop_areas, presence: true
+  validates :name, :line_ids, :stop_area_ids, presence: true
 
   has_array_of :stop_areas, class_name: 'Chouette::StopArea'
   has_array_of :lines, class_name: 'Chouette::Line'

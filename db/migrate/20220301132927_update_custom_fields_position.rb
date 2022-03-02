cd8353e6549ecf21ffbfe924621df06b4d7bf355
Hai Hieu VU <haihieu.vu@enroute.mobi>
2022-03-02 09:52:48 +0100

    CHOUETTE-1823 add includes in migration

--- db/migrate/20220301132927_update_custom_fields_position.rb
+++ db/migrate/20220301132927_update_custom_fields_position.rb
@@ -1,7 +1,7 @@
 class UpdateCustomFieldsPosition < ActiveRecord::Migration[5.2]
   def change
      on_public_schema_only do
-      Workgroup.find_each do |workgroup|
+      Workgroup.includes(:custom_fields).find_each do |workgroup|
         workgroup.custom_fields.group_by{|i| [i[:workgroup_id], i[:resource_type]]}.each do |k, group|
           group.each_with_index do |custom_field, index|
             custom_field.update(position: index)

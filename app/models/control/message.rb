14d6a769faf9414e9294e859f52a62acd566aba8
Alban Peignier <alban.peignier@enroute.mobi>
2022-03-03 14:26:28 +0100

    CHOUETTE-1816 Use enumerize for Control::Message#criticity
    * Remove info criticity

--- app/models/control/message.rb
+++ app/models/control/message.rb
@@ -5,6 +5,7 @@ module Control
     belongs_to :source, polymorphic: true, optional: false
     belongs_to :control_run, class_name: "Control::Base::Run", optional: false
 
-    validates_inclusion_of :criticity, in: [ "info", "warning", "error" ]
+    extend Enumerize
+		enumerize :criticity, in: %w(warning error)
   end
 end

32372f1e27004f8c36e4f2811f19e3a6ce1d1263
cnjanga <cedric.njanga@enroute.mobi>
2022-03-10 13:48:06 +0100

    CHOUETTE-1821 display control criticity in description part

--- app/packs/src/control_lists/control.js
+++ app/packs/src/control_lists/control.js
@@ -10,6 +10,42 @@ const ControlMixin = superclass => class Control extends superclass {
 		this.criticity = this.criticity || 'warning'
 	}
 	get inputSelector() { return 'controls_attributes' }
+
+	renderClassDescription(expanded, text) {
+		const domParser = new DOMParser()
+		const parse = string => domParser.parseFromString(string, 'text/html').body.childNodes[0]
+
+		let color
+
+		switch(this.criticity) {
+			case 'warning':
+				color = '#ed7f00'
+				break
+			case 'error':
+				color = '#da2f36'
+				break
+		}
+
+		const container = parse('<div class="flex w-full"></div>')
+		const criticity = parse(`<span class="fa fa-circle mr-xs" style="color:${color};"></span>`)
+		const name = parse(`<div>${this.name}</div>`)
+		const description = parse(`<div class="ml-auto">${text}</div>`)
+
+		container.appendChild(criticity)
+
+		if(Boolean(this.name) && !expanded) {
+			container.appendChild(name)
+			// description.classList.add('ml-auto')
+		}
+
+		container.appendChild(description)
+
+		return container.outerHTML
+	}
+
+	get criticityIcon() {
+		return 
+	}
 }
 
 export const Control = flow(ResourceMixin, ControlMixin)(class {})

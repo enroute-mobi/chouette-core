f605be2706887f86983fd8d5b713496d3cb4f5bb
Guillaume Perot <guillaume.perot@enroute.mobi>
2022-03-11 15:26:57 +0100

    CHOUETTE-1734 - disable email input for user edit view

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,7 +5,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:name,
-      :email,
       :password,
       :password_confirmation,
       :current_password,

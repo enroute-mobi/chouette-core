1c8629eb79fd871e700ea4c3318dfbd2c5e6d2e2
Hai Hieu VU <haihieu.vu@enroute.mobi>
2022-03-29 05:09:40 +0200

    CHOUETTE-1909 define particulars-has_many instead of a dynamic collection name

--- app/models/concerns/referent_support.rb
+++ app/models/concerns/referent_support.rb
@@ -5,17 +5,15 @@ module ReferentSupport
     belongs_to :referent, class_name: class_name
     validate :valid_referent
 
-    has_many specific_collections, class_name: class_name, foreign_key: 'referent_id'
+    has_many :particulars, class_name: class_name, foreign_key: 'referent_id'
 
     def valid_referent
       if referent_id.present? && referent?
-        errors.add(:referent_id,
-          I18n.t("#{self.class.collections}.errors.referent_id.a_referent_cannot_have_a_referent"))
+        errors.add :referent_id, :a_referent_cannot_have_a_referent
       end
 
       if referent.present? && !referent.referent?
-        errors.add(:referent_id,
-          I18n.t("#{self.class.collections}.errors.referent_id.an_object_used_as_referent_must_be_flagged_as_referent"))
+        errors.add :referent_id, :an_object_used_as_referent_must_be_flagged_as_referent
       end
     end
 
@@ -29,18 +27,6 @@ module ReferentSupport
   end
 
   module ClassMethods
-    def specific_collections
-      @specific_collections ||= case class_name
-        when 'Chouette::StopArea' then :specific_stops
-        when 'Chouette::Line' then :specific_lines
-        when 'Chouette::Company' then :specific_companies
-      end
-    end
-
-    def collections
-      @collections ||= class_name.split('::').last.underscore.pluralize
-    end
-
     def class_name
       self.name
     end

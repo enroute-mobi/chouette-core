e9412128069fc69ab860db9cad1e979f37b52f8c
Luc Donnet <luc.donnet@enroute.mobi>
2022-06-02 15:24:03 +0200

    CHOUETTE-1998 Fix policy, controler for shared Control::List

--- app/policies/control_list_policy.rb
+++ app/policies/control_list_policy.rb
@@ -5,16 +5,20 @@ class ControlListPolicy < ApplicationPolicy
     end
   end
 
+  def show
+    current_workbench.control_lists.exists?(record.id)
+  end
+
   def create?
     user.has_permission?('control_lists.create')
   end
 
   def destroy?
-    user.has_permission?('control_lists.destroy') && @current_workbench.control_lists.include?(record)
+    user.has_permission?('control_lists.destroy') && current_workbench.control_lists.exists?(record.id)
   end
 
   def update?
-    user.has_permission?('control_lists.update') && @current_workbench.control_lists.include?(record)
+    user.has_permission?('control_lists.update') && current_workbench.control_lists.exists?(record.id)
   end
 
   def execute?

533c7d86bedf36e2e493bfb2101b7ec35c8a96c7
cnjanga <cedric.njanga@enroute.mobi>
2022-06-21 10:06:30 +0200

    CHOUETTE-2015 CHOUETTE-2015 CHOUETTE-2015 add specs

---
+++ spec/models/processing_rule_spec.rb
@@ -0,0 +1,6 @@
+RSpec.describe ProcessingRule, type: :model do
+	it { should belong_to(:workbench).required }
+  it { should belong_to(:processable).polymorphic }
+	it { is_expected.to enumerize(:processable_type).in('Macro::List', 'Control::List') }
+	it { is_expected.to enumerize(:operation_step).in('after_import', 'before_merge', 'after_merge', 'after_aggregate') }
+end

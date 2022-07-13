73cf8ecd21bbfb5f62307506c2e79f37a9ebdbab
cnjanga <cedric.njanga@enroute.mobi>
2022-07-13 12:13:41 +0200

    CHOUETTE-2151 remove JS file, handle everything in simple form custom input file (AlpineJS)

--- app/inputs/per_page_input.rb
+++ app/inputs/per_page_input.rb
@@ -1,16 +1,16 @@
 class PerPageInput < SimpleForm::Inputs::StringInput
   delegate :content_tag, :concat, :link_to, :javascript_additional_packs, to: :template
 
+  # Fake input with several links + hidden input
+  # We use AlpineJS to dynamically set hidden input value + submit form when user click on link
   def input(wrapper_options)
-    javascript_additional_packs 'inputs/per_page'
-
-    content_tag(:div, class: 'flex items-center') do
-      concat link_to "30", '', class: "btn-link mx-2", style: "#{value == 30 ? 'font-weight: bold' : ''}", 'data-per_page': 30, 'x-on:click': 'onPerPageClick'
+    content_tag(:div, class: 'flex items-center', 'x-data': "{ #{attribute_name}: #{value} }", 'x-init': "$watch('#{attribute_name}', () => document.querySelector('form').submit())") do
+      concat link_to "30", '', class: "btn-link mx-2", style: "#{value == 30 ? 'font-weight: bold' : ''}", 'x-on:click.prevent': "#{attribute_name} = 30"
       concat "-"
-      concat link_to "50", '', class: "btn-link mx-2", style: "#{value == 50 ? 'font-weight: bold' : ''}", 'data-per_page': 50, 'x-on:click': 'onPerPageClick'
+      concat link_to "50", '', class: "btn-link mx-2", style: "#{value == 50 ? 'font-weight: bold' : ''}", 'x-on:click.prevent': "#{attribute_name} = 50"
       concat "-"
-      concat link_to "100", '', class: "btn-link ml-2", style: "#{value == 100 ? 'font-weight: bold' : ''}", 'data-per_page': 100, 'x-on:click': 'onPerPageClick'
-      concat @builder.hidden_field(attribute_name)
+      concat link_to "100", '', class: "btn-link ml-2", style: "#{value == 100 ? 'font-weight: bold' : ''}", 'x-on:click.prevent': "#{attribute_name} = 100" 
+      concat @builder.hidden_field(attribute_name, { 'x-model': attribute_name })
      end
   end
 

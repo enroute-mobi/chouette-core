3098971416cedd4e664401b023b2fd00159b9b54
Luc Donnet <luc.donnet@enroute.mobi>
2022-07-13 10:47:22 +0200

    CHOUETTE-2151 display per_page selection for search

--- app/inputs/per_page_input.rb
+++ app/inputs/per_page_input.rb
@@ -3,9 +3,15 @@ class PerPageInput < SimpleForm::Inputs::StringInput
 
   def input(wrapper_options)
     content_tag(:div, class: 'flex items-center') do
-       concat link_to "30", '?per_page=30', class: 'btn-link mr-2'
-       concat link_to "50", '?per_page=50', class: 'btn-link mr-2'
-       concat link_to "100", '?per_page=100', class: 'btn-link'
+       concat link_to "30", '?per_page=30', class: "btn-link mx-2", style: "#{value == 30 ? 'font-weight: bold' : ''}"
+       concat "-"
+       concat link_to "50", '?per_page=50', class: "btn-link mx-2", style: "#{value == 50 ? 'font-weight: bold' : ''}"
+       concat "-"
+       concat link_to "100", '?per_page=100', class: "btn-link ml-2", style: "#{value == 100 ? 'font-weight: bold' : ''}"
      end
+  end
+
+  def value
+    object.send(attribute_name) if object.respond_to? attribute_name
   end
 end

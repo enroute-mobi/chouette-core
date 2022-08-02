14f1f241beff303b26d3eb164686a4ed5e0cbb7e
Hai Hieu VU <haihieu.vu@enroute.mobi>
2022-08-02 08:24:29 +0200

    CHOUETTE-1935 control presence of associated models

---
+++ app/models/control/presence_associated_model.rb
@@ -0,0 +1,96 @@
+module Control
+    class PresenceAssociatedModel < Control::Base
+
+      module Options
+        extend ActiveSupport::Concern
+
+        included do
+          option :target_model
+          option :collection
+          option :minimum
+          option :maximum
+
+          enumerize :target_model, in: %w{Referential Line StopArea JourneyPattern VehicleJourney Company Route TimeTable}
+          validates :target_model, :collection, presence: true
+          validates :minimum, :maximum, numericality: { only_integer: true, allow_nil: true }
+        end
+      end
+      include Options
+
+      validate :minimum_or_maximum
+
+      def candidate_collections
+        Chouette::ModelAttribute.for(self.class.target_model.values) do
+          define Referential, :lines
+          define Referential, :routes
+          define Referential, :vehicle_journeys
+          define Referential, :time_tables
+
+          define Chouette::StopArea, :routes
+          define Chouette::StopArea, :lines
+          define Chouette::StopArea, :Entrances
+          define Chouette::StopArea, :connection_links
+
+          define Chouette::Line, :routes
+          define Chouette::Line, :secondary_companies
+
+          define Chouette::Route, :stop_points
+          define Chouette::Route, :journey_patterns
+          define Chouette::Route, :vehicle_journeys
+
+          define Chouette::JourneyPattern, :stop_points
+          define Chouette::JourneyPattern, :vehicle_journeys
+
+          define Chouette::VehicleJourney, :time_tables
+
+          define Chouette::TimeTable, :periods
+          define Chouette::TimeTable, :dates
+        end
+      end
+
+      private
+
+      def minimum_or_maximum
+        return if minimum.present? || maximum.present?
+
+        errors.add(:minimum, :invalid)
+      end
+
+      class Run < Control::Base::Run
+        include Options
+
+        def run
+          faulty_models.each do |model|
+            control_messages.create({
+              message_attributes: {
+                name: model.try(:name) || model.id,
+              },
+              criticity: criticity,
+              source: model,
+              message_key: :presence_associated_model
+            })
+          end
+        end
+
+        def faulty_models
+          context
+            .send(collection)
+            .group(target_model.underscore)
+            .having(condition, { minimum: minimum, maximum: maximum })
+            .count
+            .keys
+        end
+
+        def condition
+          if minimum.present? && maximum.present?
+            'count(*) < :minimum or count(*) > :maximum'
+          elsif minimum.present?
+            'count(*) < :minimum'
+          else
+            'count(*) > :maximum'
+          end
+        end
+      end
+    end
+  end
+  
---
+++ spec/models/control/presence_associated_model_spec.rb
@@ -0,0 +1,87 @@
+RSpec.describe Control::PresenceAssociatedModel do
+
+  describe Control::PresenceAssociatedModel::Run do
+
+    let(:control_list_run) do
+      Control::List::Run.create referential: context.referential, workbench: context.workbench
+    end
+
+    let(:control_run) do
+      Control::PresenceAssociatedModel::Run.create(
+        control_list_run: control_list_run,
+        criticity: "warning",
+        options: {
+          target_model: target_model,
+          collection: collection,
+          minimum: min,
+          maximum: max
+        },
+        position: 0
+      )
+    end
+
+    describe "#run" do
+      subject { control_run.run }
+
+      let(:context) do
+        Chouette.create do
+          shape :shape
+          referential do
+            route :route do
+              journey_pattern shape: :shape
+            end
+          end
+        end
+      end
+
+      let(:referential) { context.referential }
+
+      let(:expected_message) do
+        an_object_having_attributes({
+          source: source,
+          criticity: criticity,
+          message_attributes: {"name" => attribute_name}
+        })
+      end
+
+      before do
+        referential.switch
+      end
+
+      describe 'Routes' do
+        let(:route) { context.route(:route) }
+        let(:source) { route }
+        let(:attribute_name) { route.name }
+        let(:target_model) { 'Route' }
+
+        describe '#journey_patterns' do
+          let(:collection) { 'journey_patterns' }
+
+          let(:criticity) { 'warning' }
+
+          context 'when number of model associated is not in the range [min, max]' do
+            let(:min) { 9 }
+            let(:max) { 10 }
+
+            it 'should create warning message' do
+              subject
+
+              expect(control_run.control_messages).to include(expected_message)
+            end
+          end
+
+          context 'when number of model associated is in the range [min, max]' do
+            let(:min) { 1 }
+            let(:max) { 10 }
+
+            it 'should not create warning message' do
+              subject
+
+              expect(control_run.control_messages).to be_empty
+            end
+          end
+        end
+      end
+    end
+  end
+end

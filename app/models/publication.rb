5088af8e7b26745538af6804618d7c514bd88b39
Alban Peignier <alban.peignier@enroute.mobi>
2022-08-03 17:01:33 +0200

    CHOUETTE-2119 Use ExportBuilder to manage exports creation
    * Remove PublicationSetup which managed exports creation
    * Avoid any PublicationSetup export creation except for export options
    validation

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -27,6 +27,8 @@ class Publication < ApplicationModel
     update_columns status: :running, started_at: Time.now
   end
 
+  delegate :workgroup, to: :publication_setup
+
   %i[failed successful successful_with_warnings].each do |s|
     define_method "#{s}!" do
       update status: s, ended_at: Time.now
@@ -50,17 +52,22 @@ class Publication < ApplicationModel
   def run
     running!
     run_export
-  rescue
+  rescue => e
+    Chouette::Safe.capture "Publication ##{id} failed", e
     failed!
   end
 
+  def referential
+    parent.new
+  end
+
   def run_export
-    parent.new.switch do |referential|
+    referential.switch do
       all_synchronous = true
-      publication_setup.new_exports(referential).each do |export|
+
+      exports_builder.exports.each do |export|
         all_synchronous = all_synchronous && export.synchronous
         begin
-          export.publication = self
           Rails.logger.info "Launching export #{export.name}"
           export.save!
         rescue => e
@@ -106,5 +113,136 @@ class Publication < ApplicationModel
   def export_output
     export&.file
   end
+
+  def previous
+    publication_setup.publications.order(created_at: :desc).where.not(id: self).first
+  end
+
+  def publish_per_line?
+    publication_setup.publish_per_line
+  end
+
+  def exports_builder_class
+    publish_per_line? ? ExportBuilder::PerLine : ExportBuilder::Full
+  end
+
+  def exports_builder
+    exports_builder_class.new(self)
+  end
+
   attr_accessor :rollback
+
+  # Manage the creation of Export or Exports for the Publication
+  module ExportBuilder
+    class Base
+      def initialize(publication)
+        @publication = publication
+      end
+
+      attr_reader :publication
+      delegate :referential, :workgroup, :publication_setup, to: :publication
+
+      def build_export(line: nil)
+        attributes = export_attributes
+
+        if line
+          attributes[:name] = "#{attributes[:name]} - #{Chouette::Line.model_name.human} #{line.name}"
+          attributes[:line_ids] = [ line.id ]
+        end
+
+        publication.exports.build(attributes)
+      end
+
+      def publication_export_options
+        publication_setup.export_options
+      end
+
+      def publication_name
+        "#{Publication.model_name.human} #{publication.name}"
+      end
+
+      def export_attributes
+        publication_export_options.merge(
+          referential: referential,
+          name: publication_name,
+          creator: publication_name,
+          synchronous: true,
+          workgroup: workgroup
+        )
+      end
+    end
+
+    class Full < Base
+      def exports
+        [ build_export ]
+      end
+    end
+
+    class PerLine < Base
+      def exports
+        published_lines.map do |line|
+          freshness_attributes = {}
+          unless rollback?
+            freshness_attributes[:updated_at] = lines_status.updated_at(line)
+            freshness_attributes[:published_at] = publication_timestamps[line.id]
+          end
+
+          PublishedLine.new(self, line, freshness_attributes).export
+        end.compact
+      end
+
+      def rollback?
+        publication.rollback
+      end
+
+      def export_scope
+        @export_scope ||= Export::Scope::Options.new(referential, publication_setup.export_scope_options).scope
+      end
+
+      def published_lines
+        export_scope.lines
+      end
+
+      def lines_status
+        @lines_status ||= publication.referential.lines_status
+      end
+
+      def publication_timestamps
+        @publication_timestamps ||=
+          begin
+            # We need to use a subquery because Rails (5) fails to group.maximum on "virtual" column line_id created by select expression
+            query = "select line_id, max(created_at) from (#{publication_setup.exports.select("json_array_elements_text((options->'line_ids')::json)::int as line_id", :created_at).to_sql}) as s group by line_id"
+            database_timezone ||= Time.find_zone("UTC")
+
+            ActiveRecord::Base.connection.select_rows(query).map do |line_id, time|
+              [ line_id, database_timezone.parse(time) ]
+            end.to_h
+          end
+      end
+
+      class PublishedLine
+        def initialize(builder, line, attributes = {})
+          @builder, @line = builder, line
+          attributes.each { |k,v| send "#{k}=", v }
+        end
+        attr_reader :line, :builder
+        attr_accessor :published_at, :updated_at
+
+        delegate :publication, :build_export, to: :builder
+
+        def stale?
+          updated_at.nil? || published_at.nil? || updated_at > published_at
+        end
+
+        def export
+          unless stale?
+            Rails.logger.info "Skip export for Line ##{line.id} for Publication ##{publication.id} (updated_at: #{updated_at}, published_at: #{published_at})"
+            return
+          end
+
+          build_export line: line
+        end
+      end
+    end
+  end
 end
--- spec/observers/notifiable_operation_observer_spec.rb
+++ spec/observers/notifiable_operation_observer_spec.rb
@@ -52,34 +52,32 @@ RSpec.describe NotifiableOperationObserver do
     end
   end
 
-  [ Aggregate, NightlyAggregate ].each do |aggregate_class|
-    context "when operation is an #{aggregate_class}" do
-      let(:workgroup) { context.workgroup }
+  context "when operation is an Aggregate" do
+    let(:workgroup) { context.workgroup }
 
-      let(:operation)  { aggregate_class.new }
-      before { allow(operation).to receive(:workbench_for_notifications).and_return(workbench) }
+    let(:operation)  { Aggregate.new }
+    before { allow(operation).to receive(:workbench_for_notifications).and_return(workbench) }
 
-      let(:notification_center) { operation.workbench_for_notifications.notification_center }
+    let(:notification_center) { operation.workbench_for_notifications.notification_center }
 
-      %w{successful failed}.each do |status|
-        context "when status is #{status}" do
-          before { operation.status = status }
+    %w{successful failed}.each do |status|
+      context "when status is #{status}" do
+        before { operation.status = status }
 
-          it "sends notification via Workbench NotificationCenter" do
-            expect(notification_center).to receive(:notify).with(operation)
-            subject.after_update(operation)
-          end
+        it "sends notification via Workbench NotificationCenter" do
+          expect(notification_center).to receive(:notify).with(operation)
+          subject.after_update(operation)
         end
       end
+    end
 
-      %w{running}.each do |status|
-        context "when status is #{status}" do
-          before { operation.status = status }
+    %w{running}.each do |status|
+      context "when status is #{status}" do
+        before { operation.status = status }
 
-          it "doesn't send notification via Workbench NotificationCenter" do
-            expect(notification_center).to_not receive(:notify).with(operation)
-            subject.after_update(operation)
-          end
+        it "doesn't send notification via Workbench NotificationCenter" do
+          expect(notification_center).to_not receive(:notify).with(operation)
+          subject.after_update(operation)
         end
       end
     end

310fff559b93ca99ce3b195a6837af9f451491ff
Alban Peignier <alban.peignier@enroute.mobi>
2022-08-04 11:32:37 +0200

    CHOUETTE-2119 Remove specs on removed PublicationSetup methods

--- spec/models/publication_setup_spec.rb
+++ spec/models/publication_setup_spec.rb
@@ -1,4 +1,3 @@
-
 RSpec.describe PublicationSetup, type: :model, use_chouette_factory: true do
   it { should belong_to :workgroup }
   it { should have_many :destinations }
@@ -49,64 +48,6 @@ RSpec.describe PublicationSetup, type: :model, use_chouette_factory: true do
       let(:search_publication_setup) { PublicationSetup.create(name: "Search", workgroup: context.workgroup, publish_per_line: false, export_options: { type: 'Export::Gtfs' } ) }
       it 'should return publication setups without the publication setup in argument if it exists' do
         expect(PublicationSetup.same_api_usage(search_publication_setup)).to match_array([publication_setup3])
-      end
-    end
-  end
-
-  describe '#published_line_ids' do
-    let(:workgroup) { referential.workgroup }
-    let(:line_referential) { referential.line_referential }
-    let(:line_provider) { context.line_provider(:first_lp) }
-    let(:company) { context.company(:first_company) }
-    let(:line) { context.line(:first) }
-
-    context 'when export_options.line_ids is defined' do
-      it 'should return the selected line ids' do
-        allow(publication_setup).to receive(:export_options) {{ line_ids: line_ids }}
-        expect(publication_setup.published_line_ids(referential)).to match_array(line_ids)
-      end
-    end
-
-    context 'when export_options.company_ids is defined' do
-      it 'should return the selected company ids' do
-        line.update(company: company)
-        publication_setup.export_options = { company_ids: [company.id] }
-
-        expect(publication_setup.published_line_ids(referential)).to match_array([line.id])
-      end
-    end
-
-    context 'when export_options.line_provider_ids is defined' do
-      it 'should return the selected line provider ids' do
-        line.update(line_provider: line_provider)
-        publication_setup.export_options = { line_provider_ids: [line_provider.id] }
-
-        expect(publication_setup.published_line_ids(referential)).to match_array([line.id])
-      end
-    end
-
-    context 'when no line options is defined' do
-      it 'should return all the lines associated to publication setup\'s workgroup' do
-        publication_setup.export_options = {}
-        expect(publication_setup.published_line_ids(referential)).to match_array(publication_setup.workgroup.line_referential.lines.pluck(:id))
-      end
-    end
-  end
-
-  describe '#new_exports' do
-    context 'when published_per_line is set to true' do
-      it 'should one export per line present in the export scope' do
-        allow(publication_setup).to receive(:publish_per_line) { true }
-        exports = publication_setup.new_exports(referential)
-        expect(exports.length).to eq(line_ids.length)
-      end
-    end
-
-    context 'when published_per_line is set to false' do
-      it 'should return only one export' do
-        allow(publication_setup).to receive(:publish_per_line) { false }
-        exports = publication_setup.new_exports(referential)
-        expect(exports.length).to eq(1)
       end
     end
   end

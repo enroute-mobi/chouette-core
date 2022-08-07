694ec12f1faabccc2e4f37b3a481e80bc2cca817
Luc <luc.donnet@enroute.mobi>
2022-08-07 17:13:14 +0200

    CHOUETTE-1891 Fix specs

--- spec/models/control/model_status_spec.rb
+++ spec/models/control/model_status_spec.rb
@@ -23,15 +23,14 @@ RSpec.describe Control::ModelStatus do
     end
 
     let(:referential) { context.referential }
-    subject { control_run.run }
 
     let(:expected_message) do
       an_object_having_attributes({
         source: source,
         criticity: "warning",
         message_attributes: {
-          name: source.name,
-          expected_status: I18n.t("enumerize.expected_status.#{expected_status}")
+          "name" => source.name,
+          "expected_status" => I18n.t("enumerize.expected_status.#{expected_status}")
         },
         message_key: "model_status"
       })
@@ -39,32 +38,32 @@ RSpec.describe Control::ModelStatus do
 
     before { referential.switch }
 
-    describe "#StopArea" do
+    describe "on StopArea" do
 
       let(:target_model) { "StopArea" }
       let(:stop_area) { context.stop_area }
       let(:source) { stop_area }
 
 
-      describe "#enabled" do
+      describe "enabled" do
         let(:expected_status) { 'enabled' }
 
-        context "deleted_at is not nil" do
-          before { stop_area.update confirmed_at: DateTime.now, deleted_at: nil }
+        context "search stop areas with deleted_at is not nil" do
+          before { stop_area.update deleted_at: Date.current }
 
           it "should create a warning message" do
             control_run.run
-
+            
             expect(control_run.control_messages).to include(expected_message)
           end
         end
       end
 
-      describe "#disabled" do
+      describe "disabled" do
         let(:expected_status) { 'disabled' }
 
-        context "confirmed_at is not nil" do
-          before { stop_area.update  deleted_at: Date.current}
+        context "search stop areas with confirmed_at not nil and deleted_at is nil" do
+          before { stop_area.update  deleted_at: nil, confirmed_at: DateTime.now }
 
           it "should create a warning message" do
             control_run.run
@@ -75,16 +74,16 @@ RSpec.describe Control::ModelStatus do
       end
     end
 
-    describe "#Line" do
+    describe "on Line" do
       let(:target_model) { "Line" }
       let(:line) { referential.lines.first }
       let(:source) { line }
 
-      describe "#enabled" do
+      describe "enabled" do
         let(:expected_status) { 'enabled' }
 
-        context "when deactivated is true" do
-          before { line.update deactivated: false }
+        context "search linew with deactivated to true" do
+          before { line.update deactivated: true }
 
           it "should create a warning message" do
             control_run.run
@@ -95,11 +94,11 @@ RSpec.describe Control::ModelStatus do
 
       end
 
-      describe "#disabled" do
+      describe "disabled" do
         let(:expected_status) { 'disabled' }
 
-       context "when deactivated is false" do
-          before { line.update deactivated: true }
+       context "search lines with deactivated to false" do
+          before { line.update deactivated: false }
 
           it "should create a warning message" do
             control_run.run

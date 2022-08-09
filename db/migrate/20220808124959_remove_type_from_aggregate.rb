a340fc4d4f01a23ab1315edd09c87d16c3f5e37d
Alban Peignier <alban.peignier@enroute.mobi>
2022-08-09 09:22:20 +0200

    CHOUETTE-2175 Remove useless/invalid remove_index

--- db/migrate/20220808124959_remove_type_from_aggregate.rb
+++ db/migrate/20220808124959_remove_type_from_aggregate.rb
@@ -2,7 +2,6 @@ class RemoveTypeFromAggregate < ActiveRecord::Migration[5.2]
   def change
     on_public_schema_only do
       remove_column :aggregates, :type
-      remove_index :aggregates, :type
     end
   end
 end

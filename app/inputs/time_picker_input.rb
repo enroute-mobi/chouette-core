9c9d0e3bd0dee5d9da5b1e7cf9d3fe302afa0f7f
Alban Peignier <alban.peignier@enroute.mobi>
2022-08-10 09:37:50 +0200

    CHOUETTE-2100 Replace TimePicker by hour and minute selects

--- app/inputs/time_picker_input.rb
+++ app/inputs/time_picker_input.rb
@@ -1,32 +1,34 @@
+# frozen_string_literal: true
+
 class TimePickerInput < SimpleForm::Inputs::Base
-  delegate :content_tag, :concat, :javascript_additional_packs, to: :template
+  delegate :select_hour, :select_minute, to: :template
 
   def input
-    javascript_additional_packs 'inputs/date_picker'
+    select_hour(time_of_day, hour_options, input_html_options) +
+      select_minute(time_of_day, minute_options, input_html_options)
+  end
 
-    # Never update the code before read https://flatpickr.js.org/examples/#flatpickr--external-elements
-    input_html_options[:type] = 'text'
-    input_html_options[:data] ||= { 'input': ''}
-    input_html_options[:style] = 'background-color: white;'
+  def base_options
+    { prefix: object_name }
+  end
 
-    content_tag(:div, class: 'time_picker_block input-group') do
-      concat @builder.text_field(attribute_name, input_html_options)
-      concat( content_tag(:div, class: 'input-group-btn') do
-        concat clock_button
-      end)
-    end
+  def attribute_name_with_position(position)
+    "#{attribute_name}(#{position}i)"
+  end
+
+  def hour_options
+    base_options.merge(field_name: attribute_name_with_position(1))
+  end
+
+  def minute_options
+    base_options.merge(field_name: attribute_name_with_position(2), minute_step: 5)
+  end
+
+  def time_of_day
+    @time_of_day ||= @builder.object.send(attribute_name)
   end
 
   def input_html_classes
     super.push 'form-control'
   end
-
-  private
-
-  def clock_button
-    content_tag(:a, title: "toggle", class: 'btn btn-default color-danger', 'data-toggle': "time_picker") do
-      concat content_tag(:i, "", class: 'far fa-clock')
-    end
-  end
-
 end
--- app/models/point_of_interest/hour.rb
+++ app/models/point_of_interest/hour.rb
@@ -19,5 +19,16 @@ module PointOfInterest
 
     attribute :opening_time_of_day, TimeOfDay::Type::TimeWithoutZone.new
     attribute :closing_time_of_day, TimeOfDay::Type::TimeWithoutZone.new
+
+    %i[opening_time_of_day closing_time_of_day].each do |attribute|
+      # ?? Rails 5 ActiveRecord::AttributeAssignment .. doesn't create an object
+      # by invoke writer with multiparameter attributes (like {1 => 13, 2 => 15})
+      define_method "#{attribute}=" do |time_of_day|
+        if time_of_day.is_a?(Hash) && time_of_day.keys == [1,2]
+          time_of_day = TimeOfDay.new(time_of_day[1], time_of_day[2])
+        end
+        super time_of_day
+      end
+    end
   end
 end

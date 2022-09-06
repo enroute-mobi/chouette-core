47ce612eec7eadd2fc0a6586a20903d0cfab3134
cnjanga <cedric.njanga@enroute.mobi>
2022-09-06 16:44:34 +0200

    CHOUETTE-2202 fix cdn issue with mozilla pdf library by switch to pdfobject npm package + add specs

--- app/packs/entrypoints/documents/show.js
+++ app/packs/entrypoints/documents/show.js
@@ -1,51 +1,8 @@
 import Alpine from 'alpinejs'
+import FileRender from '../../src/documents/FileRender'
 
-class FileRender {
-	constructor(container, contentType) {
-
-		this.container = container
-		this.contentType = contentType
-
-		this.preview = {
-			width: 500,
-			height: 500
-		}
-	}
-
-	call(url) {
-		switch(this.contentType) {
-			case 'application/pdf':
-				return this.#renderPDF(url)
-			case 'image/jpeg':
-			case 'image/png':
-				return this.#renderImage(url)
-		}
-	}
-
-	async #renderPDF(url) {
-		const file = await pdfjsLib.getDocument({ url }).promise
-		const fileCover = await file.getPage(1)
-		const viewport = fileCover.getViewport({ scale: 0.8 })
-
-		// Canvas
-		const canvas = document.createElement('canvas')
-		canvas.width = this.preview.width
-		canvas.height = this.preview.height
-		const canvasContext = canvas.getContext('2d')
-		this.container.append(canvas)
-
-		fileCover.render({ canvasContext, viewport })
-	}
-
-	#renderImage(url) {
-		const img = new Image(this.preview.width, this.preview.height)
-		img.src = url
-		this.container.append(img)
-	}
-}
-
-Alpine.data('filePreview', ({ contentType, url }) => ({
+Alpine.data('filePreview', document => ({
 	async init() {
-		new FileRender(this.$el, contentType).call(url)
+		FileRender.call(this.$el, document)
 	}
 }))

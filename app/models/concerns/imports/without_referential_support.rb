913f503f2460e83124d52e1ed623485cd429155c
Alban Peignier <alban.peignier@enroute.mobi>
2022-09-07 16:25:20 +0200

    CHOUETTE-2106 Fixes file indentation

--- app/models/concerns/imports/without_referential_support.rb
+++ app/models/concerns/imports/without_referential_support.rb
@@ -1,26 +1,26 @@
 module Imports
-	module WithoutReferentialSupport
-		extend ActiveSupport::Concern
+  module WithoutReferentialSupport
+    extend ActiveSupport::Concern
 
-		# Overrides #import method to remove referential management
-		def import
-			Chouette::Benchmark.measure "import_#{import_type}", id: id do
-				update status: 'running', started_at: Time.now
+    # Overrides #import method to remove referential management
+    def import
+      Chouette::Benchmark.measure "import_#{import_type}", id: id do
+        update status: 'running', started_at: Time.now
 
-				import_without_status
+        import_without_status
 
         self.status = 'successful' if status == 'running'
         self.ended_at = Time.zone.now
-			end
-		rescue => e
-			update status: 'failed', ended_at: Time.zone.now
-			Chouette::Safe.capture "#{self.class.name} ##{id} failed", e
-		ensure
-			save
+      end
+    rescue StandardError => e
+      update status: 'failed', ended_at: Time.zone.now
+      Chouette::Safe.capture "#{self.class.name} ##{id} failed", e
+    ensure
+      save
 
-			# Invoke the freaky logic /o\
-			notify_parent
-			notify_state
-		end
-	end
+      # Invoke the freaky logic /o\
+      notify_parent
+      notify_state
+    end
+  end
 end

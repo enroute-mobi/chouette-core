82d1a6837969cea0c347daeb57f9c53c717ee6ff
Alban Peignier <alban.peignier@enroute.mobi>
2022-09-13 11:49:55 +0200

    CHOUETTE-2288 Ensure Aggregate can notify workbench users

--- app/models/concerns/notifiable_support.rb
+++ app/models/concerns/notifiable_support.rb
@@ -16,9 +16,9 @@ module NotifiableSupport
   def notification_recipients
     case notification_target.to_s
     when 'user'
-      [ user&.email ].compact
+      [user&.email].compact
     when 'workbench'
-      workbench.users.pluck(:email)
+      (try(:workbench_for_notifications) || workbench).users.pluck(:email)
     else
       []
     end

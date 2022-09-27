2f3d69c06c217b877b13bb5a2124b11174be0618
Alban Peignier <alban.peignier@enroute.mobi>
2022-09-27 10:26:27 +0200

    CHOUETTE-2362 Remove legacy Cron retrieve_all_sources

--- config/initializers/cron.rb
+++ config/initializers/cron.rb
@@ -1,2 +1,2 @@
 Cron.every_5_minutes :check_import_operations, :check_ccset_operations, :check_nightly_aggregates, :handle_dead_workers
-Cron.every_day_at_3AM :purge_workgroups, :purge_referentials, :retrieve_all_sources, :audit_referentials
+Cron.every_day_at_3AM :purge_workgroups, :purge_referentials, :audit_referentials

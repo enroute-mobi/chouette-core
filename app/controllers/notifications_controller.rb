870b3025890f2a09eaf2721f12843c71871a2ec7
Alban Peignier <alban.peignier@enroute.mobi>
2022-10-14 18:02:05 +0200

    CHOUETTE-2407 CHOUETTE-2374 Disable javascript notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,13 +1,6 @@
 class NotificationsController < ChouetteController
   def index
-    render json: {} && return unless params[:channel]
-
-    notifications = Notification.where(channel: params[:channel]).order(:created_at)
-    if params[:lastSeen] && params[:lastSeen].to_i > 0
-      notifications = notifications.where('id > ?', params[:lastSeen].to_i)
-    else
-      notifications = [notifications.last]
-    end
-    render json: notifications.compact.map(&:full_payload)
+    # See CHOUETTE-2407
+    render json: {}
   end
 end

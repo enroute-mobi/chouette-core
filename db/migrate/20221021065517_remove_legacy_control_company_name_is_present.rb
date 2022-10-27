850db1154d026d5b2412408f4f5169a98fabf971
Alban Peignier <alban.peignier@enroute.mobi>
2022-10-27 11:17:01 +0200

    CHOUETTE-93 Avoid dependency problem with ComplianceCheck by using destroy_all

--- db/migrate/20221021065517_remove_legacy_control_company_name_is_present.rb
+++ db/migrate/20221021065517_remove_legacy_control_company_name_is_present.rb
@@ -3,7 +3,7 @@
 class RemoveLegacyControlCompanyNameIsPresent < ActiveRecord::Migration[5.2]
   def up
     on_public_schema_only do
-      ComplianceCheck.where(compliance_control_name: 'CompanyControl::NameIsPresent').delete_all
+      ComplianceCheck.where(compliance_control_name: 'CompanyControl::NameIsPresent').destroy_all
       ComplianceControl.where(type: 'CompanyControl::NameIsPresent').delete_all
     end
   end

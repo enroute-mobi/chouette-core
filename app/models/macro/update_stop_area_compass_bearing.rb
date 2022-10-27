beb578c6a55ee123bc5f0e12984ad4faa38e5ab4
Alban Peignier <alban.peignier@enroute.mobi>
2022-10-27 16:37:42 +0200

    CHOUETTE-2117 CHOUETTE-2117 Remove (complex) initial join on UpdateStopAreaCompassBearing#stop_areas

--- app/models/macro/update_stop_area_compass_bearing.rb
+++ app/models/macro/update_stop_area_compass_bearing.rb
@@ -1,28 +1,24 @@
 module Macro
   class UpdateStopAreaCompassBearing < Macro::Base
-
     class Run < Macro::Base::Run
       def run
         stop_areas.find_each do |stop_area|
-          stop_area.compass_bearing = average_bearings[stop_area.id]
-          if stop_area.save
-            self.macro_messages.create(
-              criticity: "info",
-              message_attributes: { name: stop_area.name, bearing: stop_area.compass_bearing },
-              source: stop_area
+          compass_bearing = average_bearings[stop_area.id]
+          next unless compass_bearing
+
+          if stop_area.update compass_bearing: compass_bearing
+            macro_messages.create(
+              criticity: 'info', source: stop_area,
+              message_attributes: { name: stop_area.name, bearing: stop_area.compass_bearing }
             )
           else
-            self.macro_messages.create(
-              criticity: "error",
-              message_key: "invalid",
-              source: stop_area
-            )
+            macro_messages.create criticity: 'error', message_key: 'invalid', source: stop_area
           end
         end
       end
 
       def stop_areas
-        scope.stop_areas.relationship_with_shape.without_compass_bearing
+        scope.stop_areas.without_compass_bearing
       end
 
       def average_bearings

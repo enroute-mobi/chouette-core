3909a52d50e11b32ed7929b7a220ce6d1f10858c
Guillaume Perot <guillaume.perot@enroute.mobi>
2022-11-21 10:52:15 +0100

    CHOUETTE-2339 change migration (not public schema), remove unused locales

--- db/migrate/20221114153323_drop_access_links_and_access_points_table.rb
+++ db/migrate/20221114153323_drop_access_links_and_access_points_table.rb
@@ -1,8 +1,6 @@
 class DropAccessLinksAndAccessPointsTable < ActiveRecord::Migration[5.2]
-  def change
-    on_public_schema_only do
-      drop_table :access_links
-      drop_table :access_points
-    end
+  def up
+    drop_table :access_links
+    drop_table :access_points
   end
 end

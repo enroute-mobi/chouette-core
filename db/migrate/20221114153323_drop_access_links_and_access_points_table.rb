47e7aceefda1b3414002dcc244e239e9f1df8dca
Alban Peignier <alban.peignier@enroute.mobi>
2022-11-29 22:02:11 +0100

    CHOUETTE-2339 Avoid errors when access_links/access_points tables are not present

--- db/migrate/20221114153323_drop_access_links_and_access_points_table.rb
+++ db/migrate/20221114153323_drop_access_links_and_access_points_table.rb
@@ -1,6 +1,6 @@
 class DropAccessLinksAndAccessPointsTable < ActiveRecord::Migration[5.2]
   def up
-    drop_table :access_links
-    drop_table :access_points
+    drop_table :access_links, if_exists: true
+    drop_table :access_points, if_exists: true
   end
 end

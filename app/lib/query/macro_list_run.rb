f5450eed14813185da82f48b52157f86d94b3d04
Guillaume Perot <guillaume.perot@enroute.mobi>
2022-12-21 12:00:32 +0100

    CHOUETTE-2552 - update specs

--- app/lib/query/macro_list_run.rb
+++ app/lib/query/macro_list_run.rb
@@ -1,4 +1,7 @@
 module Query
   class MacroListRun < Query::Operation
+    def name(value)
+      where(value, :matches, :name)
+    end
   end
 end
--- spec/lib/query/macro_list_run_spec.rb
+++ spec/lib/query/macro_list_run_spec.rb
@@ -22,4 +22,24 @@ RSpec.describe Query::MacroListRun do
       expect(scope).to eq([macro_list_run1])
     end
   end
+
+  describe "#statuses" do
+    Macro::List::Run.status.values.each do |status|
+      context "when the queried status is #{status}" do
+        subject { query.statuses(status).scope }
+
+        it "includes imports with this status" do
+          macro_list_run1.update_column :status, status
+          is_expected.to include(macro_list_run1)
+        end
+
+        it "excludes imports without this status" do
+          other_status = (Macro::List::Run.status.values - [ status ]).first
+
+          macro_list_run1.update_column :status, other_status
+          is_expected.to_not include(macro_list_run1)
+        end
+      end
+    end
+  end
 end

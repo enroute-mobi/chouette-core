0a2e74f84a56ac186e618f535c966abf84dd0d99
Guillaume Perot <guillaume.perot@enroute.mobi>
2023-02-21 16:26:12 +0100

    CHOUETTE-2611 - fix subject translation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -7,7 +7,8 @@ class ApplicationMailer < ActionMailer::Base
   end
 
   def mail_subject(subject, options={})
-    subject_translated = !options[:translate] ? subject : t(subject, options)
+    #subject_translated = !options[:translate] ? subject : t(subject, options)
+    subject_translated = t(subject, options)
     [ subject_prefix, subject_translated ].compact.join(' ')
   end
 end

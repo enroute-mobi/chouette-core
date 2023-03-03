cc138f32854d145a3725bcdbabc61035b2c4c1e5
Hai Hieu VU <haihieu.vu@enroute.mobi>
2023-03-03 09:04:27 +0100

    CHOUETTE-2606 use I18n pluralization

--- app/helpers/aggregates_helper.rb
+++ app/helpers/aggregates_helper.rb
@@ -18,25 +18,19 @@ module AggregatesHelper
     attr_reader :metrics
 
     def decorated_metrics
-      [ vehicle_journey_count, overlapping_period_count ].compact.join(', ')
+      [ decorated_vehicle_journey_count, decorated_overlapping_period_count ].compact.join(', ')
     end
 
-    def vehicle_journey_count
+    def decorated_vehicle_journey_count
       return unless metrics['vehicle_journey_count'] > 0
 
-      [
-        metrics['vehicle_journey_count'],
-        Aggregate::Resource.tmf(metrics['vehicle_journey_count'] == 1 ? :vehicle_journey : :vehicle_journeys)
-      ].join(' ')
+      Aggregate::Resource.tmf(:vehicle_journey_count, count: metrics['vehicle_journey_count'])
     end
 
-    def overlapping_period_count
+    def decorated_overlapping_period_count
       return unless metrics['overlapping_period_count'] > 0
 
-      [
-        metrics['overlapping_period_count'],
-        Aggregate::Resource.tmf(metrics['overlapping_period_count'] == 1 ? :cleaned_period : :cleaned_periods)
-      ].join(' ')
+      Aggregate::Resource.tmf(:overlapping_period_count, count: metrics['overlapping_period_count'])
     end
   end
 end

1817d153411a6ec153aa6c6b11ebce2ef5feefcc
Alban Peignier <alban.peignier@enroute.mobi>
2023-04-14 13:18:26 +0200

    CHOUETTE-2749 Improve Control::FormatAttribute specs code style

--- spec/models/control/format_attribute_spec.rb
+++ spec/models/control/format_attribute_spec.rb
@@ -1,64 +1,67 @@
+# frozen_string_literal: true
+
 RSpec.describe Control::FormatAttribute do
-
   describe Control::FormatAttribute::Run do
-
-		it { should validate_presence_of :target_model }
-  	it { should validate_presence_of :target_attribute }
-  	it { should validate_presence_of :expected_format }
-		it do should enumerize(:target_model).in(
-      %w{Line StopArea Route JourneyPattern VehicleJourney Company Entrance PointOfInterest Document Shape Network ConnectionLink})
+    it { should validate_presence_of :target_model }
+    it { should validate_presence_of :target_attribute }
+    it { should validate_presence_of :expected_format }
+    it do
+      should enumerize(:target_model).in(
+        %w[Line StopArea Route JourneyPattern VehicleJourney Company Entrance PointOfInterest Document Shape Network
+           ConnectionLink]
+      )
     end
 
-		it 'should validate_presence of :model_attribute' do
-			valid_control_run = described_class.new target_model: 'Line', target_attribute: 'name'
+    it 'should validate_presence of :model_attribute' do
+      valid_control_run = described_class.new target_model: 'Line', target_attribute: 'name'
 
-			valid_control_run.valid?
+      valid_control_run.valid?
 
-			expect(valid_control_run.model_attribute).to be
-			expect(valid_control_run.errors.details[:model_attribute]).to be_empty
+      expect(valid_control_run.model_attribute).to be
+      expect(valid_control_run.errors.details[:model_attribute]).to be_empty
 
-			invalid_control_run = described_class.new target_model: 'Line', target_attribute: 'names'
+      invalid_control_run = described_class.new target_model: 'Line', target_attribute: 'names'
 
-			invalid_control_run.valid?
+      invalid_control_run.valid?
 
-			expect(invalid_control_run.model_attribute).to be_nil
-			expect(invalid_control_run.errors.details[:model_attribute]).not_to be_empty
-		end
+      expect(invalid_control_run.model_attribute).to be_nil
+      expect(invalid_control_run.errors.details[:model_attribute]).not_to be_empty
+    end
 
     let(:control_list_run) do
       Control::List::Run.create referential: context.referential, workbench: context.workbench
     end
 
     let(:target_attribute) { 'name' }
-		let(:expected_format) { '[BFHJ][0-9]{4,6}-[A-Z]{3}' }
+    let(:expected_format) { '[BFHJ][0-9]{4,6}-[A-Z]{3}' }
 
     let(:control_run) do
       Control::FormatAttribute::Run.create(
         control_list_run: control_list_run,
-        criticity: "warning",
+        criticity: 'warning',
         options: { target_model: target_model, target_attribute: target_attribute, expected_format: expected_format },
         position: 0
       )
     end
 
-    describe "#run" do
+    describe '#run' do
       subject { control_run.run }
 
       let(:referential) { context.referential }
 
       let(:expected_message) do
         an_object_having_attributes({
-          source: source,
-          criticity: 'warning',
-          message_attributes: {
-            'name' => source.try(:name) || source.id,
-            'expected_format' => expected_format ,
-            'target_attribute' => target_attribute
-          }
-        })
+                                      source: source,
+                                      criticity: 'warning',
+                                      message_attributes: {
+                                        'name' => source.try(:name) || source.id,
+                                        'expected_format' => expected_format,
+                                        'target_attribute' => target_attribute
+                                      }
+                                    })
       end
 
-      describe "#StopArea" do
+      describe '#StopArea' do
         let(:context) do
           Chouette.create do
             stop_area :with_a_good_name, name: 'B9999-AAA'
@@ -68,7 +71,7 @@ RSpec.describe Control::FormatAttribute do
         end
 
         before { referential.switch }
-        let(:target_model) { "StopArea" }
+        let(:target_model) { 'StopArea' }
         let(:source) { context.stop_area(:with_a_bad_name) }
         let(:stop_area_with_a_good_name) { context.stop_area(:with_a_good_name) }
 
@@ -80,7 +83,7 @@ RSpec.describe Control::FormatAttribute do
         end
       end
 
-      describe "#Entrance" do
+      describe '#Entrance' do
         let(:context) do
           Chouette.create do
             entrance :good_name, name: 'B9999-AAA'
@@ -90,7 +93,7 @@ RSpec.describe Control::FormatAttribute do
           end
         end
 
-        let(:target_model) { "Entrance" }
+        let(:target_model) { 'Entrance' }
         let(:source) { context.entrance(:bad_name) }
 
         it 'should create messages for entrances with bad attribute format' do

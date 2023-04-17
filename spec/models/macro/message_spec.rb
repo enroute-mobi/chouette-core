1b94f60069e2cc5bf4368aea003ba450921108a8
Alban Peignier <alban.peignier@enroute.mobi>
2023-04-17 23:40:45 +0200

    CHOUETTE-2768 Create target_model without loading source

--- spec/models/macro/message_spec.rb
+++ spec/models/macro/message_spec.rb
@@ -1,6 +1,26 @@
+# frozen_string_literal: true
+
 RSpec.describe Macro::Message do
-  describe ".table_name" do
+  let(:message) { Macro::Message.new }
+
+  describe '.table_name' do
     subject { described_class.table_name }
-    it { is_expected.to eq("public.macro_messages") }
+    it { is_expected.to eq('public.macro_messages') }
+  end
+
+  describe '#i18n_target_model' do
+    subject { message.i18n_target_model }
+
+    context 'when source type is not defined' do
+      before { message.source_type = nil }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when source type is "Chouette::VehicleJourney"' do
+      before { message.source_type = 'Chouette::VehicleJourney' }
+
+      it { is_expected.to eq 'course' }
+    end
   end
 end

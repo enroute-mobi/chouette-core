62653f3bf995b74f07dff5fb003617fae82be112
Hai Hieu VU <haihieu.vu@enroute.mobi>
2023-05-17 16:55:29 +0200

    CHOUETTE-2849 Add Document Provider short name

--- app/models/document_provider.rb
+++ app/models/document_provider.rb
@@ -4,4 +4,5 @@ class DocumentProvider < ActiveRecord::Base
   has_many :documents
 	
 	validates :name, presence: true
+	validates :short_name, presence: true, uniqueness: { scope: :workbench }, format: { with: %r{\A[0-9a-zA-Z_]+\Z} }
 end
--- spec/models/document_provider_spec.rb
+++ spec/models/document_provider_spec.rb
@@ -2,4 +2,21 @@ RSpec.describe DocumentProvider, type: :model do
   it { should belong_to(:workbench).required }
   it { should have_many :documents }
   it { should validate_presence_of :name }
+
+  describe '#short_name' do
+    let(:workbench) { create(:workbench) }
+    subject { DocumentProvider.create short_name: short_name, name: 'doc provider', workbench: workbench }
+
+    context "when short name is not in the validation format" do
+      let(:short_name) { 'INVALID SHORT NAME' }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context "when short name is in the validation format" do
+      let(:short_name) { 'VALID_SHORT_NAME' }
+
+      it { is_expected.to be_valid }
+    end
+  end
 end

bb50988a1ed45b789be702c5a8e40fc9412e22a5
Hai Hieu VU <haihieu.vu@enroute.mobi>
2023-05-23 11:44:32 +0200

    CHOUETTE-2851 Fix code indent

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -11,36 +11,36 @@ class Api::V1::DocumentsController < Api::V1::WorkbenchController
 
   private
 
-	def document_provider
-		current_workbench.document_providers.find_by(short_name: document_params[:document_provider]) ||
-		current_workbench.default_document_provider
+  def document_provider
+    current_workbench.document_providers.find_by(short_name: document_params[:document_provider]) ||
+    current_workbench.default_document_provider
   end
 
-	def document_attributes
-		document_params
-			.except(:document_provider)
-			.with_defaults(codes: [])
-			.tap do |document_params|
-				document_params[:codes].each do |code|
-					code[:code_space_id] = current_workbench.workgroup.code_spaces.find_by(short_name: code.delete(:code_space))&.id
-				end
+  def document_attributes
+    document_params
+      .except(:document_provider)
+      .with_defaults(codes: [])
+      .tap do |document_params|
+        document_params[:codes].each do |code|
+          code[:code_space_id] = current_workbench.workgroup.code_spaces.find_by(short_name: code.delete(:code_space))&.id
+        end
 
-				document_params[:codes_attributes] = document_params.delete(:codes)
-				document_params[:document_type_id] = current_workbench.workgroup.document_types.find_by(short_name:  document_params.delete('document_type'))&.id
-			end
-	end
+        document_params[:codes_attributes] = document_params.delete(:codes)
+        document_params[:document_type_id] = current_workbench.workgroup.document_types.find_by(short_name:  document_params.delete('document_type'))&.id
+      end
+  end
 
-	def document_params
-		params
-			.require(:document)
-			.permit(
-				:name,
-				:description,
-				:file,
-				:document_type,
-				:document_provider,
-				validity_period: [:from, :to],
-				codes: [:code_space, :value],
-			)
+  def document_params
+    params
+      .require(:document)
+      .permit(
+        :name,
+        :description,
+        :file,
+        :document_type,
+        :document_provider,
+        validity_period: [:from, :to],
+        codes: [:code_space, :value],
+      )
   end
 end
--- spec/models/document_provider_spec.rb
+++ spec/models/document_provider_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe DocumentProvider, type: :model do
     let(:workbench) { create(:workbench) }
     subject { DocumentProvider.create short_name: short_name, name: 'doc provider', workbench: workbench }
 
-    context "when short name is not in the validation format" do
+    context 'when short name is not in the validation format' do
       let(:short_name) { 'INVALID SHORT NAME' }
 
       it { is_expected.not_to be_valid }
     end
 
-    context "when short name is in the validation format" do
+    context 'when short name is in the validation format' do
       let(:short_name) { 'VALID_SHORT_NAME' }
 
       it { is_expected.to be_valid }

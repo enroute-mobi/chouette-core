b51a133ad65b5049b50bae9ee35b26b42343be94
Guillaume Perot <guillaume.perot@enroute.mobi>
2023-07-20 11:39:04 +0200

    CHOUETTE-2990 - update migration and schema

--- db/migrate/20230718115813_add_defered_constraint_on_controls.rb
+++ db/migrate/20230718115813_add_defered_constraint_on_controls.rb
@@ -1,9 +1,6 @@
 class AddDeferedConstraintOnControls < ActiveRecord::Migration[5.2]
   def up
     on_public_schema_only do
-      remove_index :controls, name: :index_controls_position
-      add_index :controls, %i[control_list_id control_context_id position], unique: true, name: 'index_controls_position'
-
       execute <<-SQL
         ALTER TABLE controls ADD CONSTRAINT index_controls_position UNIQUE USING INDEX index_controls_position DEFERRABLE INITIALLY DEFERRED;
       SQL
@@ -15,7 +12,6 @@ class AddDeferedConstraintOnControls < ActiveRecord::Migration[5.2]
       execute <<-SQL
         ALTER TABLE controls DROP CONSTRAINT index_controls_position;
       SQL
-      add_index :controls, %i[control_list_id position], unique: true
     end
   end
 end

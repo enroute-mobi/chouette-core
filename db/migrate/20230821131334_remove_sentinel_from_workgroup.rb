d05174cca82ab28799b164b3ece92741b077e583
Hai Hieu VU <haihieu.vu@enroute.mobi>
2023-08-25 10:03:45 +0200

    CHOUETTE-3016 delete the NotificationRules which use the hole_sentinel notifycation_type

--- db/migrate/20230821131334_remove_sentinel_from_workgroup.rb
+++ db/migrate/20230821131334_remove_sentinel_from_workgroup.rb
@@ -1,6 +1,8 @@
 class RemoveSentinelFromWorkgroup < ActiveRecord::Migration[5.2]
   def change
     on_public_schema_only do
+      NotificationRule.where(notification_type: 'hole_sentinel').delete_all
+
       remove_column :workgroups, :sentinel_delay
       remove_column :workgroups, :sentinel_min_hole_size
     end

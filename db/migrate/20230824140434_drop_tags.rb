59c648d1ff566c5ff7252921d2382250a70d34cd
Alban Peignier <alban.peignier@enroute.mobi>
2023-08-25 15:22:49 +0200

    CHOUETTE-3061 Remove tags/taggings tables in every schema

--- db/migrate/20230824140434_drop_tags.rb
+++ db/migrate/20230824140434_drop_tags.rb
@@ -1,8 +1,6 @@
 class DropTags < ActiveRecord::Migration[5.2]
   def change
-    on_public_schema_only do
-      drop_table :tags
-      drop_table :taggings
-    end
+    drop_table :tags
+    drop_table :taggings
   end
 end

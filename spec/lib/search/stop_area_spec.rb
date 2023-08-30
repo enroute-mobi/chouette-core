cbabf933f5dc1d6f5ecf39e840471d73532ccef9
Hai Hieu VU <haihieu.vu@enroute.mobi>
2023-08-30 11:27:37 +0200

    CHOUETTE-3042 add saved_search partial

--- spec/lib/search/stop_area_spec.rb
+++ spec/lib/search/stop_area_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Search::StopArea do
   let(:scope) { Chouette::StopArea.where(name: ['1', '2', '3']) }
 
   context 'search with pagination' do
-    subject { Search::StopArea.new(scope, search: { per_page: 2, page: 1 }).collection }
+    subject { described_class.new(scope, search: { per_page: 2, page: 1 }).collection }
 
     it 'returns first 2 stop areas from the given scope' do
       is_expected.to match_array([first, second])
@@ -23,7 +23,7 @@ RSpec.describe Search::StopArea do
   end
 
   context 'search without pagination' do
-    subject { Search::StopArea.new(scope, search: { per_page: 2, page: 1 }).without_pagination.collection }
+    subject { described_class.new(scope, search: { per_page: 2, page: 1 }).without_pagination.collection }
 
     it 'returns all stop areas from the given scope' do
       is_expected.to match_array([first, second, last])
@@ -31,7 +31,7 @@ RSpec.describe Search::StopArea do
   end
 
   context 'search with order and pagination' do
-    subject { Search::StopArea.new(scope, search: { per_page: 2, page: 1, order: { name: :desc } }).collection }
+    subject { described_class.new(scope, search: { per_page: 2, page: 1, order: { name: :desc } }).collection }
 
     it 'returns 2 stop areas with order for the name attribute from the given scope' do
       is_expected.to eq [last, second]
@@ -39,7 +39,7 @@ RSpec.describe Search::StopArea do
   end
 
   context 'search with order' do
-    subject { Search::StopArea.new(scope, search: { order: { name: :desc } }).collection }
+    subject { described_class.new(scope, search: { order: { name: :desc } }).collection }
 
     it 'returns all stop areas without order for the name attribute from the given scope' do
       is_expected.to eq [last, second, first]
@@ -48,7 +48,7 @@ RSpec.describe Search::StopArea do
 
   context 'search without order and without pagination' do
     subject do
-      Search::StopArea.new(
+      described_class.new(
         scope,
         search: { per_page: 2, page: 1, order: { name: :desc } }
       ).without_order.without_pagination.collection

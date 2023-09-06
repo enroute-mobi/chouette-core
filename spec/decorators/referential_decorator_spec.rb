c1594cd0ac163e9df7b30e13f608233f3442e74e
Alban Peignier <alban.peignier@enroute.mobi>
2023-09-06 08:43:00 +0200

    CHOUETTE-3083 Fixes (ugly) specs

--- spec/decorators/referential_decorator_spec.rb
+++ spec/decorators/referential_decorator_spec.rb
@@ -1,25 +1,24 @@
-RSpec.describe ReferentialDecorator, type: [:helper, :decorator] do
+RSpec.describe ReferentialDecorator, type: %i[helper decorator] do
   include Support::DecoratorHelpers
 
-  let( :workbench ){ build_stubbed :workbench }
-  let( :object ){ build_stubbed :referential, workbench: workbench }
-  let( :referential ){ object }
-  let( :user ){ build_stubbed :user }
+  let(:workbench) { build_stubbed :workbench }
+  let(:object) { build_stubbed :referential, workbench: workbench }
+  let(:referential) { object }
+  let(:user) { build_stubbed :user }
 
   describe 'delegation' do
     it 'delegates all' do
-      %i{xx xxx anything save!}.each do |method|
-        expect( object ).to receive(method)
+      %i[xx xxx anything save!].each do |method|
+        expect(object).to receive(method)
       end
       # Almost as powerful as Quicktest :P
-      %i{xx xxx anything save!}.each do |method|
+      %i[xx xxx anything save!].each do |method|
         subject.send method
       end
     end
   end
 
   describe 'action links for' do
-
     context 'unarchived referential' do
       context 'no rights' do
         it 'has only show and Calendar actions' do
@@ -28,79 +27,87 @@ RSpec.describe ReferentialDecorator, type: [:helper, :decorator] do
       end
 
       context 'all rights and different organisation' do
-
-        let( :user ){ build_stubbed :allmighty_user }
+        let(:user) { build_stubbed :allmighty_user }
 
         it 'has only default actions' do
-          expect_action_link_elements.to match_array ["Consulter", "Calendriers", "Dupliquer"]
+          expect_action_link_elements.to match_array %w[Consulter Calendriers Dupliquer]
           expect_action_link_hrefs.to match_array([
-            [object],
-            referential_time_tables_path(object),
-            new_workbench_referential_path(referential.workbench, from: object.id)
-          ])
+                                                    [object],
+                                                    referential_time_tables_path(object),
+                                                    new_workbench_referential_path(referential.workbench,
+                                                                                   from: object.id)
+                                                  ])
         end
       end
       context 'all rights and same organisation' do
-        let( :user ){ build_stubbed :allmighty_user, organisation: referential.organisation }
-        let( :action){ :index }
-        context "on index" do
+        let(:user) { build_stubbed :allmighty_user, organisation: referential.organisation }
+        let(:action) { :index }
+        context 'on index' do
           it 'has corresponding actions' do
-            expect_action_link_elements(action).to match_array ["Consulter", "Editer ce jeu de données", "Calendriers", "Dupliquer", "Contrôler", "Archiver","Supprimer ce jeu de données"]
+            expect_action_link_elements(action).to match_array ['Consulter', 'Editer ce jeu de données', 'Calendriers',
+                                                                'Dupliquer', 'Contrôler', 'Archiver', 'Supprimer ce jeu de données']
             expect_action_link_hrefs(action).to match_array([
-              [object],
-              [:edit, object],
-              referential_time_tables_path(object),
-              new_workbench_referential_path(referential.workbench, from: object.id),
-              select_compliance_control_set_referential_path(object),
-              archive_referential_path(object),
-              [object]
-            ])
+                                                              [object],
+                                                              [:edit, object],
+                                                              referential_time_tables_path(object),
+                                                              new_workbench_referential_path(referential.workbench,
+                                                                                             from: object.id),
+                                                              new_workbench_control_list_run_path(
+                                                                referential.workbench, referential_id: object.id
+                                                              ),
+                                                              archive_referential_path(object),
+                                                              [object]
+                                                            ])
           end
         end
 
-        context "on show" do
-          let( :action){ :show }
+        context 'on show' do
+          let(:action) { :show }
           it 'has corresponding actions' do
-            expect_action_link_elements(action).to match_array ["Courses", "Editer ce jeu de données", "Calendriers", "Dupliquer", "Contrôler", "Archiver", "Nettoyer", "Supprimer ce jeu de données"]
+            expect_action_link_elements(action).to match_array ['Courses', 'Editer ce jeu de données', 'Calendriers',
+                                                                'Dupliquer', 'Contrôler', 'Archiver', 'Nettoyer', 'Supprimer ce jeu de données']
             expect_action_link_hrefs(action).to match_array([
-              [:edit, object],
-              referential_vehicle_journeys_path(object),
-              referential_time_tables_path(object),
-              new_workbench_referential_path(referential.workbench, from: object.id),
-              select_compliance_control_set_referential_path(object),
-              archive_referential_path(object),
-              new_referential_clean_up_path(object),
-              [object]
-            ])
+                                                              [:edit, object],
+                                                              referential_vehicle_journeys_path(object),
+                                                              referential_time_tables_path(object),
+                                                              new_workbench_referential_path(referential.workbench,
+                                                                                             from: object.id),
+                                                              new_workbench_control_list_run_path(
+                                                                referential.workbench, referential_id: object.id
+                                                              ),
+                                                              archive_referential_path(object),
+                                                              new_referential_clean_up_path(object),
+                                                              [object]
+                                                            ])
           end
         end
 
         context 'with a failed referential' do
-          before{
+          before do
             referential.ready = false
             referential.failed_at = Time.now
-          }
-          context "on index" do
+          end
+          context 'on index' do
             it 'has corresponding actions' do
               expect_action_link_elements(action).to match_array [
-                "Consulter", "Supprimer ce jeu de données"
+                'Consulter', 'Supprimer ce jeu de données'
               ]
               expect_action_link_hrefs(action).to match_array([
-                [object],
-                [object]
-              ])
+                                                                [object],
+                                                                [object]
+                                                              ])
             end
           end
 
-          context "on show" do
-            let( :action){ :show }
+          context 'on show' do
+            let(:action) { :show }
             it 'has corresponding actions' do
               expect_action_link_elements(action).to eq [
-                "Supprimer ce jeu de données"
+                'Supprimer ce jeu de données'
               ]
               expect_action_link_hrefs(action).to eq([
-                [object]
-              ])
+                                                       [object]
+                                                     ])
             end
           end
         end
@@ -108,10 +115,10 @@ RSpec.describe ReferentialDecorator, type: [:helper, :decorator] do
     end
 
     context 'archived referential' do
-      before {
+      before do
         referential.ready = true
         referential.archived_at = 42.seconds.ago
-      }
+      end
       context 'no rights' do
         it 'has only show and calendar actions' do
           expect_action_link_hrefs.to match_array([[object], referential_time_tables_path(object)])
@@ -119,38 +126,41 @@ RSpec.describe ReferentialDecorator, type: [:helper, :decorator] do
       end
 
       context 'all rights and different organisation' do
-        let( :user ){ build_stubbed :allmighty_user }
+        let(:user) { build_stubbed :allmighty_user }
         it 'has only default actions' do
-          expect_action_link_elements.to match_array ["Consulter", "Calendriers", "Dupliquer"]
+          expect_action_link_elements.to match_array %w[Consulter Calendriers Dupliquer]
           expect_action_link_hrefs.to match_array([
-            [object],
-            referential_time_tables_path(object),
-            new_workbench_referential_path(referential.workbench, from: object.id)
-          ])
+                                                    [object],
+                                                    referential_time_tables_path(object),
+                                                    new_workbench_referential_path(referential.workbench,
+                                                                                   from: object.id)
+                                                  ])
         end
       end
 
       context 'all rights and same organisation' do
-        let( :user ){ build_stubbed :allmighty_user, organisation: referential.organisation }
+        let(:user) { build_stubbed :allmighty_user, organisation: referential.organisation }
         it 'has only default actions' do
-          expect_action_link_elements.to match_array ["Consulter", "Calendriers", "Dupliquer", "Désarchiver", "Supprimer ce jeu de données"]
+          expect_action_link_elements.to match_array ['Consulter', 'Calendriers', 'Dupliquer', 'Désarchiver',
+                                                      'Supprimer ce jeu de données']
           expect_action_link_hrefs.to match_array([
-            [object],
-            referential_time_tables_path(object),
-            new_workbench_referential_path(referential.workbench, from: object.id),
-            unarchive_referential_path(object),
-            [object]
-          ])
+                                                    [object],
+                                                    referential_time_tables_path(object),
+                                                    new_workbench_referential_path(referential.workbench,
+                                                                                   from: object.id),
+                                                    unarchive_referential_path(object),
+                                                    [object]
+                                                  ])
         end
       end
     end
 
     context 'finalized offer' do
-      before {
+      before do
         referential.ready = true
         referential.failed_at = nil
         referential.referential_suite_id = 1
-      }
+      end
       context 'no rights' do
         it 'has only show and calendar actions' do
           expect_action_link_hrefs.to match_array([[object], referential_time_tables_path(object)])
@@ -158,25 +168,26 @@ RSpec.describe ReferentialDecorator, type: [:helper, :decorator] do
       end
 
       context 'all rights and different organisation' do
-        let( :user ){ build_stubbed :allmighty_user }
+        let(:user) { build_stubbed :allmighty_user }
         it 'has only default actions' do
-          expect_action_link_elements.to match_array ["Consulter", "Calendriers"]
+          expect_action_link_elements.to match_array %w[Consulter Calendriers]
           expect_action_link_hrefs.to match_array([
-            [object],
-            referential_time_tables_path(object),
-          ])
+                                                    [object],
+                                                    referential_time_tables_path(object)
+                                                  ])
         end
       end
 
       context 'all rights and same organisation' do
-        let( :user ){ build_stubbed :allmighty_user, organisation: referential.organisation }
+        let(:user) { build_stubbed :allmighty_user, organisation: referential.organisation }
         it 'has only default actions' do
-          expect_action_link_elements.to match_array ["Consulter", "Calendriers", "Contrôler"]
+          expect_action_link_elements.to match_array %w[Consulter Calendriers Contrôler]
           expect_action_link_hrefs.to match_array([
-            [object],
-            referential_time_tables_path(object),
-            select_compliance_control_set_referential_path(object)
-          ])
+                                                    [object],
+                                                    referential_time_tables_path(object),
+                                                    new_workbench_control_list_run_path(referential.workbench,
+                                                                                        referential_id: object.id)
+                                                  ])
         end
       end
     end
--- spec/helpers/table_builder_helper_spec.rb
+++ spec/helpers/table_builder_helper_spec.rb
@@ -93,7 +93,7 @@ describe TableBuilderHelper, type: :helper do
                         <ul class="other">
                             <li class=""><a href="/referentials/#{referential.id}/time_tables">Calendriers</a></li>
                             <li class=""><a href="/workbenches/#{workbench.id}/referentials/new?from=#{referential.id}">Dupliquer</a></li>
-                            <li class=""><a href="/referentials/#{referential.id}/select_compliance_control_set">Contrôler</a></li>
+                            <li class=""><a href="/workbenches/#{workbench.id}/control_list_runs/new?referential_id=#{referential.id}">Contrôler</a></li>
                             <li class=""><a rel="nofollow" data-method="put" href="/referentials/#{referential.id}/archive">Archiver</a></li>
                         </ul>
                         <ul class="footer">

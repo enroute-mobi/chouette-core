40adb5a3943dd4e297dd9b339b713fb655c240a6
Alban Peignier <alban.peignier@enroute.mobi>
2023-12-04 14:02:25 +0100

    CHOUETTE-3215 Use Chouette::Factory to create ServiceCount specs contexts

--- spec/models/service_count_spec.rb
+++ spec/models/service_count_spec.rb
@@ -1,63 +1,44 @@
 # frozen_string_literal: true
 
 RSpec.describe ServiceCount, type: :model do
-  let(:line_referential) { create(:line_referential) }
-  let(:workbench) { create(:workbench, line_referential: line_referential) }
-  let(:line) { create(:line, line_referential: line_referential) }
-  let(:line2) { create(:line, line_referential: line_referential) }
-  let(:route) { create(:route, line: line) }
-  let(:route2) { create(:route, line: line2) }
-  let(:journey_pattern) { create(:journey_pattern, route: route) }
-  let(:journey_pattern2) { create(:journey_pattern, route: route2) }
+  describe '.compute_for_referential' do
+    let(:context) do
+      Chouette.create do
+        referential do
+          time_table :time_table_a,
+                     int_day_types: Cuckoo::Timetable::DaysOfWeek::SATURDAY | Cuckoo::Timetable::DaysOfWeek::SUNDAY,
+                     periods: [Period.parse('2030-01-07..2030-01-20')]
 
-  let(:period_start) { Date.parse('2030-01-07') }
-  let(:period_end)   { Date.parse('2030-01-27') }
-  let(:metadatas1) do
-    create(:referential_metadata, lines: line_referential.lines, periodes: [period_start..period_end.prev_day])
-  end
-  let(:metadatas2) do
-    create(:referential_metadata, lines: line_referential.lines, periodes: [period_start.next..period_end])
-  end
-  let(:referential) { create(:workbench_referential, workbench: workbench, metadatas: [metadatas1, metadatas2]) }
+          time_table :time_table_b, periods: [Period.parse('2030-01-14..2030-01-27')]
 
-  let(:lines) { [line, line2] }
+          route do
+            journey_pattern :journey_pattern_1 do
+              vehicle_journey time_tables: [:time_table_a]
+              vehicle_journey time_tables: [:time_table_a]
+              vehicle_journey time_tables: %i[time_table_a time_table_b]
+              vehicle_journey time_tables: [:time_table_b]
+            end
+          end
 
-  before do
-    lines
-    referential.switch
-  end
-
-  describe '#compute_for_referential' do
-    let(:time_table_a) do
-      build(
-        :time_table,
-        int_day_types: Cuckoo::Timetable::DaysOfWeek::SATURDAY | Cuckoo::Timetable::DaysOfWeek::SUNDAY
-      ).tap do |t|
-        t.periods.new(period_start: Date.parse('2030-01-07'), period_end: Date.parse('2030-01-20'))
-        t.save!
+          route do
+            journey_pattern :journey_pattern_2 do
+              vehicle_journey time_tables: [:time_table_a]
+            end
+          end
+        end
       end
     end
-    let(:time_table_b) do
-      build(:time_table, int_day_types: Cuckoo::Timetable::DaysOfWeek::EVERYDAY).tap do |t|
-        t.periods.new(period_start: Date.parse('2030-01-14'), period_end: Date.parse('2030-01-27'))
-        t.save!
-      end
-    end
-    let!(:vehicle_journey1) do
-      create(:vehicle_journey, journey_pattern: journey_pattern, time_tables: [time_table_a])
-    end
-    let!(:vehicle_journey2) do
-      create(:vehicle_journey, journey_pattern: journey_pattern, time_tables: [time_table_a])
-    end
-    let!(:vehicle_journey3) do
-      create(:vehicle_journey, journey_pattern: journey_pattern, time_tables: [time_table_a, time_table_b])
-    end
-    let!(:vehicle_journey4) do
-      create(:vehicle_journey, journey_pattern: journey_pattern, time_tables: [time_table_b])
-    end
-    let!(:vehicle_journey_other) do
-      create(:vehicle_journey, journey_pattern: journey_pattern2, time_tables: [time_table_a])
-    end
+
+    let(:referential) { context.referential }
+    before { referential.switch }
+
+    let(:journey_pattern) { context.journey_pattern(:journey_pattern_1) }
+    let(:route) { journey_pattern.route }
+    let(:line) { route.line }
+
+    let(:journey_pattern2) { context.journey_pattern(:journey_pattern_2) }
+    let(:route2) { journey_pattern2.route }
+    let(:line2) { route2.line }
 
     it 'cleans previous stats' do
       old_date = Date.parse('2023-11-24')

4e736fe8d8d1bb06b495e9fc101465e49821c737
Aymeric Le Dorze <aymeric.le-dorze@enroute.mobi>
2024-01-29 15:09:56 +0100

    CHOUETTE-3070 Remove useless code

--- app/models/chouette/vehicle_journey.rb
+++ app/models/chouette/vehicle_journey.rb
@@ -385,16 +385,6 @@ module Chouette
       attrs
     end
 
-    def missing_stops_in_relation_to_a_journey_pattern(selected_journey_pattern)
-      selected_journey_pattern.stop_points - self.stop_points
-    end
-    def extra_stops_in_relation_to_a_journey_pattern(selected_journey_pattern)
-      self.stop_points - selected_journey_pattern.stop_points
-    end
-    def extra_vjas_in_relation_to_a_journey_pattern(selected_journey_pattern)
-      extra_stops = self.extra_stops_in_relation_to_a_journey_pattern(selected_journey_pattern)
-      self.vehicle_journey_at_stops.select { |vjas| extra_stops.include?( vjas.stop_point)}
-    end
     def time_table_tokens=(ids)
       self.time_table_ids = ids.split(",")
     end
@@ -408,17 +398,6 @@ module Chouette
       end
 
       dates.empty? ? [] : [dates.min, dates.max]
-    end
-
-    def update_journey_pattern( selected_journey_pattern)
-      return unless selected_journey_pattern.route_id==self.route_id
-
-      missing_stops_in_relation_to_a_journey_pattern(selected_journey_pattern).each do |sp|
-        self.vehicle_journey_at_stops.build( :stop_point => sp)
-      end
-      extra_vjas_in_relation_to_a_journey_pattern(selected_journey_pattern).each do |vjas|
-        vjas._destroy = true
-      end
     end
 
     def fill_passing_times!

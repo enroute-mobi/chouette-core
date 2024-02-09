3da44569f4e8f1a5589c18f735ad17039fb79a3d
Hai Hieu VU <haihieu.vu@enroute.mobi>
2024-02-09 16:04:57 +0100

    CHOUETTE-3345 show codes by group in views

---
+++ app/helpers/codes_helper.rb
@@ -0,0 +1,18 @@
+module CodesHelper
+  def codes_group_by_short_name(codes)
+    [].tap do |groups|
+      codes.group_by { |c| c.code_space.short_name }.each do |short_name, group|
+        values = group.sort_by(&:value).map(&:value).join(', ')
+        groups << Group.new(short_name, values)
+      end
+    end
+  end
+
+  class Group
+    def initialize(short_name, values)
+      @short_name = short_name
+      @values = values
+    end
+    attr_reader :short_name, :values
+  end
+end

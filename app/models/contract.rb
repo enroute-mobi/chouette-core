c9471ce93cc5c826a76103a1f1d68ad4a7eef196
Alban Peignier <alban.peignier@enroute.mobi>
2024-02-29 11:02:05 +0100

    CHOUETTE-3303 Make Contract#name and #lines mandatory

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -3,8 +3,15 @@
 class Contract < ApplicationModel
   include CodeSupport
 
-  belongs_to :company, required: true, class_name: 'Chouette::Company'
-  belongs_to :workbench, required: true, class_name: 'Workbench'
+  belongs_to :company, optional: false, class_name: 'Chouette::Company'
+  belongs_to :workbench, optional: false, class_name: 'Workbench'
 
   has_array_of :lines, class_name: 'Chouette::Line'
+
+  validates :name, :lines, presence: true
+
+  def self.with_lines(*lines)
+    lines = lines.flatten
+    where('line_ids::integer[] && ARRAY[?]', lines.map(&:id))
+  end
 end

4a54e17119ca77bd0ff9cbf4b7b61a1e6fadbdc9
Luc <luc.donnet@enroute.mobi>
2024-02-29 12:14:28 +0100

    CHOUETTE-3412 Use good type for graphql StopPoint attributes

--- app/graphql/types/stop_point_type.rb
+++ app/graphql/types/stop_point_type.rb
@@ -3,14 +3,9 @@ module Types
     description 'A Chouette StopPoint'
 
     field :objectid, String, null: false
-    field :position, String, null: false
-    field :for_boarding, String, null: true
-    field :for_alighting, String, null: true
-
-    field :route, Types::RouteType, null: false, description: "The StopPoint's Route"
-    def route
-      LazyLoading::Routes.new(context, object.route_id)
-    end
+    field :position, Integer, null: false
+    field :for_boarding, Boolean, null: true
+    field :for_alighting, Boolean, null: true
 
     field :stop_area, Types::StopAreaType, null: false, description: "The StopPoint's StopArea"
     def stop_area

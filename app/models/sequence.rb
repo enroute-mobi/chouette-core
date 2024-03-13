97f309c836629a342b9d7fa1980c63585995ac22
Hai Hieu VU <haihieu.vu@enroute.mobi>
2024-03-13 22:45:13 +0100

    CHOUETTE-3343 add macro Create code from sequence

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -9,7 +9,17 @@ class Sequence < ApplicationModel
 
   enumerize :sequence_type, in: %i[range_sequence], scope: true
 
+  validates :range_start, :range_end, numericality: { only_integer: true }
+
   def range_values
     "#{range_start}-#{range_end}"
+  end
+
+  def values(offset: 1, limit: 1000)
+    value_start = range_start + (offset - 1) * limit
+    value_end = value_start + limit - 1
+    value_end = range_end if value_end > range_end
+
+    (value_start..value_end).to_a
   end
 end

d764df12b45812527b49007b5c94f67196843032
Luc <luc.donnet@enroute.mobi>
2024-04-10 15:08:40 +0200

    CHOUETTE-3533 Fix Sequence#static_list load and save

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -5,19 +5,11 @@ class Sequence < ApplicationModel
 
   belongs_to :workbench, optional: false, class_name: 'Workbench'
 
-  validates :name, :sequence_type, presence: true
-
   enumerize :sequence_type, in: %i[range_sequence static_list], default: :range_sequence, scope: true
 
-  validates :range_start, :range_end, numericality: { only_integer: true }, allow_blank: false
-
-  validate :range_start_less_than_range_end
-
-  # attribute :static_list, ::Sequence::Type.new
-
-  # def static_list
-  #   self.static_list.join(',')
-  # end
+  validates :name, presence: true
+  validates :range_start, :range_end, numericality: { only_integer: true }, allow_blank: false, if: proc { |sequence| sequence.sequence_type.range_sequence? }
+  validate :range_start_less_than_range_end, if: proc { |sequence| sequence.sequence_type.range_sequence? }
 
   def range_values
     "#{range_start}-#{range_end}"
@@ -41,14 +33,4 @@ class Sequence < ApplicationModel
     errors.add(:range_end, :range_start_less_than_range_end)
   end
 
-  class Type < ::ActiveRecord::Type::Value
-    def cast(value)
-      return if value.blank?
-      value.first.split(',') if value.is_a?(String)
-    end
-    def serialize(value)
-      return if value.blank?
-      value.to_s
-    end
-  end
 end

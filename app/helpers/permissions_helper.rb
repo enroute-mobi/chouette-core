4de1fe207d4dc4f54582980e8a71d74b86e6e2fb
Guillaume Perot <guillaume.perot@enroute.mobi>
2024-04-22 16:41:59 +0200

    CHOUETTE-3108 - Add new permission restrictions options for workgroup_workbench#edit

--- app/helpers/permissions_helper.rb
+++ app/helpers/permissions_helper.rb
@@ -3,7 +3,7 @@ module PermissionsHelper
   def permissions_array_to_hash(permissions_array)
     {}.tap do |result|
       result.default_proc = proc {|hash, key| hash[key] = [] }
-      permissions_array.sort.each do |permission|
+      permissions_array.each do |permission|
         feature = permission.split('.').first
         result[feature] << permission
       end

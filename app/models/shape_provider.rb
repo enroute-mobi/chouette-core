576b20c9ba9b888efb1e4b8afc618f11fd3d5d7c
Alban Peignier <alban.peignier@enroute.mobi>
2024-05-01 23:42:52 +0200

    CHOUETTE-3454 Fixes dependent in ShapeReferential has_many

--- app/models/shape_provider.rb
+++ app/models/shape_provider.rb
@@ -4,10 +4,10 @@ class ShapeProvider < ApplicationModel
   belongs_to :shape_referential, required: true
   belongs_to :workbench, required: true
 
-  has_many :shapes
-  has_many :point_of_interests, class_name: 'PointOfInterest::Base'
-  has_many :point_of_interest_categories, class_name: 'PointOfInterest::Category'
-  has_many :service_facility_sets, class_name: 'ServiceFacilitySet'
+  has_many :shapes, dependent: :delete_all
+  has_many :point_of_interests, class_name: 'PointOfInterest::Base', dependent: :delete_all
+  has_many :point_of_interest_categories, class_name: 'PointOfInterest::Category', dependent: :delete_all
+  has_many :service_facility_sets, class_name: 'ServiceFacilitySet', dependent: :delete_all
 
   validates :short_name, presence: true
 

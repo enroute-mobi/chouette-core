463eedc94783d1d2d6ff5f4ae0f86889729324cd
Alban Peignier <alban.peignier@enroute.mobi>
2024-05-01 23:40:58 +0200

    CHOUETTE-3454 Include only ServiceFacilitys not parent

--- app/inputs/service_facility_set_input.rb
+++ app/inputs/service_facility_set_input.rb
@@ -3,7 +3,7 @@
 class ServiceFacilitySetInput < GroupedTomSelectInput
   def options
     super.merge(
-      group_method: :self_and_sub_categories,
+      group_method: :sub_categories,
       group_label_method: :human_name,
       label_method: :human_name,
       value_method: :code

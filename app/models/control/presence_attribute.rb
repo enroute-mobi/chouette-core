bd012160ef069ff1c14e8064511068895a038a93
Hai Hieu VU <haihieu.vu@enroute.mobi>
2024-05-13 17:24:28 +0200

    CHOUETTE-3238 add referent and parent on the Control Presence of Attribute

--- app/models/control/presence_attribute.rb
+++ app/models/control/presence_attribute.rb
@@ -50,6 +50,8 @@ module Control
             select Chouette::StopArea, :compass_bearing
             select Chouette::StopArea, :coordinates
             select Chouette::StopArea, :waiting_time
+            select Chouette::StopArea, :referent
+            select Chouette::StopArea, :parent
 
             # Chouette::Company
             select Chouette::Company, :name
@@ -216,10 +218,6 @@ module Control
         def faulty_models
           query.send query_method
         end
-      end
-
-      def model_attribute_code
-        @model_attribute_code ||= "#{target_model.underscore}##{target_attribute}"
       end
 
       def model_collection

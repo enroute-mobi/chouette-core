8e8f920f97f053216b89842689138cbb09f057e3
Guillaume Perot <guillaume.perot@enroute.mobi>
2024-06-11 18:08:33 +0200

    CHOUETTE-3717 - Add new component for edit several values for static_list option on sequences

--- app/decorators/sequence_decorator.rb
+++ app/decorators/sequence_decorator.rb
@@ -8,4 +8,14 @@ class SequenceDecorator < Af83::Decorator
   create_action_link
 
   with_instance_decorator(&:crud)
+
+  define_instance_method :alpine_state do |is_export|
+    initial_state = { sequence_type: object.sequence_type, static_list: object.static_list }
+
+    object.static_list.reduce(initial_state) do |result, (k, v)|
+      result[k.camelcase(:lower)] = v
+
+      result
+    end.to_json
+  end
 end
---
+++ app/packs/entrypoints/sequences/form.js
@@ -0,0 +1,52 @@
+import Alpine from 'alpinejs'
+import { bindAll, uniq } from 'lodash'
+
+class Store {
+	constructor({
+		sequence_type = '',
+		static_list = null,
+	} = {}) {
+		this.sequence_type = sequence_type
+		this.static_list = static_list
+
+		bindAll(this, 'clipboardComponent')
+	}
+
+	clipboardComponent() {
+		return {
+			items: [],
+			pasteFromClipboard(event) {
+					event.preventDefault();
+					const clipboardData = event.clipboardData || window.clipboardData;
+					const pastedData = clipboardData.getData('Text');
+					this.addItems(pastedData);
+			},
+			addItems(data) {
+					const newItems = data.split(/[\s,]+/).map(item => item.trim()).filter(item => item && !this.items.includes(item));
+					this.static_list = this.items.concat(newItems);
+			},
+			removeItem(index) {
+					this.static_list.splice(index, 1);
+					this.updateTextArea();
+			},
+			updateItems(event) {
+				const inputValue = event.target.value;
+				const newItems = inputValue.split(/[\n]+/).map(item => item.trim()).filter(item => item);
+				this.static_list = Array.from(new Set(newItems)); // Remove duplicates
+				this.updateTextArea();
+			},
+			finalizeItems(event) {
+				const inputValue = event.target.value;
+				const newItems = inputValue.split(/[\s,]+/).map(item => item.trim()).filter(item => item && !this.items.includes(item));
+				this.static_list = this.items.concat(newItems);
+				this.updateTextArea();
+			},
+			updateTextArea() {
+				const textarea = document.querySelector('textarea[name="sequence[static_list]"]');
+				textarea.value = this.static_list.join('\n');
+			}
+		}
+	}
+}
+
+Alpine.data('sequenceForm', state => new Store(state))

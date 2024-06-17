8e5d9a5d6ecc253af7383d2478f4e9fa0cdc631e
Aymeric Le Dorze <aymeric.le-dorze@enroute.mobi>
2024-06-17 16:54:18 +0200

    CHOUETTE-3683 Fix destruction of a shape referential triggering the deletion instead of the destruction of its sub-models

--- app/models/shape_referential.rb
+++ app/models/shape_referential.rb
@@ -1,15 +1,15 @@
-class ShapeReferential < ApplicationModel
+# frozen_string_literal: true
 
+class ShapeReferential < ApplicationModel
   has_one :workgroup
   has_many :shape_providers, dependent: :destroy
-  has_many :shapes, dependent: :delete_all
-  has_many :point_of_interests, class_name: "PointOfInterest::Base", dependent: :delete_all
-  has_many :point_of_interest_categories, class_name: "PointOfInterest::Category", dependent: :delete_all
-  has_many :service_facility_sets, class_name: 'ServiceFacilitySet', dependent: :delete_all
-  has_many :accessibility_assessments, class_name: 'AccessibilityAssessment', dependent: :delete_all
+  has_many :shapes, dependent: :destroy
+  has_many :point_of_interests, class_name: 'PointOfInterest::Base', dependent: :destroy
+  has_many :point_of_interest_categories, class_name: 'PointOfInterest::Category', dependent: :destroy
+  has_many :service_facility_sets, class_name: 'ServiceFacilitySet', dependent: :destroy
+  has_many :accessibility_assessments, class_name: 'AccessibilityAssessment', dependent: :destroy
 
   def name
     self.class.ts
   end
-
 end

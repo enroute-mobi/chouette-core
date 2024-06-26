c003807e2e5452f49c491ecc47910c51d8962e32
Alban Peignier <alban.peignier@enroute.mobi>
2024-06-26 09:33:32 +0200

    CHOUETTE-3778 Simplify LegacyOperationJob.organisation_id specs

--- spec/jobs/legacy_operation_job_spec.rb
+++ spec/jobs/legacy_operation_job_spec.rb
@@ -2,32 +2,28 @@
 
 RSpec.describe LegacyOperationJob do
   subject(:job) { LegacyOperationJob.new operation }
-  let(:operation) { double }
 
-  describe '.organisation_id' do |_variable|
+  describe '.organisation_id' do
     subject { job.organisation_id }
 
-    let(:organisation_id) { 42 }
-    let(:organisation) { Organisation.new(id: organisation_id) }
+    let(:workbench_id) { 42 }
+    let(:workbench) { Workbench.new(id: workbench_id) }
 
-    context 'when the Operations is an Import' do
-      let(:operation) { Import::Gtfs.new organisation: organisation }
-      let(:workbench_id) { 44 }
-      let(:workbench) { Workbench.new id: workbench_id }
-      before { allow(operation).to receive(:workbench).and_return(workbench) }
+    context 'when the Operation is an Import' do
+      let(:operation) { Import::Gtfs.new workbench: workbench }
 
       it { is_expected.to eq(workbench_id) }
     end
 
-    context 'when the Operations is an Export' do
-      let(:operation) { Export::Gtfs.new organisation: organisation }
+    context 'when the Operation is an Export' do
+      let(:operation) { Export::Gtfs.new workbench: workbench }
 
       it { is_expected.to be_nil }
     end
 
-    context 'when the Operations is an Publication' do
+    context 'when the Operation is an Publication' do
       let(:operation) { Publication.new }
-      before { allow(operation).to receive(:organisation).and_return(organisation) }
+      before { allow(operation).to receive(:workbench).and_return(workbench) }
 
       it { is_expected.to be_nil }
     end

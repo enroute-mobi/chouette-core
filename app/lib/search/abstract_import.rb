d77f9e78be4ffbe0946740a11827b4df60e0136d
Aymeric Le Dorze <aymeric.le-dorze@enroute.mobi>
2024-07-09 16:42:02 +0200

    CHOUETTE-3105 Charts on Imports

--- app/lib/search/abstract_import.rb
+++ app/lib/search/abstract_import.rb
@@ -2,8 +2,22 @@
 
 module Search
   class AbstractImport < ::Search::Operation
+    AUTHORIZED_GROUP_BY_ATTRIBUTES = (superclass::AUTHORIZED_GROUP_BY_ATTRIBUTES + %w[status]).freeze
+
     def query_class
       Query::Import
+    end
+
+    class Chart < ::Search::Base::Chart
+      private
+
+      def all_status_keys
+        ::Import::Base.status.values
+      end
+
+      def label_status_key(key)
+        I18n.t(key, scope: 'imports.status')
+      end
     end
   end
 end

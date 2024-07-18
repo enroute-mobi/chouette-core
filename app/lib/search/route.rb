bbbdd5bf4997afe38b83decd5e75c39ad5ce9c18
Guillaume Perot <guillaume.perot@enroute.mobi>
2024-07-18 17:59:05 +0200

    CHOUETTE-3002 - use new search for routes

---
+++ app/lib/search/route.rb
@@ -0,0 +1,21 @@
+module Search
+  class Route < Base
+    # All search attributes
+    attribute :text
+    attribute :wayback
+
+    attr_accessor :workbench
+
+    def query(scope)
+      Query::Route.new(scope)
+                    .text(text)
+                    .wayback(wayback)
+    end
+
+    private
+
+    class Order < ::Search::Order
+      attribute :name, default: :asc
+    end
+  end
+end

bdfccccf2cd3d5632388bf108be804e4c44e739e
Alban Peignier <alban.peignier@enroute.mobi>
2024-09-24 10:05:34 +0200

    CHOUETTE-3994 Prevent empty Line/StopArea group

--- app/models/line_group.rb
+++ app/models/line_group.rb
@@ -4,6 +4,8 @@ class LineGroup < Group
   has_many :members, class_name: "LineGroup::Member", foreign_key: :group_id, dependent: :destroy, inverse_of: :group
   has_many :lines, class_name: "Chouette::Line", through: :members, inverse_of: :groups
 
+  validates :line_ids, length: { minimum: 1 }
+
   class Member < Group::Member
     belongs_to :line, class_name: "Chouette::Line"
     validates :line_id, uniqueness: { scope: %i[group_id] }
--- app/models/stop_area_group.rb
+++ app/models/stop_area_group.rb
@@ -4,6 +4,8 @@ class StopAreaGroup < Group
   has_many :members, class_name: "StopAreaGroup::Member", foreign_key: :group_id, dependent: :destroy, inverse_of: :group
   has_many :stop_areas, class_name: "Chouette::StopArea", through: :members, inverse_of: :groups
 
+  validates :stop_area_ids, length: { minimum: 1 }
+
   class Member < Group::Member
     belongs_to :stop_area, class_name: "Chouette::StopArea"
     validates :stop_area_id, uniqueness: { scope: %i[group_id] }

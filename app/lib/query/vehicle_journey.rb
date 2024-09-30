256f53226677451596f66b59ac01003068eaf18c
Hai Hieu VU <haihieu.vu@enroute.mobi>
2024-09-30 21:44:37 +0200

    CHOUETTE-4072 Search Vehicle Journeys by code

--- app/lib/query/vehicle_journey.rb
+++ app/lib/query/vehicle_journey.rb
@@ -4,9 +4,17 @@ module Query
   class VehicleJourney < Base
     def text(value)
       change_scope(if: value.present?) do |scope|
+        column_names = column_names(scope)
+        scope = scope.select(*column_names).from(custom_from(scope, column_names))
+
         published_journey_name = scope.arel_table[:published_journey_name]
         objectid = scope.arel_table[:objectid]
-        scope.where(published_journey_name.matches("%#{value}%")).or( scope.where(objectid.matches("%#{value}%")) )
+        code = scope.arel_table[:code]
+
+        scope.where(published_journey_name.matches("%#{value}%"))
+             .or(scope.where(objectid.matches("%#{value}%")))
+             .or(scope.where(code.matches("%#{value}%")))
+             .distinct
       end
     end
 
@@ -45,5 +53,15 @@ module Query
       end
     end
 
+    private
+
+    def custom_from(scope, column_names)
+      select = column_names + ["code_spaces.short_name || ':' || referential_codes.value AS code"]
+      "(#{scope.joins(codes: :code_space).select(*select).to_sql}) AS #{scope.table_name}"
+    end
+
+    def column_names(scope)
+      scope.column_names.map { |c| "#{scope.table_name}.#{c}" }
+    end
   end
 end

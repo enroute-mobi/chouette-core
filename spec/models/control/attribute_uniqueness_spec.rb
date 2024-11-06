43ec7f1567f9c2a4be4acc0b11fb436bb7983206
Luc <luc.donnet@enroute.mobi>
2024-11-06 18:27:49 +0100

    CHOUETTE-2427 Create a simple context for each scenario

--- spec/models/control/attribute_uniqueness_spec.rb
+++ spec/models/control/attribute_uniqueness_spec.rb
@@ -6,19 +6,6 @@ RSpec.describe Control::AttributeUniqueness do
       Control::List::Run.create referential: referential, workbench: workbench
     end
 
-    let(:context) do
-      Chouette.create do
-        stop_area_provider :other
-        workbench :other
-
-        referential do
-          vehicle_journey :first, published_journey_name: 'duplicate', published_journey_identifier: 'id_1'
-          vehicle_journey :second, published_journey_name: 'duplicate', published_journey_identifier: 'id_2'
-          vehicle_journey :last
-        end
-      end
-    end
-
     let(:control_run) do
       described_class.create(
         control_list_run: control_list_run,
@@ -30,32 +17,27 @@ RSpec.describe Control::AttributeUniqueness do
       )
     end
 
-    let(:referential) { context.referential }
-    let(:workbench) { referential.workbench }
-
-    let(:first_duplicate_vehicle_journey) { context.vehicle_journey(:first) }
-    let(:second_duplicate_vehicle_journey) { context.vehicle_journey(:second) }
-
-    let(:first_duplicate_stop) { referential.stop_areas.first }
-    let(:second_duplicate_stop) { referential.stop_areas.second }
-    let(:last_stop) { referential.stop_areas.last }
-
-    let(:stop_area_provider) { first_duplicate_stop.stop_area_provider }
-
-    let(:other_workbench) { context.workbench(:other) }
-    let(:other_stop_area_provider) { context.stop_area_provider(:other) }
-
-    before do
-      referential.switch
-    end
-
     subject do
       control_run.run
       control_run.control_messages
     end
 
     describe '#run' do
-      describe '#vehicle_journeys' do
+      context 'for Vehicle journeys' do
+        let(:context) do
+          Chouette.create do
+            referential do
+              vehicle_journey :first, published_journey_name: 'duplicate', published_journey_identifier: 'id_1'
+              vehicle_journey :second, published_journey_name: 'duplicate', published_journey_identifier: 'id_2'
+            end
+          end
+        end
+
+        let(:first_duplicate_vehicle_journey) { context.vehicle_journey(:first) }
+        let(:second_duplicate_vehicle_journey) { context.vehicle_journey(:second) }
+        let(:referential) { context.referential }
+        let(:workbench) { context.referential.workbench }
+
         let(:target_model) { 'VehicleJourney' }
         let(:target_attribute) { 'published_journey_name' }
         let(:uniqueness_scope) { nil }
@@ -84,20 +66,23 @@ RSpec.describe Control::AttributeUniqueness do
           )
         end
 
-        it 'should create warning messages' do
+        before do
+          referential.switch
+        end
+
+        it 'should create warning messages for duplicated vehicle journeys' do
           is_expected.to include(first_expected_message)
           is_expected.to include(second_expected_message)
         end
       end
 
-      describe '#stop_areas' do
+      describe 'for Stop areas' do
         let(:target_model) { 'StopArea' }
         let(:target_attribute) { 'name' }
 
-        before do
-          first_duplicate_stop.update name: 'duplicate'
-          second_duplicate_stop.update name: 'duplicate'
-        end
+        let(:first_duplicate_stop) { context.stop_area(:first) }
+        let(:second_duplicate_stop) { context.stop_area(:second) }
+        let(:workbench) { context.workbench(:first) }
 
         let(:first_expected_message) do
           an_object_having_attributes(
@@ -113,23 +98,23 @@ RSpec.describe Control::AttributeUniqueness do
           )
         end
 
-        let(:not_expected_message) do
-          an_object_having_attributes(
-            source: last_stop,
-            criticity: 'warning'
-          )
-        end
-
         context "When uniqueness scope is 'All'" do
           let(:uniqueness_scope) { 'all' }
 
-          it 'should create warning messages' do
-            is_expected.to include(first_expected_message)
-            is_expected.to include(second_expected_message)
+          let(:context) do
+            Chouette.create do
+              workbench :first do
+                stop_area :first, name: 'duplicate'
+              end
+              workbench :second do
+                stop_area :second, name: 'duplicate'
+              end
+            end
           end
 
-          it 'should not ccreate message for the last stop area' do
-            is_expected.not_to include(not_expected_message)
+          it 'should create warning messages for duplicated stop areas' do
+            is_expected.to include(first_expected_message)
+            is_expected.to include(second_expected_message)
           end
         end
 
@@ -137,19 +122,35 @@ RSpec.describe Control::AttributeUniqueness do
           let(:uniqueness_scope) { 'provider' }
 
           context 'with the same provider' do
+
+            let(:context) do
+              Chouette.create do
+                workbench :first do
+                  stop_area :first, name: 'duplicate'
+                  stop_area :second, name: 'duplicate'
+                end
+              end
+            end
+
             it 'should create warning messages' do
               is_expected.to include(first_expected_message)
               is_expected.to include(second_expected_message)
             end
-
-            it 'should not ccreate message for the last stop area' do
-              is_expected.not_to include(not_expected_message)
-            end
           end
 
           context 'with other provider' do
-            before do
-              first_duplicate_stop.update stop_area_provider: other_stop_area_provider
+
+            let(:context) do
+              Chouette.create do
+                workbench :first do
+                  stop_area_provider :first do
+                    stop_area :first, name: 'duplicate'
+                  end
+                  stop_area_provider :second do
+                    stop_area :second, name: 'duplicate'
+                  end
+                end
+              end
             end
 
             it 'should not create warning messages' do
@@ -161,20 +162,33 @@ RSpec.describe Control::AttributeUniqueness do
         context "When uniqueness scope is 'Workbench'" do
           let(:uniqueness_scope) { 'workbench' }
 
-          before do
-            first_duplicate_stop.update stop_area_provider: other_stop_area_provider
-          end
-
           context 'with the same workbench' do
+            let(:context) do
+              Chouette.create do
+                workbench :first do
+                  stop_area :first, name: 'duplicate'
+                  stop_area :second, name: 'duplicate'
+                end
+              end
+            end
+
             it 'should create warning messages' do
               is_expected.to include(first_expected_message)
               is_expected.to include(second_expected_message)
             end
           end
 
-          context 'with other provider' do
-            before do
-              stop_area_provider.update workbench: other_workbench
+          context 'with other workbench' do
+
+            let(:context) do
+              Chouette.create do
+                workbench :first do
+                  stop_area :first, name: 'duplicate'
+                end
+                workbench :second do
+                  stop_area :second, name: 'duplicate'
+                end
+              end
             end
 
             it 'should not create warning messages' do

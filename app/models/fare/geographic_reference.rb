06098d33531ae88de613ce979c114f42264f7bb9
Hai Hieu VU <haihieu.vu@enroute.mobi>
2024-12-12 16:34:34 +0100

    CHOUETTE-4022 add short name validation

--- app/models/fare/geographic_reference.rb
+++ app/models/fare/geographic_reference.rb
@@ -5,5 +5,7 @@ module Fare
     self.table_name = 'fare_geographic_references'
 
     belongs_to :fare_zone, class_name: 'Fare::Zone', foreign_key: 'fare_zone_id'
+
+    validates :short_name, presence: true
   end
 end
--- app/models/fare/zone.rb
+++ app/models/fare/zone.rb
@@ -15,7 +15,48 @@ module Fare
 
     has_many :stop_area_zones, class_name: 'Fare::StopAreaZone', foreign_key: 'fare_zone_id', dependent: :delete_all
     has_many :stop_areas, through: :stop_area_zones
-    has_many :fare_geographic_references, class_name: 'Fare::GeographicReference', foreign_key: 'fare_zone_id', dependent: :delete_all
+    has_many :fare_geographic_references, class_name: 'Fare::GeographicReference',
+                                          foreign_key: 'fare_zone_id',
+                                          dependent: :delete_all
     accepts_nested_attributes_for :fare_geographic_references, allow_destroy: true, reject_if: :all_blank
+
+    validates_associated :fare_geographic_references
+    validate :validate_fare_geographic_references
+
+    def validate_fare_geographic_references
+      return if GeographicReferenceUniqueness.new(fare_geographic_references).valid?
+
+      errors.add(:fare_geographic_references, :invalid)
+    end
+
+    class GeographicReferenceUniqueness
+      def initialize(fare_geographic_references)
+        @fare_geographic_references = fare_geographic_references
+      end
+      attr_reader :fare_geographic_references
+
+      def valid?
+        validate
+      end
+
+      def validate
+        return true if duplicated_short_name.empty?
+
+        duplicated_short_name.each do |fare_geographic_reference|
+          fare_geographic_reference.errors.add(
+            :short_name,
+            :duplicate_values_in_fare_geographic_references
+          )
+        end
+
+        false
+      end
+
+      def duplicated_short_name
+        fare_geographic_references
+          .group_by(&:short_name)
+          .flat_map { |_, group| group.many? ? group : [] }
+      end
+    end
   end
 end

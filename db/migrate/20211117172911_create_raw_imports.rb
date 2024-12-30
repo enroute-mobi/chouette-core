6f0779923422dae78a6f594b5e443d2b37bc511d
Aymeric Le Dorze <aymeric.le-dorze@enroute.mobi>
2024-12-30 18:32:00 +0100

    CHOUETTE-4271 Fix duplicate in column names in migrations

--- db/migrate/20211117172911_create_raw_imports.rb
+++ db/migrate/20211117172911_create_raw_imports.rb
@@ -3,7 +3,6 @@ class CreateRawImports < ActiveRecord::Migration[5.2]
     on_public_schema_only do
       create_table :raw_imports do |t|
         t.references :model, polymorphic: true, index: true
-        t.string :model_type
         t.text :content
 
         t.timestamps

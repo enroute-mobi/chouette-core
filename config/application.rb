0cf7d6aaf5e487572046091747d391f34ac069f0
Aymeric Le Dorze <aymeric.le-dorze@enroute.mobi>
2024-12-31 16:33:11 +0100

    CHOUETTE-4162 Monkey patches should be in app/overrides

--- config/application.rb
+++ config/application.rb
@@ -157,11 +157,16 @@ module ChouetteIhm
     config.semantic_logger.environment = nil
     config.rails_semantic_logger.add_file_appender = false
     config.logger_reopen_max = nil
+
+    overrides = Rails.root.join('app/overrides')
+    Rails.autoloaders.main.ignore(overrides)
+
+    config.to_prepare do
+      Dir.glob("#{overrides}/**/*_override.rb").sort.each do |override|
+        load override
+      end
+    end
   end
 end
-
-require_relative '../app/lib/geo_ext'
-require_relative '../app/lib/http_ext'
-require_relative '../app/lib/enumerable_ext'
 
 require 'prometheus/middleware/exporter'

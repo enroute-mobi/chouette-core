d32cc2ef245307c267c05d47a020319241aacb91
Alban Peignier <alban.peignier@enroute.mobi>
2025-01-06 08:51:38 +0100

    CHOUETTE-4063 Add a dedicated spec where deprecated option is ignored
    * Improve code style

--- app/controllers/api/v1/imports_controller.rb
+++ app/controllers/api/v1/imports_controller.rb
@@ -1,64 +1,68 @@
 # frozen_string_literal: true
 
-class Api::V1::ImportsController < Api::V1::WorkbenchController
-  respond_to :json, only: [:show, :index, :create]
+module Api
+  module V1
+    class ImportsController < Api::V1::WorkbenchController
+      respond_to :json, only: %i[show index create]
 
-  def create
-    args = workbench_import_params.merge(creator: 'Webservice')
+      def create
+        args = workbench_import_params.merge(creator: 'Webservice')
 
-    @import = current_workbench.workbench_imports.new(args)
+        @import = current_workbench.workbench_imports.new(args)
 
-    if @import.flag_urgent && !policy(@import).option_flag_urgent?
-      logger.error("Import #{@import.name} uses flag_urgent but workbench #{current_workbench.name} inside organisation #{current_workbench.name} doesn't have permission referentials.flag_urgent")
-      @import.flag_urgent = false
+        if @import.flag_urgent && !policy(@import).option_flag_urgent?
+          logger.error("Import #{@import.name} uses flag_urgent but workbench #{current_workbench.name} inside organisation #{current_workbench.name} doesn't have permission referentials.flag_urgent")
+          @import.flag_urgent = false
+        end
+
+        if @import.save
+          render json: import_json(@import), status: :created
+        else
+          render json: { status: 'error', messages: @import.errors.full_messages }
+        end
+      end
+
+      def index
+        render json: imports_map
+      end
+
+      def show
+        import = current_workbench.workbench_imports.find(params[:id])
+        render json: import_json(import)
+      end
+
+      private
+
+      def import_json(import)
+        {
+          id: import.id,
+          status: import.status,
+          workbench_id: import.workbench_id,
+          name: import.name,
+          created_at: import.created_at,
+          updated_at: import.updated_at,
+          started_at: import.started_at,
+          ended_at: import.ended_at,
+          creator: import.creator,
+          options: {
+            automatic_merge: import.automatic_merge,
+            archive_on_fail: import.archive_on_fail,
+            flag_urgent: import.flag_urgent
+          }
+        }
+      end
+
+      def imports_map
+        current_workbench.workbench_imports.includes(:children).collect do |import|
+          import_json(import)
+        end
+      end
+
+      def workbench_import_params
+        permitted_keys = %i[name file]
+        permitted_keys << { options: %w[automatic_merge archive_on_fail flag_urgent] }
+        params.require(:workbench_import).permit(permitted_keys)
+      end
     end
-
-    if @import.save
-      render json: import_json(@import), status: :created
-    else
-      render json: { status: "error", messages: @import.errors.full_messages }
-    end
-  end
-
-  def index
-    render json: imports_map
-  end
-
-  def show
-    import = current_workbench.workbench_imports.includes(:children).find(params[:id])
-    render json: import_json(import)
-  end
-
-  private
-
-  def import_json(import)
-    {
-      id: import.id,
-      status: import.status,
-      workbench_id: import.workbench_id,
-      name: import.name,
-      created_at: import.created_at,
-      updated_at: import.updated_at,
-      started_at: import.started_at,
-      ended_at: import.ended_at,
-      creator: import.creator,
-      options: {
-        automatic_merge: import.automatic_merge,
-        archive_on_fail: import.archive_on_fail,
-        flag_urgent: import.flag_urgent
-      }
-    }
-  end
-
-  def imports_map
-    current_workbench.workbench_imports.includes(:children).collect do |import|
-      import_json(import)
-    end
-  end
-
-  def workbench_import_params
-    permitted_keys = %i(name file)
-    permitted_keys << {options: %w[automatic_merge archive_on_fail flag_urgent]}
-    params.require(:workbench_import).permit(permitted_keys)
   end
 end

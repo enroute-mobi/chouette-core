e0ec9527fbcbd76ed5e6b0974527dc4bb638be72
Alban Peignier <alban.peignier@enroute.mobi>
2025-01-28 09:25:15 +0100

    CHOUETTE-4059 Improve code style

--- app/models/control/transport_mode_compatibility.rb
+++ app/models/control/transport_mode_compatibility.rb
@@ -3,19 +3,18 @@
 module Control
   class TransportModeCompatibility < Control::Base
     class Run < Control::Base::Run
-
       def run
         anomalies.each do |anomaly|
           control_messages.create({
-            message_attributes: {
-              stop_area_name: anomaly.stop_area_name,
-              line_name: anomaly.line_name
-            },
-            criticity: criticity,
-            source_id: anomaly.stop_area_id,
-            source_type: 'Chouette::StopArea',
-            message_key: :transport_mode_compatibility
-          })
+                                    message_attributes: {
+                                      stop_area_name: anomaly.stop_area_name,
+                                      line_name: anomaly.line_name
+                                    },
+                                    criticity: criticity,
+                                    source_id: anomaly.stop_area_id,
+                                    source_type: 'Chouette::StopArea',
+                                    message_key: :transport_mode_compatibility
+                                  })
         end
       end
 
@@ -32,16 +31,16 @@ module Control
 
       def query
         routes
-        .joins(:stop_areas, :line)
-        .where('stop_areas.transport_mode <> lines.transport_mode')
-        .where('stop_areas.transport_mode IS NOT NULL')
-        .where("lines.transport_mode <> 'undefined' AND lines.transport_mode IS NOT NULL")
-        .distinct
-        .select(
-          'stop_areas.id AS stop_area_id',
-          'stop_areas.name AS stop_area_name',
-          'lines.name AS line_name'
-        ).to_sql
+          .joins(:stop_areas, :line)
+          .where('stop_areas.transport_mode <> lines.transport_mode')
+          .where('stop_areas.transport_mode IS NOT NULL')
+          .where("lines.transport_mode <> 'undefined' AND lines.transport_mode IS NOT NULL")
+          .distinct
+          .select(
+            'stop_areas.id AS stop_area_id',
+            'stop_areas.name AS stop_area_name',
+            'lines.name AS line_name'
+          ).to_sql
       end
 
       def routes
--- spec/models/control/transport_mode_compatibility_spec.rb
+++ spec/models/control/transport_mode_compatibility_spec.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 RSpec.describe Control::TransportModeCompatibility do
   it 'should be one of the available Control' do
     expect(Control.available).to include(described_class)
   end
 
   describe Control::TransportModeCompatibility::Run do
-
     let(:control_list_run) do
       Control::List::Run.create referential: referential, workbench: workbench
     end
@@ -12,12 +13,10 @@ RSpec.describe Control::TransportModeCompatibility do
     let(:control_run) do
       described_class.create(
         control_list_run: control_list_run,
-        criticity: "warning",
+        criticity: 'warning',
         position: 0
       )
     end
-
-
 
     let(:workbench) { context.workbench }
     let(:referential) { context.referential }
@@ -36,11 +35,11 @@ RSpec.describe Control::TransportModeCompatibility do
         Chouette.create do
           workbench do
             line :line, name: 'Line', transport_mode: 'bus'
-  
+
             stop_area :first_correct, transport_mode: 'bus'
             stop_area :second_correct, transport_mode: 'bus'
             stop_area :wrong, name: 'Wrong', transport_mode: 'tramway'
-  
+
             referential do
               route(line: :line, stop_areas: %i[first_correct second_correct wrong]) do
                 journey_pattern
@@ -52,13 +51,13 @@ RSpec.describe Control::TransportModeCompatibility do
 
       let(:expected_message) do
         an_object_having_attributes({
-          source: wrong_stop_area,
-          criticity: 'warning',
-          message_attributes: {
-            'stop_area_name' => 'Wrong',
-            'line_name' => 'Line'
-          }
-        })
+                                      source: wrong_stop_area,
+                                      criticity: 'warning',
+                                      message_attributes: {
+                                        'stop_area_name' => 'Wrong',
+                                        'line_name' => 'Line'
+                                      }
+                                    })
       end
 
       it 'should detect the faulty Stop Area' do

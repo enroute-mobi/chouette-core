e12a1255157bf29aba8ec8dd648560b9edec4f2f
Alban Peignier <alban.peignier@enroute.mobi>
2025-04-03 09:09:17 +0200

    CHOUETTE-4271 Disable host_authorization for /healthz

--- config/application.rb
+++ config/application.rb
@@ -89,7 +89,10 @@ module ChouetteIhm
 
     config.i18n.default_locale = SmartEnv[:RAILS_LOCALE].to_sym
 
-    config.hosts << URI.parse(SmartEnv[:PUBLIC_HOST]).host if SmartEnv[:PUBLIC_HOST]
+    if SmartEnv[:PUBLIC_HOST]
+      config.hosts << URI.parse(SmartEnv[:PUBLIC_HOST]).host
+      config.host_authorization = { exclude: ->(request) { request.path == "/healthz" } }
+    end
 
     # Configure Browserify to use babelify to compile ES6
     # config.browserify_rails.commandline_options = "-t [ babelify --presets [ react es2015 ] ]"

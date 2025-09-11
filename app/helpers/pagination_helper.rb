275a742a0eb6f2a6943f50438cec450701a18e9a
Aymeric Le Dorze <aymeric.le-dorze@enroute.mobi>
2025-09-11 16:19:11 +0200

    CHOUETTE-4768 Prevent  some macro messages SQL requests

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -2,7 +2,7 @@ module PaginationHelper
 
   def new_pagination collection, cls = nil, **options
     # k = collection.first.class unless collection.empty?
-    pinfos = page_entries_info collection, html: false
+    pinfos = page_entries_info collection, options.merge(html: false)
     if collection.total_pages > 1
       links = content_tag :div, '', class: 'page_links' do
         will_paginate collection, container: false, page_links: false, previous_label: '', next_label: '', param_name: (collection.try(:pagination_param_name) || "page"), renderer: options[:renderer]

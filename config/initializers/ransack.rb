32c8c81199d0b67ca2e81336cff76773a0f828f9
Aymeric Le Dorze <aymeric.le-dorze@enroute.mobi>
2025-09-17 10:05:22 +0200

    CHOUETTE-4921 Remove monkey patch that replaces BETWEEN with >= AND <

--- config/initializers/ransack.rb
+++
@@ -1,9 +0,0 @@
-module Arel
-  module Predications
-    def between other
-      min = other.is_a?(Range) ? other.min : other[0]
-      max = other.is_a?(Range) ? other.max : other[1]
-      gteq(min).and(lt(max))
-    end
-  end
-end

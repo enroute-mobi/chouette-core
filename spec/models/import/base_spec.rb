d3f79f5bac27a34f2277846a0e34ae20df78b640
Aymeric Le Dorze <aymeric.le-dorze@enroute.mobi>
2025-09-29 18:33:45 +0200

    CHOUETTE-4721 Rewrite some tests due to travel time with Timecop that prevents some migrations from occurring

--- spec/models/import/base_spec.rb
+++ spec/models/import/base_spec.rb
@@ -18,32 +18,29 @@ RSpec.describe Import::Base, type: :model do
     let(:other_workbench) { create(:workbench) }
 
     it 'removes files from imports older than 60 days' do
-      file_purgeable = Timecop.freeze(60.days.ago) do
-        create(:workbench_import, workbench: workbench)
+      file_purgeable = create(:workbench_import, workbench: workbench)
+      other_file_purgeable = create(:workbench_import, workbench: other_workbench)
+
+      Timecop.freeze(60.days.from_now) do
+        Import::Workbench.new(workbench: workbench).purge_imports
+
+        expect(file_purgeable.reload.file_url).to be_nil
+        expect(other_file_purgeable.reload.file_url).not_to be_nil
       end
-
-      other_file_purgeable = Timecop.freeze(60.days.ago) do
-        create(:workbench_import, workbench: other_workbench)
-      end
-
-      Import::Workbench.new(workbench: workbench).purge_imports
-
-      expect(file_purgeable.reload.file_url).to be_nil
-      expect(other_file_purgeable.reload.file_url).not_to be_nil
     end
 
     it 'removes imports older than 90 days' do
-      old_import = Timecop.freeze(90.days.ago) do
-        create(:workbench_import, workbench: workbench)
+      old_import = create(:workbench_import, workbench: workbench)
+
+      Timecop.freeze(90.days.from_now) do
+        expect { Import::Workbench.new(workbench: workbench).purge_imports }.to change {
+          old_import.workbench.imports.purgeable.count
+        }.from(1).to(0)
+
+        expect { Import::Workbench.new(workbench: workbench).purge_imports }.not_to(change do
+          old_import.workbench.imports.purgeable.count
+        end)
       end
-
-      expect { Import::Workbench.new(workbench: workbench).purge_imports }.to change {
-        old_import.workbench.imports.purgeable.count
-      }.from(1).to(0)
-
-      expect { Import::Workbench.new(workbench: workbench).purge_imports }.not_to(change do
-        old_import.workbench.imports.purgeable.count
-      end)
     end
   end
 

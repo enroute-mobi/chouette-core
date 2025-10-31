ed707a7d4877ff168c98a538567565fbfe8c1dba
Luc <luc.donnet@enroute.mobi>
2025-10-31 11:15:34 +0100

    CHOUETTE-4466 Use rubocop on files

--- spec/models/export/gtfs/booking_arrangements_spec.rb
+++ spec/models/export/gtfs/booking_arrangements_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe Export::Gtfs::BookingArrangements::Decorator do
     subject { decorator.prior_notice_duration_min }
 
     context 'when BookingArrangement#book_when is "advance_and_day_of_travel"' do
-      before {
+      before do
         booking_arrangement.book_when = 'advance_and_day_of_travel'
         booking_arrangement.minimum_booking_period = 60
-      }
+      end
 
       it { is_expected.to eq(1) }
     end
@@ -24,7 +24,7 @@ RSpec.describe Export::Gtfs::BookingArrangements::Decorator do
   describe '#prior_notice_last_day' do
     subject { decorator.prior_notice_last_day }
     context 'when BookingArrangement#latest_booking_time is present' do
-      before { booking_arrangement.latest_booking_time = "01:00" }
+      before { booking_arrangement.latest_booking_time = '01:00' }
 
       it { is_expected.to eq(1) }
     end
@@ -32,15 +32,14 @@ RSpec.describe Export::Gtfs::BookingArrangements::Decorator do
     context 'when BookingArrangement#latest_booking_time is not present' do
       it { is_expected.to eq(nil) }
     end
-
   end
 
   describe '#prior_notice_last_time' do
     subject { decorator.prior_notice_last_time }
     context 'when BookingArrangement#latest_booking_time is present' do
-      before { booking_arrangement.latest_booking_time = "01:00" }
+      before { booking_arrangement.latest_booking_time = '01:00' }
 
-      it { is_expected.to eq("01:00:00") }
+      it { is_expected.to eq('01:00:00') }
     end
 
     context 'when BookingArrangement#latest_booking_time is not present' do
@@ -57,25 +56,25 @@ RSpec.describe Export::Gtfs::BookingArrangements::Decorator do
     end
 
     context 'when BookingArrangement#book_when is "advance_and_day_of_travel"' do
-      before {
+      before do
         booking_arrangement.book_when = 'advance_and_day_of_travel'
-      }
+      end
 
       it { is_expected.to eq(1) }
     end
 
     context 'when BookingArrangement#book_when is "until_previous_day"' do
-      before {
+      before do
         booking_arrangement.book_when = 'until_previous_day'
-      }
+      end
 
       it { is_expected.to eq(2) }
     end
 
     context 'when BookingArrangement#book_when is "day_of_travel_only"' do
-      before {
+      before do
         booking_arrangement.book_when = 'day_of_travel_only'
-      }
+      end
 
       it { is_expected.to eq(nil) }
     end
@@ -85,25 +84,27 @@ RSpec.describe Export::Gtfs::BookingArrangements::Decorator do
     subject { decorator.gtfs_attributes }
 
     context 'when BookingArrangement#book_when is "advance_and_day_of_travel"' do
-      before {
+      before do
         booking_arrangement.book_when = 'advance_and_day_of_travel'
         booking_arrangement.booking_notes = 'test'
         booking_arrangement.phone = '0610253212'
         booking_arrangement.url = 'http://localhost'
         booking_arrangement.booking_url = 'http://localhost'
-      }
+      end
 
-      it { is_expected.to eq({
-        booking_rule_id: nil,
-        booking_type: 1,
-        message: 'test',
-        phone_number: '0610253212',
-        info_url: 'http://localhost',
-        booking_url: 'http://localhost',
-        prior_notice_duration_min: nil,
-        prior_notice_last_day: nil,
-        prior_notice_last_time: nil,
-      }) }
+      it {
+        is_expected.to eq({
+                            booking_rule_id: nil,
+                            booking_type: 1,
+                            message: 'test',
+                            phone_number: '0610253212',
+                            info_url: 'http://localhost',
+                            booking_url: 'http://localhost',
+                            prior_notice_duration_min: nil,
+                            prior_notice_last_day: nil,
+                            prior_notice_last_time: nil
+                          })
+      }
     end
   end
 end

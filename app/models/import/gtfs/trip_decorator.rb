c280dc8dbd5d39239ad62825d547c92276c4e8a7
Alban Peignier <alban.peignier@enroute.mobi>
2025-12-15 08:08:58 +0100

    CHOUETTE-5055 Add specs on Import::Gtfs::TripDecorator

--- app/models/import/gtfs/trip_decorator.rb
+++ app/models/import/gtfs/trip_decorator.rb
@@ -1,249 +1,262 @@
-class Import::Gtfs
-  # Add helper methods on GTFS Trip
-  class TripDecorator < Import::Gtfs::Decorator
-    attr_accessor :default_time_zone
+# frozen_string_literal: true
 
-    delegate :each, :length, :[], to: :stop_ids
+module Import
+  class Gtfs
+    # Add helper methods on GTFS Trip
+    class TripDecorator < Import::Gtfs::Decorator
+      attr_accessor :default_time_zone
 
-    def route_signature
-      @route_signature ||= [
-        route_id,
-        direction_id,
-        stop_times_signature
-      ].freeze
-    end
+      delegate :each, :length, :[], to: :stop_ids
 
-    def stop_ids
-      @stop_ids ||= stop_times.map(&:stop_id)
-    end
-
-    def journey_pattern_signature
-      # TODO: manage more invalid cases ?
-      return nil unless stop_times.many?
-
-      @journey_pattern_signature ||= [
-        *route_signature,
-        headsign,
-        shape_id,
-        *stop_ids
-      ].freeze
-    end
-
-    def journey_pattern
-      return unless lookup && journey_pattern_signature
-      @journey_pattern ||= lookup.journey_patterns.find_by(signature: journey_pattern_signature)
-    end
-
-    def chouette_route_id
-      journey_pattern&.route_id
-    end
-
-    def accessibility_assessment
-      lookup.accessibility_assessments.find_by(wheelchair_accessible: wheelchair_accessible)
-    end
-
-    def service_facility_set
-      lookup.service_facility_sets.find_by(bikes_allowed: bikes_allowed)
-    end
-
-    def vehicle_journey_at_stops
-      unless journey_pattern
-        errors.add :journey_pattern_invalid
-        return []
+      def route_signature
+        @route_signature ||= [
+          route_id,
+          direction_id,
+          route_stops_signature
+        ].freeze
       end
 
-      unless stop_times.many?
-        errors.add :stop_times_many_required
-        return []
+      def stop_ids
+        @stop_ids ||= stop_times.map(&:stop_id)
       end
 
-      [].tap do |vehicle_journey_at_stops|
-        previous = nil
-        journey_pattern.stop_points.each_with_index do |stop_point, position|
-          stop_time = stop_times[position]
+      def journey_pattern_signature
+        # TODO: manage more invalid cases ?
+        return nil unless stop_times.many?
 
-          decorator = StopTimeDecorator.new(
-            stop_time,
-            stop_point: stop_point,
-            starting_day_offset: starting_day_offset,
-            default_time_zone: default_time_zone,
-            previous: previous
-          )
+        @journey_pattern_signature ||= [
+          *route_signature,
+          headsign,
+          shape_id,
+          *stop_ids
+        ].freeze
+      end
 
-          unless decorator.valid?
-            decorator.errors.each do |stop_time_error|
-              stop_time_error.message_attributes[:trip_id] = id
-              errors << stop_time_error
+      def journey_pattern
+        return unless lookup && journey_pattern_signature
+
+        @journey_pattern ||= lookup.journey_patterns.find_by(signature: journey_pattern_signature)
+      end
+
+      def chouette_route_id
+        journey_pattern&.route_id
+      end
+
+      def accessibility_assessment
+        return unless lookup
+
+        lookup.accessibility_assessments.find_by(wheelchair_accessible: wheelchair_accessible)
+      end
+
+      def service_facility_set
+        return unless lookup
+
+        lookup.service_facility_sets.find_by(bikes_allowed: bikes_allowed)
+      end
+
+      def vehicle_journey_at_stops
+        unless stop_times.many?
+          errors.add :stop_times_many_required
+          return []
+        end
+
+        unless journey_pattern
+          errors.add :journey_pattern_invalid
+          return []
+        end
+
+        [].tap do |vehicle_journey_at_stops|
+          previous = nil
+          journey_pattern.stop_points.each_with_index do |stop_point, position|
+            stop_time = stop_times[position]
+
+            decorator = StopTimeDecorator.new(
+              stop_time,
+              stop_point: stop_point,
+              starting_day_offset: starting_day_offset,
+              default_time_zone: default_time_zone,
+              previous: previous
+            )
+
+            unless decorator.valid?
+              decorator.errors.each do |stop_time_error|
+                stop_time_error.message_attributes[:trip_id] = id
+                errors << stop_time_error
+              end
+              # TODO: we could skip the model creation
             end
-            # TODO: we could skip the model creation
-          end
 
-          previous = decorator
-          vehicle_journey_at_stops << decorator.chouette_model
-        end
-      end
-    end
-
-    class StopTimeDecorator < Import::Gtfs::Decorator
-      attr_accessor :starting_day_offset, :default_time_zone, :stop_point, :previous
-
-      def self.time_of_day(name, as:)
-        define_method as do
-          cached_value = instance_variable_get("@#{as}")
-          return cached_value if cached_value
-
-          value = try(name)
-          return if value.blank?
-
-          time_of_day(value).tap do |cached_value|
-            instance_variable_set "@#{as}", cached_value
+            previous = decorator
+            vehicle_journey_at_stops << decorator.chouette_model
           end
         end
       end
 
-      time_of_day :departure_time, as: :departure_time_of_day
-      time_of_day :arrival_time, as: :arrival_time_of_day
-      time_of_day :start_pickup_drop_off_window, as: :earliest_departure_time_of_day
-      time_of_day :end_pickup_drop_off_window, as: :latest_arrival_time_of_day
+      class StopTimeDecorator < Import::Gtfs::Decorator
+        attr_accessor :starting_day_offset, :default_time_zone, :stop_point, :previous
 
-      def time_of_day(raw_gtfs_time, offset: starting_day_offset)
-        return if raw_gtfs_time.blank?
+        def self.time_of_day(name, as:)
+          define_method as do
+            cached_value = instance_variable_get("@#{as}")
+            return cached_value if cached_value
 
-        gtfs_time = GTFS::Time.parse(raw_gtfs_time).from_day_offset(offset)
-        return if gtfs_time.blank?
+            value = try(name)
+            return if value.blank?
 
-        TimeOfDay.create(gtfs_time, time_zone: default_time_zone).without_utc_offset
-      end
+            time_of_day(value).tap do |cached_value|
+              instance_variable_set "@#{as}", cached_value
+            end
+          end
+        end
 
-      def stop_attributes
-        {
-          stop_point_id: stop_point.id,
-          departure_time_of_day:,
-          arrival_time_of_day:,
-          earliest_departure_time_of_day:,
-          latest_arrival_time_of_day:,
-        }.compact
-      end
+        time_of_day :departure_time, as: :departure_time_of_day
+        time_of_day :arrival_time, as: :arrival_time_of_day
+        time_of_day :start_pickup_drop_off_window, as: :earliest_departure_time_of_day
+        time_of_day :end_pickup_drop_off_window, as: :latest_arrival_time_of_day
 
-      def flexible?
-        return false if [ try(:departure_time), try(:arrival_time) ].any?
+        def time_of_day(raw_gtfs_time, offset: starting_day_offset)
+          return if raw_gtfs_time.blank?
 
-        [
-          try(:start_pickup_drop_off_window),
-          try(:end_pickup_drop_off_window),
-          try(:location_group_id),
-          # try(:location_id)
-        ].any?
-      end
+          gtfs_time = GTFS::Time.parse(raw_gtfs_time).from_day_offset(offset)
+          return if gtfs_time.blank?
 
-      def validate
-        super
+          TimeOfDay.create(gtfs_time, time_zone: default_time_zone).without_utc_offset
+        end
 
-        unless flexible?
-          if departure_time_of_day && arrival_time_of_day
+        def stop_attributes
+          {
+            stop_point_id: stop_point.id,
+            departure_time_of_day:,
+            arrival_time_of_day:,
+            earliest_departure_time_of_day:,
+            latest_arrival_time_of_day:
+          }.compact
+        end
+
+        def flexible?
+          return false if [try(:departure_time), try(:arrival_time)].any?
+
+          [
+            try(:start_pickup_drop_off_window),
+            try(:end_pickup_drop_off_window),
+            try(:location_group_id)
+            # try(:location_id)
+          ].any?
+        end
+
+        def validate
+          super
+
+          if flexible?
+            if earliest_departure_time_of_day && latest_arrival_time_of_day
+              unless earliest_departure_time_of_day < latest_arrival_time_of_day
+                errors.add :invalid_pickup_drop_off_window
+              end
+            else
+              errors.add :missing_start_pickup_drop_off_window if earliest_departure_time_of_day.nil?
+              errors.add :missing_end_pickup_drop_off_window if latest_arrival_time_of_day.nil?
+            end
+          elsif departure_time_of_day && arrival_time_of_day
             errors.add :arrival_after_departure if arrival_time_of_day > departure_time_of_day
           else
             errors.add :missing_departure_time if departure_time_of_day.nil?
             errors.add :missing_arrival_time if arrival_time_of_day.nil?
           end
-        else
-          if earliest_departure_time_of_day && latest_arrival_time_of_day
-            errors.add :invalid_pickup_drop_off_window unless earliest_departure_time_of_day < latest_arrival_time_of_day
-          else
-            errors.add :missing_start_pickup_drop_off_window if earliest_departure_time_of_day.nil?
-            errors.add :missing_end_pickup_drop_off_window if latest_arrival_time_of_day.nil?
-          end
-        end
 
-        if arrival_time_of_day && previous&.departure_time_of_day
+          return unless arrival_time_of_day && previous&.departure_time_of_day
+
           errors.add :non_chronological if previous.departure_time_of_day > arrival_time_of_day
         end
+
+        def chouette_model
+          Chouette::VehicleJourneyAtStop.new(stop_attributes)
+        end
+      end
+
+      def time_table_id
+        unless service_id
+          errors.add :service_undefined
+          return
+        end
+
+        return unless lookup && starting_day_offset
+
+        lookup.time_tables.find_id(service_id, starting_day_offset: starting_day_offset)
+      end
+
+      def vehicle_journey_time_table_relationships
+        unless time_table_id
+          errors.add :service_unknown, message_attributes: { service_id: service_id }
+          return []
+        end
+        [Chouette::TimeTablesVehicleJourney.new(time_table_id: time_table_id)]
+      end
+
+      def vehicle_journey_attributes
+        {
+          route_id: chouette_route_id,
+          journey_pattern: journey_pattern,
+          published_journey_name: published_journey_name,
+          published_journey_identifier: published_journey_identifier,
+          codes: [code],
+          accessibility_assessment: accessibility_assessment,
+          service_facility_sets: [service_facility_set],
+          vehicle_journey_at_stops: vehicle_journey_at_stops,
+          vehicle_journey_time_table_relationships: vehicle_journey_time_table_relationships
+        }
       end
 
       def chouette_model
-        Chouette::VehicleJourneyAtStop.new(stop_attributes)
-      end
-    end
-
-    def time_table_id
-      unless service_id
-        errors.add :service_undefined
+        @chouette_model ||= Chouette::VehicleJourney.new(vehicle_journey_attributes)
       end
 
-      return unless lookup && starting_day_offset
-      lookup.time_tables.find_id(service_id, starting_day_offset: starting_day_offset)
-    end
-
-    def vehicle_journey_time_table_relationships
-      unless time_table_id
-        errors.add :service_unknown, message_attributes: { service_id: service_id }
-        return []
+      def validate
+        super
+        chouette_model
       end
-      [ Chouette::TimeTablesVehicleJourney.new(time_table_id: time_table_id) ]
-    end
 
-    def vehicle_journey_attributes
-      {
-        route_id: chouette_route_id,
-        journey_pattern: journey_pattern,
-        published_journey_name: published_journey_name,
-        published_journey_identifier: published_journey_identifier,
-        codes: [ code ],
-        accessibility_assessment: accessibility_assessment,
-        service_facility_sets: [ service_facility_set ],
-        vehicle_journey_at_stops: vehicle_journey_at_stops,
-        vehicle_journey_time_table_relationships: vehicle_journey_time_table_relationships
-      }
-    end
+      def code
+        return unless code_space
 
-    def chouette_model
-      @chouette_model ||= Chouette::VehicleJourney.new(vehicle_journey_attributes)
-    end
+        ReferentialCode.new(code_space: code_space, value: id)
+      end
 
-    def validate
-      super
-      chouette_model
-    end
+      def published_journey_name
+        short_name.presence || id
+      end
 
-    def code
-      ReferentialCode.new(code_space: code_space, value: id)
-    end
+      def published_journey_identifier
+        id
+      end
 
-    def published_journey_name
-      short_name.presence || id
-    end
+      def starting_day_offset
+        return 0 if stop_times.empty?
 
-    def published_journey_identifier
-      id
-    end
+        first_stop_time = stop_times.first
+        first_time = first_stop_time.departure_time || first_stop_time.start_pickup_drop_off_window
+        return 0 if first_time.blank?
 
-    def starting_day_offset
-      return 0 if stop_times.empty?
+        gtfs_first_time = GtfsTime.parse(first_time)
+        return 0 unless gtfs_first_time
 
-      first_stop_time = stop_times.first
-      first_time = first_stop_time.departure_time || first_stop_time.start_pickup_drop_off_window
-      return 0 if first_time.blank?
+        @starting_day_offset ||= gtfs_first_time.day_offset
+      end
 
-      @starting_day_offset ||= GtfsTime.parse(first_time).day_offset
-    end
+      def route_stops_signature
+        skip = true
+        stop_times.reverse_each.filter_map do |stop_time|
+          next if skip && stop_time.pickup_type.nil? && stop_time.drop_off_type.nil?
 
-    private
-
-    def stop_times_signature
-      skip = true
-      stop_times.reverse_each.filter_map do |stop_time|
-        # FIXME when pickup_type/drop_off_typeare not defined in the file, the signature is empty
-        next if skip && stop_time.pickup_type.nil? && stop_time.drop_off_type.nil?
-
-        skip = false
-        flexible = stop_time.start_pickup_drop_off_window.present? && stop_time.end_pickup_drop_off_window.present?
-        [
-          stop_time.stop_id.presence || stop_time.location_group_id,
-          stop_time.pickup_type || '0',
-          stop_time.drop_off_type || '0',
-          flexible
-        ]
-      end.to_a
+          skip = false
+          flexible = stop_time.start_pickup_drop_off_window.present? && stop_time.end_pickup_drop_off_window.present?
+          [
+            stop_time.stop_id.presence || stop_time.location_group_id,
+            stop_time.pickup_type || '0',
+            stop_time.drop_off_type || '0',
+            flexible
+          ]
+        end.to_a
+      end
     end
   end
 end
--- spec/models/import/gtfs/trip_decorator_spec.rb
+++ spec/models/import/gtfs/trip_decorator_spec.rb
@@ -13,10 +13,6 @@ RSpec.describe Import::Gtfs::TripDecorator do
       stop_times: stop_times
     )
   end
-  let(:stop_time_1_pickup) { nil }
-  let(:stop_time_1_drop_off) { nil }
-  let(:stop_time_2_pickup) { nil }
-  let(:stop_time_2_drop_off) { nil }
   let(:stop_times) do
     [
       GTFS::StopTime.new(
@@ -24,18 +20,14 @@ RSpec.describe Import::Gtfs::TripDecorator do
         stop_id: 'BEATTY_AIRPORT',
         arrival_time: '8:00:00',
         departure_time: '8:00:00',
-        stop_sequence: '1',
-        pickup_type: stop_time_1_pickup,
-        drop_off_type: stop_time_1_drop_off
+        stop_sequence: '1'
       ),
       GTFS::StopTime.new(
         trip_id: 'AAMV1',
         stop_id: 'AMV',
         arrival_time: '9:00:00',
         departure_time: '9:00:00',
-        stop_sequence: '2',
-        pickup_type: stop_time_2_pickup,
-        drop_off_type: stop_time_2_drop_off
+        stop_sequence: '2'
       )
     ]
   end
@@ -47,41 +39,24 @@ RSpec.describe Import::Gtfs::TripDecorator do
       it { is_expected.to eq(['AAMV', '0', []]) }
     end
 
-    context 'with pickup/drop_off' do
-      let(:stop_time_1_pickup) { '1' }
-      let(:stop_time_2_drop_off) { '1' }
+    context 'with defined pickup/drop_off' do
+      before do
+        stop_times.first.pickup_type = '1'
+        stop_times.second.drop_off_type = '1'
+      end
 
       it { is_expected.to eq(['AAMV', '0', [['AMV', '0', '1', false], ['BEATTY_AIRPORT', '1', '0', false]]]) }
 
       context 'but not on the last stop' do
-        let(:stop_time_2_drop_off) { nil }
+        before { stop_times.last.drop_off_type = nil }
 
         it { is_expected.to eq(['AAMV', '0', [['BEATTY_AIRPORT', '1', '0', false]]]) }
       end
 
       context 'with location_group_id' do
-        let(:stop_times) do
-          [
-            GTFS::StopTime.new(
-              trip_id: 'AAMV1',
-              stop_id: nil,
-              location_group_id: 'FLEXIBLE',
-              arrival_time: '8:00:00',
-              departure_time: '8:00:00',
-              stop_sequence: '1',
-              pickup_type: stop_time_1_pickup,
-              drop_off_type: stop_time_1_drop_off
-            ),
-            GTFS::StopTime.new(
-              trip_id: 'AAMV1',
-              stop_id: 'AMV',
-              arrival_time: '9:00:00',
-              departure_time: '9:00:00',
-              stop_sequence: '2',
-              pickup_type: stop_time_2_pickup,
-              drop_off_type: stop_time_2_drop_off
-            )
-          ]
+        before do
+          stop_times.first.stop_id = nil
+          stop_times.first.location_group_id = 'FLEXIBLE'
         end
 
         it { is_expected.to eq(['AAMV', '0', [['AMV', '0', '1', false], ['FLEXIBLE', '1', '0', false]]]) }
@@ -93,5 +68,300 @@ RSpec.describe Import::Gtfs::TripDecorator do
     subject { trip_decorator.journey_pattern_signature }
 
     it { is_expected.to eq(['AAMV', '0', [], 'to Amargosa Valley', nil, 'BEATTY_AIRPORT', 'AMV']) }
+
+    context 'when stop times is empty' do
+      before { trip.stop_times.clear }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when stop times contains a single Stop Time' do
+      before { trip.stop_times = [stop_times.first] }
+
+      it { is_expected.to be_nil }
+    end
+  end
+
+  describe '#stop_ids' do
+    subject { trip_decorator.stop_ids }
+
+    it { is_expected.to eq(%w[BEATTY_AIRPORT AMV]) }
+  end
+
+  describe '#journey_pattern' do
+    subject { trip_decorator.journey_pattern }
+
+    let(:journey_pattern_found_by_signature) { double('Journey Pattern found by signature') }
+    before { trip_decorator.lookup = double(journey_patterns: double) }
+
+    it do
+      expect(trip_decorator.lookup.journey_patterns).to receive(:find_by).with(signature: trip_decorator.journey_pattern_signature).and_return(journey_pattern_found_by_signature)
+      is_expected.to eq(journey_pattern_found_by_signature)
+    end
+
+    context 'when lookup is nil' do
+      before { trip_decorator.lookup = nil }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when journey_pattern_signature is nil' do
+      before do
+        trip_decorator.lookup = double
+        allow(trip_decorator).to receive(:journey_pattern_signature).and_return(nil)
+      end
+
+      it { is_expected.to be_nil }
+    end
+  end
+
+  describe '#chouette_route_id' do
+    subject { trip_decorator.chouette_route_id }
+
+    context 'when journey_pattern is associated to the Route 42' do
+      before { allow(trip_decorator).to receive(:journey_pattern).and_return(Chouette::JourneyPattern.new(route_id: 42)) }
+
+      it { is_expected.to eq(42) }
+    end
+
+    context 'when journey_pattern is not defined' do
+      before { allow(trip_decorator).to receive(:journey_pattern).and_return(nil) }
+
+      it { is_expected.to be_nil }
+    end
+  end
+
+  describe '#accessibility_assessment' do
+    subject { trip_decorator.accessibility_assessment }
+
+    let(:accessibility_assessment_found) { double('Accessibility Assessment found by wheelchair_accessible') }
+    before { trip_decorator.lookup = double(accessibility_assessments: double) }
+
+    it do
+      expect(trip_decorator.lookup.accessibility_assessments).to receive(:find_by).with(wheelchair_accessible: trip.wheelchair_accessible).and_return(accessibility_assessment_found)
+      is_expected.to eq(accessibility_assessment_found)
+    end
+
+    context 'when lookup is nil' do
+      before { trip_decorator.lookup = nil }
+
+      it { is_expected.to be_nil }
+    end
+  end
+
+  describe '#service_facility_set' do
+    subject { trip_decorator.service_facility_set }
+
+    let(:service_facility_set_found) { double('ServiceFacilitySet found by bikes_allowed') }
+    before { trip_decorator.lookup = double(service_facility_sets: double) }
+
+    it do
+      expect(trip_decorator.lookup.service_facility_sets).to receive(:find_by).with(bikes_allowed: trip.bikes_allowed).and_return(service_facility_set_found)
+      is_expected.to eq(service_facility_set_found)
+    end
+
+    context 'when lookup is nil' do
+      before { trip_decorator.lookup = nil }
+
+      it { is_expected.to be_nil }
+    end
+  end
+
+  describe '#vehicle_journey_at_stops' do
+    subject { trip_decorator.vehicle_journey_at_stops }
+
+    context  'when journey_pattern is not defined' do
+      before { allow(trip_decorator).to receive(:journey_pattern).and_return(nil) }
+
+      it { is_expected.to be_empty }
+
+      describe 'errors' do
+        subject { trip_decorator.errors }
+        before { trip_decorator.vehicle_journey_at_stops }
+
+        it { is_expected.to include(an_object_having_attributes(message_key: :journey_pattern_invalid)) }
+      end
+    end
+
+    context  'when stop_times is empty' do
+      before { trip.stop_times.clear }
+
+      it { is_expected.to be_empty }
+
+      describe 'errors' do
+        subject { trip_decorator.errors }
+        before { trip_decorator.vehicle_journey_at_stops }
+
+        it { is_expected.to include(an_object_having_attributes(message_key: :stop_times_many_required)) }
+      end
+    end
+
+    context  'when stop_times is empty' do
+      before { trip.stop_times.clear }
+
+      it { is_expected.to be_empty }
+    end
+
+    context 'when stop times contains a single Stop Time' do
+      before { trip.stop_times = [stop_times.first] }
+
+      it { is_expected.to be_empty }
+    end
+
+    context 'when JourneyPattern is available' do
+      let(:context) do
+        Chouette.create do
+          route stop_count: 2 do
+            journey_pattern
+          end
+        end
+      end
+
+      let(:journey_pattern) { context.journey_pattern }
+      before { allow(trip_decorator).to receive(:journey_pattern).and_return(journey_pattern) }
+
+      it { is_expected.to have_attributes(size: 2) }
+    end
+  end
+
+  describe '#time_table_id' do
+    subject { trip_decorator.time_table_id }
+
+    let(:time_table_found) { double('TimeTable found by service_id') }
+    before { trip_decorator.lookup = double(time_tables: double) }
+
+    it do
+      expect(trip_decorator.lookup.time_tables).to receive(:find_id).with(trip.service_id,
+                                                                          starting_day_offset: trip_decorator.starting_day_offset).and_return(time_table_found)
+      is_expected.to eq(time_table_found)
+    end
+
+    context 'when lookup is nil' do
+      before { trip_decorator.lookup = nil }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when service_id is not defined' do
+      before { trip.service_id = nil }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when lookup is not defined' do
+      before { trip_decorator.lookup = nil }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when lookup is not defined' do
+      before { allow(trip_decorator).to receive(:starting_day_offset).and_return(nil) }
+
+      it { is_expected.to be_nil }
+    end
+  end
+
+  describe '#vehicle_journey_time_table_relationships' do
+    subject { trip_decorator.vehicle_journey_time_table_relationships }
+
+    context 'when time_table_id is not defined' do
+      before { allow(trip_decorator).to receive(:time_table_id).and_return(nil) }
+
+      it { is_expected.to be_empty }
+
+      describe 'errors' do
+        subject { trip_decorator.errors }
+        before { trip_decorator.vehicle_journey_time_table_relationships }
+
+        it {
+          is_expected.to include(an_object_having_attributes(message_key: :service_unknown,
+                                                             message_attributes: {
+                                                               service_id: trip.service_id, resource_id: trip.id
+                                                             }))
+        }
+      end
+    end
+
+    context 'when time_table_id is 42' do
+      before { allow(trip_decorator).to receive(:time_table_id).and_return(42) }
+
+      it { is_expected.to contain_exactly(an_object_having_attributes(time_table_id: 42)) }
+      it { is_expected.to contain_exactly(an_instance_of(Chouette::TimeTablesVehicleJourney)) }
+    end
+  end
+
+  describe '#code' do
+    subject { trip_decorator.code }
+
+    context 'when code_space is "test"' do
+      before { trip_decorator.code_space = CodeSpace.new(short_name: 'test') }
+
+      it { is_expected.to have_attributes(code_space: an_object_having_attributes(short_name: 'test')) }
+      it { is_expected.to have_attributes(value: trip.id) }
+    end
+
+    context 'when code_space is not defined' do
+      before { trip_decorator.code_space = nil }
+
+      it { is_expected.to be_nil }
+    end
+  end
+
+  describe '#published_journey_name' do
+    subject { trip_decorator.published_journey_name }
+
+    context 'when Trip short_name is "dummy"' do
+      before { trip.short_name = 'dummy' }
+
+      it { is_expected.to be(trip.short_name) }
+    end
+
+    context 'when Trip short_name is undefined and Trip id "AAMV1"' do
+      before { trip.short_name = nil }
+
+      it { is_expected.to be(trip.id) }
+    end
+  end
+
+  describe '#published_journey_identifier' do
+    subject { trip_decorator.published_journey_identifier }
+
+    context 'when Trip id "AAMV1"' do
+      it { is_expected.to be(trip.id) }
+    end
+  end
+
+  describe '#starting_day_offset' do
+    subject { trip_decorator.starting_day_offset }
+
+    context 'when no stop_times is defined' do
+      before { trip.stop_times.clear }
+
+      it { is_expected.to be_zero }
+    end
+
+    context 'when first stop_time departure time is 12:00:00' do
+      before { trip.stop_times.first.departure_time = '12:00:00' }
+
+      it { is_expected.to be_zero }
+    end
+
+    context 'when first stop_time departure time is 24:00:00' do
+      before { trip.stop_times.first.departure_time = '24:00:00' }
+
+      it { is_expected.to eq(1) }
+    end
+
+    context 'when first stop_time departure time is 48:00:00' do
+      before { trip.stop_times.first.departure_time = '48:00:00' }
+
+      it { is_expected.to eq(2) }
+    end
+
+    context 'when first stop_time departure time is "dummy"' do
+      before { trip.stop_times.first.departure_time = 'dummy' }
+
+      it { is_expected.to be_zero }
+    end
   end
 end

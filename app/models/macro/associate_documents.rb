5338135719cdf13c53eee09d4371e9f505e1d69f
Hai Hieu VU <haihieu.vu@enroute.mobi>
2026-02-24 10:37:15 +0100

    CHOUETTE-5293 Use documents in current workbench of the macro

--- app/models/macro/associate_documents.rb
+++ app/models/macro/associate_documents.rb
@@ -33,6 +33,7 @@ module Macro
       def each_element_in_document_memberships(&block)
         PostgreSQLCursor::Cursor.new(query).each do |attributes|
           document_name = attributes.delete 'document_name'
+          attributes.merge!(documentable_type: documentable_type)
           document_membership = DocumentMembership.new attributes
 
           block.call document_membership, document_name
@@ -43,7 +44,7 @@ module Macro
         models
           .joins(model_codes)
           .joins(document_codes)
-          .joins(documents)
+          .joins(workbench_documents)
           .joins(outer_join_document_memberships)
           .where(document_memberships: { id: nil })
           .select(select)
@@ -54,22 +55,25 @@ module Macro
         <<-SQL
           document_codes.resource_id AS document_id,
           documents.name AS document_name,
-          #{model_collection}.id AS documentable_id,
-          model_codes.resource_type AS documentable_type
+          #{model_table_name}.id AS documentable_id
         SQL
       end
 
-      def documents
+      def workbench_documents
         <<-SQL
-          INNER JOIN public.documents ON public.documents.id = document_codes.resource_id
+          INNER JOIN public.documents
+          ON public.documents.id = document_codes.resource_id
+          INNER JOIN public.document_providers
+          ON public.documents.document_provider_id = public.document_providers.id
+          AND public.document_providers.workbench_id = #{workbench.id}
         SQL
       end
 
       def model_codes
         <<-SQL
           INNER JOIN public.codes model_codes
-          ON model_codes.resource_type = #{documentable_type}
-          AND model_codes.resource_id = #{model_collection}.id
+          ON model_codes.resource_type = '#{documentable_type}'
+          AND model_codes.resource_id = #{model_table_name}.id
           AND model_codes.code_space_id = #{model_code_space}
         SQL
       end
@@ -86,14 +90,18 @@ module Macro
       def outer_join_document_memberships
         <<-SQL
           LEFT OUTER JOIN public.document_memberships
-          ON documentable_type = #{documentable_type}
-          AND documentable_id = #{model_collection}.id
+          ON documentable_type = '#{documentable_type}'
+          AND documentable_id = #{model_table_name}.id
           AND document_id = document_codes.resource_id
         SQL
       end
 
       def documentable_type
-        @documentable_type ||= "'Chouette::#{target_model}'"
+        @documentable_type ||= "Chouette::#{target_model}"
+      end
+
+      def model_table_name
+        @model_table_name ||= models.quoted_table_name
       end
 
       def models

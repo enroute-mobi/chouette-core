649662b1a9325486bd3f331c49d8b38ff366146c
Aymeric Le Dorze <aymeric.le-dorze@enroute.mobi>
2026-03-27 12:26:54 +0100

    CHOUETTE-5340 Fix broken caches in Import::NetexGeneric

--- app/models/import/netex_generic.rb
+++ app/models/import/netex_generic.rb
@@ -1630,13 +1630,17 @@ module Import
         def netex_journey_pattern_ref
           return @netex_journey_pattern_ref if defined?(@netex_journey_pattern_ref)
 
+          @netex_journey_pattern_ref = build_netex_journey_pattern_ref
+        end
+
+        def build_netex_journey_pattern_ref
           netex_journey_pattern_ref = journey_pattern_ref&.ref
           unless netex_journey_pattern_ref
             errors.add(:service_journey_without_journey_pattern)
             return nil
           end
 
-          @netex_journey_pattern_ref = netex_journey_pattern_ref
+          netex_journey_pattern_ref
         end
 
         def route_journey_pattern(attribute)
@@ -1720,6 +1724,10 @@ module Import
         end
 
         def passing_time_decorators
+          @passing_time_decorators ||= build_passing_time_decorators
+        end
+
+        def build_passing_time_decorators
           unless chouette_stop_point_ids.count == passing_times.count
             errors.add(
               :number_passing_times_incoherent,
@@ -1775,6 +1783,10 @@ module Import
           def arrival_time_of_day
             return @arrival_time_of_day if defined?(@arrival_time_of_day)
 
+            @arrival_time_of_day = build_arrival_time_of_day
+          end
+
+          def build_arrival_time_of_day
             arrival_time_of_day = if arrival_time
                                     time_of_day(arrival_time, arrival_day_offset)
                                   else
@@ -1788,12 +1800,16 @@ module Import
               return nil
             end
 
-            @arrival_time_of_day = arrival_time_of_day
+            arrival_time_of_day
           end
 
           def departure_time_of_day
             return @departure_time_of_day if defined?(@departure_time_of_day)
 
+            @departure_time_of_day = build_departure_time_of_day
+          end
+
+          def build_departure_time_of_day
             departure_time_of_day = time_of_day(departure_time, departure_day_offset)
             if !last && departure_time_of_day.nil?
               errors.add(
@@ -1803,7 +1819,7 @@ module Import
               return nil
             end
 
-            @departure_time_of_day = departure_time_of_day
+            departure_time_of_day
           end
 
           def latest_arrival_time_of_day

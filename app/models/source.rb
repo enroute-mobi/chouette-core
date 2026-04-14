5d578463c2059d42b5c80c6ee26d40750adb0ba5
Guillaume Perot <guillaume.perot@enroute.mobi>
2026-04-14 14:49:18 +0200

    CHOUETTE-5302 - pr reviews

--- app/models/source.rb
+++ app/models/source.rb
@@ -40,8 +40,6 @@ class Source < ApplicationModel
   scope :enabled, -> { where.not(retrieval_frequency: 'none') }
 
   before_validation :clean, on: :update
-  before_validation :handle_authorization_header_options
-  after_initialize :set_default_authorization_header
 
   attribute :retrieval_time_of_day, TimeOfDay::Type::TimeWithoutZone.new
   attribute :retrieval_days_of_week, Cuckoo::DaysOfWeek::Type.new
@@ -159,33 +157,17 @@ class Source < ApplicationModel
   end
 
   def clean
-    unless downloader_type == "authorization"
-      self.downloader_options = self.downloader_options.except("raw_authorization", "custom_header_name")
-    end
-  end
-
-  def downloader_option_use_standard_authorization_header?
-    downloader_options['use_standard_authorization_header'] != 'false'
+    except = []
+    except << 'raw_authorization' if !downloader_type_authorization?
+    except << 'custom_header_name' if !downloader_type_authorization? || downloader_option_use_standard_authorization_header?
+    self.downloader_options = self.downloader_options.except(*except) if except.any?
   end
 
   def downloader_option_use_standard_authorization_header
     downloader_options['use_standard_authorization_header'] != 'false'
   end
 
-  def set_default_authorization_header
-    return unless downloader_type_authorization?
-    return if downloader_options.key?('use_standard_authorization_header')
-    
-    self.downloader_options = downloader_options.merge('use_standard_authorization_header' => true)
-  end
-
-  def handle_authorization_header_options
-    if downloader_type_authorization?
-      if downloader_option_use_standard_authorization_header?
-        self.downloader_options = self.downloader_options.except("custom_header_name")
-      end
-    end
-  end
+  alias downloader_option_use_standard_authorization_header? downloader_option_use_standard_authorization_header
 
   def import_option_line_provider
     candidate_line_providers.find_by(id: import_option_line_provider_id) || workbench.default_line_provider
@@ -224,12 +206,8 @@ class Source < ApplicationModel
       import_options[$1] == 'true'
     when /^import_option_(.*)$/
       import_options[$1]
-    when /^downloader_option_use_standard_authorization_header\?$/
-      downloader_options['use_standard_authorization_header'] == 'true'
     when /^downloader_option_(.*)=$/
       downloader_options[$1] = args.first
-    when /^downloader_option_(.*)\?$/
-      downloader_options[$1] == 'true'
     when /^downloader_option_(.*)$/
       downloader_options[$1]
     else
